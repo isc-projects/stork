78aa9b87b219a68bd417aa429a03226118cae6c6
Michal Nowikowski <godfryd@isc.org>
2019-10-04 13:23:44 +0200

    added fake dashboard page

--- webui/src/app/app.component.ts
+++ webui/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+
 import { AuthService, User } from './auth.service';
 
 @Component({
@@ -12,11 +15,24 @@ export class AppComponent {
     title = 'Stork';
     currentUser = null;
 
+    items: MenuItem[];
+
     constructor(
         private router: Router,
         private auth: AuthService
     ) {
         this.auth.currentUser.subscribe(x => this.currentUser = x);
+    }
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'DHCP',
+            },
+            {
+                label: 'DNS',
+            }
+        ];
     }
 
     signOut() {
--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -9,8 +9,13 @@ import { AppComponent } from './app.component';
 import { ApiModule, BASE_PATH, Configuration } from './backend';
 
 import {ButtonModule} from 'primeng/button';
+import {MenubarModule} from 'primeng/menubar';
+import {PanelModule} from 'primeng/panel';
+import {TableModule} from 'primeng/table';
+
 import { LoginScreenComponent } from './login-screen/login-screen.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HostsTableComponent } from './hosts-table/hosts-table.component';
 
 export function cfgFactory() {
     return new Configuration();
@@ -20,7 +25,8 @@ export function cfgFactory() {
     declarations: [
         AppComponent,
         LoginScreenComponent,
-        DashboardComponent
+        DashboardComponent,
+        HostsTableComponent
     ],
     imports: [
         BrowserModule,
@@ -31,6 +37,9 @@ export function cfgFactory() {
         ApiModule.forRoot(cfgFactory),
 
         ButtonModule,
+        MenubarModule,
+        PanelModule,
+        TableModule,
     ],
     providers: [{ provide: BASE_PATH, useValue: 'http://localhost:8080/api' }],
     bootstrap: [AppComponent]
