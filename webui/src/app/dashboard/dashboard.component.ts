f71abe7fcf22c20c04a3ee501693b2ad26ec4008
Michal Nowikowski <godfryd@isc.org>
2019-10-04 13:28:59 +0200

    added missing panel import

--- webui/src/app/dashboard/dashboard.component.ts
+++ webui/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {PanelModule} from 'primeng/panel';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
