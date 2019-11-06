8aa7b9f143096e72f7d1390ff682d54c3eda6bf5
Marcin Siodelski <marcin@isc.org>
2019-11-06 13:14:34 +0100

    [#33] Code reuse in stork-db-migrate
    Reused settings structure and the password function in the migrations
    tool.

--- backend/cmd/stork-db-migrate/main.go
+++ backend/cmd/stork-db-migrate/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/crypto/ssh/terminal"
 	"github.com/jessevdk/go-flags"
 	log "github.com/sirupsen/logrus"
 	"isc.org/stork/server/database"
@@ -21,10 +20,7 @@ type upOpts struct {
 
 // Common application options.
 type Opts struct{
-	DatabaseName string `short:"d" long:"database" description:"database name" env:"STORK_DATABASE_NAME" default:"stork"`
-	UserName string `short:"u" long:"user" description:"database user name" env:"STORK_DATABASE_USER_NAME" default:"stork"`
-	Host string `long:"host" description:"database user name" env:"STORK_DATABASE_HOST" default:"localhost"`
-	Port int `short:"p" long:"port" description:"database user name" env:"STORK_DATABASE_PORT" default:"5432"`
+	dbops.DatabaseSettings
 	Init cmdOpts `command:"init" description:"Create schema versioning table in the database"`
 	Up upOpts `command:"up" description:"Run all available migrations or up to a selected version"`
 	Down cmdOpts `command:"down" description:"Revert last migration"`
@@ -47,19 +43,7 @@ func main() {
 	}
 
 	// Password from the environment variable takes precedence.
-	password := os.Getenv("STORK_DATABASE_PASSWORD")
-	if len(password) == 0 {
-		// Prompt the user for database password.
-		fmt.Printf("database password: ")
-		pass, err := terminal.ReadPassword(0)
-		fmt.Printf("\n")
-
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-
-		password = string(pass)
-	}
+	dbops.Password(&opts.DatabaseSettings)
 
 	// The up command requires special treatment. If the target version is specified
 	// it must be appended to the arguments we pass to the go-pg migrations.
@@ -71,9 +55,9 @@ func main() {
 
 	// Use the provided credentials to connect to the database.
 	db := dbops.NewPgDB(&dbops.PgOptions{
-		User:     opts.UserName,
-		Password: string(password),
-		Database: opts.DatabaseName,
+		User:     opts.User,
+		Password: opts.Password,
+		Database: opts.DbName,
 		Addr:     fmt.Sprintf("%s:%d", opts.Host, opts.Port),
 	})
 	defer db.Close()
