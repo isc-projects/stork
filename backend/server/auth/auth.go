a03e13201d11e1ac35f9382da837a0b07ec0e282
Marcin Siodelski <marcin@isc.org>
2019-12-17 11:42:15 +0100

    [#97] Added a couple authorization tests

--- backend/server/auth/auth.go
+++ backend/server/auth/auth.go
@@ -7,19 +7,31 @@ import (
 	"strconv"
 )
 
+// Checks if the given user is permitted to access a resource. Currently the
+// access pattern is very simple, the super-admin user can access all
+// resources. The admin-user can access all resources except those related
+// to users management.
 func Authorize(user *dbmodel.SystemUser, req *http.Request) (ok bool, err error) {
+	// If there is no user (possibly the user has not signed in), the user
+	// does not belong to any groups or the request is nil, reject access to
+	// the resource.
 	if user == nil || len(user.Groups) == 0 || req == nil {
 		return false, nil
 	}
 
+	// If the user is super-admin he can access all resources.
 	if user.InGroup(&dbmodel.SystemGroup{Id: dbmodel.SuperAdminGroupId}) {
 		return true, nil
 	}
 
+	// If the user does not belong to the super-admin group and trying to
+	// access the user specific information, check if the data the user
+	// is trying to access belong to this user. If not, reject access.
 	if ok, _ := regexp.Match(`users/{0,}`, []byte(req.URL.Path)); ok {
 		ok, _ := regexp.Match(`users/{1,}`+strconv.Itoa(user.Id)+`/*`, []byte(req.URL.Path))
 		return ok, nil
 	}
 
+	// All other resources can be accessed by the admin user.
 	return true, err
 }
--- backend/server/auth/auth_test.go
+++ backend/server/auth/auth_test.go
@@ -7,24 +7,48 @@ import (
 	"testing"
 )
 
-func TestAuthorize(t *testing.T) {
+// Helper function checking if the user belonging to the specified group
+// has access to the resource.
+func authorizeAccept(t *testing.T, groupId int, path string) bool {
+	// Create user with ID 5 and specified group id.
 	user := &dbmodel.SystemUser{
 		Id: 5,
 		Groups: dbmodel.SystemGroups{
 			&dbmodel.SystemGroup{
-				Id:   2,
-				Name: "admin",
+				Id: groupId,
 			},
 		},
 	}
 
-	req, _ := http.NewRequest("GET", "http://localhost:4200/api/users?start=0&limit=10", nil)
+	// Create request with the specified path and authorize.
+	req, _ := http.NewRequest("GET", "http://example.org/api"+path, nil)
 	ok, err := Authorize(user, req)
 	require.NoError(t, err)
-	require.False(t, ok)
 
-	req, _ = http.NewRequest("GET", "http://example.com/users/5/", nil)
-	ok, err = Authorize(user, req)
-	require.NoError(t, err)
-	require.True(t, ok)
+	return ok
+}
+
+// Verify that users belonging to the super-admin and admin group
+// has appropriate access privileges.
+func TestAuthorize(t *testing.T) {
+	// admin group have limited access to the users' management
+	require.False(t, authorizeAccept(t, 2, "/users?start=0&limit=10"))
+	require.False(t, authorizeAccept(t, 2, "/users/list"))
+	require.False(t, authorizeAccept(t, 2, "/users/4/password"))
+	require.False(t, authorizeAccept(t, 2, "/users//4/password/"))
+	require.True(t, authorizeAccept(t, 2, "/users/5"))
+	require.True(t, authorizeAccept(t, 2, "/users/5/password"))
+	require.True(t, authorizeAccept(t, 2, "/users//5//password"))
+
+	// super-admin has no such restrictions
+	require.True(t, authorizeAccept(t, 1, "/users?start=0&limit=10"))
+	require.True(t, authorizeAccept(t, 1, "/users/list"))
+	require.True(t, authorizeAccept(t, 1, "/users/4/password"))
+	require.True(t, authorizeAccept(t, 1, "/users//4/password/"))
+	require.True(t, authorizeAccept(t, 1, "/users/5"))
+	require.True(t, authorizeAccept(t, 1, "/users/5/password"))
+	require.True(t, authorizeAccept(t, 1, "/users//5//password"))
+
+	// admin group have no restriction on machines
+	require.True(t, authorizeAccept(t, 2, "/machines/1/"))
 }
