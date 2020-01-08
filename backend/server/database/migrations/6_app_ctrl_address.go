06918d4ce37b32a46539e501c853888a72482e83
Marcin Siodelski <marcin@isc.org>
2020-01-08 17:19:50 +0100

    [#110] Check that ctrl_address is non-empty
    Altering migration no 6.

--- backend/server/database/migrations/6_app_ctrl_address.go
+++ backend/server/database/migrations/6_app_ctrl_address.go
@@ -8,7 +8,14 @@ func init() {
 	migrations.MustRegisterTx(func(db migrations.DB) error {
 		_, err := db.Exec(`
              ALTER TABLE public.app
-               ADD COLUMN ctrl_address TEXT NOT NULL DEFAULT 'localhost';
+               ADD COLUMN ctrl_address TEXT DEFAULT 'localhost';
+
+             ALTER TABLE public.app
+               ADD CONSTRAINT app_ctrl_address_not_empty CHECK (
+                   NOT (
+                     ctrl_address IS NULL || ctrl_address = ''
+                   )
+               );
            `)
 		return err
 
