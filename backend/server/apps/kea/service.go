28ecafa0073f2d8a55ea86c05eb651965e05c3d3
Marcin Siodelski <marcin@isc.org>
2020-02-11 09:52:17 +0100

    [#137] Detection of HA services added
    There is a new function which finds existing services for a given app or
    creates new services if no match is found. This mechanism will be used when
    new app is added to Stork via the UI. It will automatically associate the
    app with the service.

--- backend/server/apps/kea/service.go
+++ backend/server/apps/kea/service.go
@@ -1,28 +1,220 @@
 package kea
 
 import (
+	dbops "isc.org/stork/server/database"
 	dbmodel "isc.org/stork/server/database/model"
+	storkutil "isc.org/stork/util"
 )
 
-func DetectHAServices(dbApp *dbmodel.App) (services []dbmodel.Service) {
+// Checks if the specified Kea app belongs to a given HA service.
+// This is done by matching the HA configuration of the given app
+// with the HA configurations of the other apps already associated
+// with the service. In particular, the HA mode must match and for
+// the peers' configurations the server names, URLs and roles must
+// match.
+func appBelongsToHAService(app *dbmodel.App, service *dbmodel.Service) bool {
+	// If the service or app is nil, or if the app is not Kea then the app
+	// surely doesn't belong to the service.
+	if service.HAService == nil || app == nil || app.Type != "kea" {
+		return false
+	}
+
+	// Try to cast the App to AppKea. If that fails we bail again.
+	var (
+		appKea dbmodel.AppKea
+		ok     bool
+	)
+	if appKea, ok = app.Details.(dbmodel.AppKea); !ok {
+		return false
+	}
+
+	var index int = -1
+	for i, d := range appKea.Daemons {
+		if (d.Name == "dhcp4" || d.Name == "dhcp6") && (d.Name == service.HAService.HAType) {
+			index = i
+		}
+	}
+
+	// Failed to find a daemon matching the service HA type. Nothing more to do.
+	if index < 0 {
+		return false
+	}
+
+	// Successfully found the daemon. Get its instance.
+	appDaemon := appKea.Daemons[index]
+
+	// Get the applications's HA configuration and check if it is set.
+	var appConfigHA dbmodel.KeaConfigHA
+	_, appConfigHA, ok = appDaemon.Config.GetHAHooksLibrary()
+	if !ok || !appConfigHA.IsSet() {
+		return false
+	}
+
+	// We have all required information about the app. Now we have to
+	// iterate over the apps already associated with the service and
+	// compare their configurations with the configuration of the
+	// app.
+	for _, sa := range service.Apps {
+		// Do not compare the app to itself.
+		if sa.ID == app.ID {
+			continue
+		}
+
+		var serviceAppKea dbmodel.AppKea
+		if serviceAppKea, ok = sa.Details.(dbmodel.AppKea); !ok {
+			continue
+		}
+
+		// For the app belonging to the service, let's iterate over the daemons
+		// to compare configurations.
+		for _, serviceDaemon := range serviceAppKea.Daemons {
+			// Daemon doesn't match the service type. They must be both 'dhcp4'
+			// or 'dhcp6'.
+			if serviceDaemon.Name != appDaemon.Name {
+				continue
+			}
+
+			// Get the HA configuration of the app belonging to the service.
+			var serviceDaemonConfigHA dbmodel.KeaConfigHA
+			_, serviceDaemonConfigHA, ok = serviceDaemon.Config.GetHAHooksLibrary()
+			if !ok || !serviceDaemonConfigHA.IsSet() || (*appConfigHA.Mode != *serviceDaemonConfigHA.Mode) {
+				// There is something wrong with the service or the mode is not matching.
+				// This service is not matching.
+				return false
+			}
+
+			// Now we have to compare the peers' configurations.
+			for _, servicePeer := range serviceDaemonConfigHA.Peers {
+				// For the given peer in the service let's find the corresponding one
+				// specified in the app's configuration.
+				ok = false
+				for _, appPeer := range appConfigHA.Peers {
+					if (*appPeer.Name == *servicePeer.Name) &&
+						(*appPeer.URL == *servicePeer.URL) &&
+						(*appPeer.Role == *servicePeer.Role) {
+						// Match found.
+						ok = true
+						break
+					}
+				}
+				// Peer not found on the URL is not matching.
+				if !ok {
+					return false
+				}
+			}
+		}
+	}
+
+	// Passed all checks that could possibly eliminate the app from the service.
+	return true
+}
+
+// Parses High Availability configuration of the given Kea app and matches that
+// configuration with existing services. If no matching service is found, it
+// is created and returned. This function neither creates nor updates any
+// services in the database. It is up to the caller of this function to
+// perform such updates based on the returned services by the function.
+// It is possible to check whether the returned service is a new instance
+// or an instance already present in the database by calling the
+// Service.IsNew() function. If this is a new service, the caller should
+// call dbmodel.AddService() to add the new service and the associations of
+// the apps with this service. Otherwise, the caller should first call the
+// UpdateBaseHAService function using the service returned by this function.
+// Next, AddAppToService() should be called to associate the app with the
+// service in the database. A single app may belong to multiple services,
+// e.g. an app can belong to Kea DHCPv4 HA service and Kea DHCPv6 HA service.
+func DetectHAServices(db *dbops.PgDB, dbApp *dbmodel.App) (services []dbmodel.Service) {
+	// If this is not Kea application it does not belong to HA service.
 	appKea, ok := dbApp.Details.(dbmodel.AppKea)
 	if !ok {
 		return services
 	}
 
+	// If this is the Kea application, we need to iterate over the DHCP daemons and
+	// match their configuration against the services.
 	for _, d := range appKea.Daemons {
+		// We only detct HA services for DHCP daemons. Other daemons do not support it.
 		if d.Config == nil || (d.Name != "dhcp4" && d.Name != "dhcp6") {
 			continue
 		}
 
-		if _, _, ok := d.Config.GetHAHooksLibrary(); ok {
-			// todo: check if there is already a matching service in the db.
-
-			service := dbmodel.Service{
-				HAService: &dbmodel.BaseHAService{
-					HAType: d.Name,
-				},
+		// Check if the configuration contains any HA configuration.
+		if _, params, ok := d.Config.GetHAHooksLibrary(); ok {
+			// Make sure that the required parameters are set.
+			if !params.IsSet() {
+				continue
 			}
+
+			// HA configuration must contain this-server-name parameter which indicates
+			// which of the peers' configurations belongs to it. Let's iterate over
+			// the configured peers to identify the one.
+			var index int = -1
+			for i, p := range params.Peers {
+				// this-server-name matches one of the peers. Remember which one.
+				if *p.Name == *params.ThisServerName {
+					index = i
+				}
+			}
+
+			// This server not found.
+			if index < 0 {
+				continue
+			}
+
+			// This server configuration found.
+			thisServer := params.Peers[index]
+
+			// Next, check if there are any existing services for the URLs found
+			// in this configuration.
+			var dbServices []dbmodel.Service
+			index = -1
+			for _, p := range params.Peers {
+				host, port := storkutil.ParseURL(*p.URL)
+				dbServices, _ = dbmodel.GetServicesByAppCtrlAddressPort(db, host, port)
+				index = -1
+				for i, service := range dbServices {
+					s := service
+					if service.HAService.HAType == d.Name && appBelongsToHAService(dbApp, &s) {
+						index = i
+						break
+					}
+				}
+				if index >= 0 {
+					break
+				}
+			}
+
+			var service dbmodel.Service
+			if index >= 0 {
+				// Service found.
+				service = dbServices[index]
+			} else {
+				// No service found in the db, so let's create one.
+				service = dbmodel.Service{
+					HAService: &dbmodel.BaseHAService{
+						HAType: d.Name,
+					},
+				}
+
+				service.Apps = append(service.Apps, dbApp)
+			}
+
+			// Set HA mode, if not set yet.
+			if len(service.HAService.HAMode) == 0 {
+				service.HAService.HAMode = *params.Mode
+			}
+
+			// Depending on the role of this server we will be setting different column
+			// of the HA service column.
+			switch *(thisServer.Role) {
+			case "primary":
+				service.HAService.PrimaryID = dbApp.ID
+			case "secondary", "standby":
+				service.HAService.SecondaryID = dbApp.ID
+			default:
+				service.HAService.BackupID = append(service.HAService.BackupID, dbApp.ID)
+			}
+
 			services = append(services, service)
 		}
 	}
--- backend/server/apps/kea/service_test.go
+++ backend/server/apps/kea/service_test.go
@@ -2,65 +2,364 @@ package kea
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	require "github.com/stretchr/testify/require"
 	dbmodel "isc.org/stork/server/database/model"
+	dbtest "isc.org/stork/server/database/test"
 )
 
-func getTestConfigDHCP4() *dbmodel.KeaConfig {
+// Returns DHCP server configuration created from a template. The template
+// parameters include root parameter, i.e. Dhcp4 or Dhcp6, High Availability
+// mode and a variadic list of HA peers. The peers are identified by names:
+// server1, server2 ...  server5. The server1 is a primary, the server2
+// is a secondary, the server3 is a standby and the remaining ones are the
+// backup servers.
+func getTestConfig(rootName, thisServerName, mode string, peerNames ...string) *dbmodel.KeaConfig {
+	type peerInfo struct {
+		URL  string
+		Role string
+	}
+	// Map server names to peer configurations.
+	peers := map[string]peerInfo{
+		"server1": {
+			URL:  "http://192.0.2.33:8000",
+			Role: "primary",
+		},
+		"server2": {
+			URL:  "http://192.0.2.66:8000",
+			Role: "secondary",
+		},
+		"server3": {
+			URL:  "http://192.0.2.66:8000",
+			Role: "standby",
+		},
+		"server4": {
+			URL:  "http://192.0.2.133:8000",
+			Role: "backup",
+		},
+		"server5": {
+			URL:  "http://192.0.2.166:8000",
+			Role: "backup",
+		},
+	}
+
+	// Output configuration of the peers from the template.
+	var peersList string
+	for _, peerName := range peerNames {
+		if peer, ok := peers[peerName]; ok {
+			peerTemplate := `
+                {
+                    "name": "%s",
+                    "url":  "%s",
+                    "role": "%s"
+                }`
+			peerTemplate = fmt.Sprintf(peerTemplate, peerName, peer.URL, peer.Role)
+			if len(peersList) > 0 {
+				peersList += ",\n"
+			}
+			peersList += peerTemplate
+		}
+	}
+
+	// Output the server configuration from the template.
 	configStr := `{
-        "Dhcp4": {
+        "%s": {
             "hooks-libraries": [
                 {
                     "library": "libdhcp_ha.so",
                     "parameters": {
                         "high-availability": [{
-                            "this-server-name": "server1",
-                            "mode": "load-balancing",
-                            "heartbeat-delay": 10000,
-                            "max-response-delay": 10000,
-                            "max-ack-delay": 5000,
-                            "max-unacked-clients": 5,
-                            "peers": [{
-                                "name": "server1",
-                                "url": "http://192.168.56.33:8000/",
-                                "role": "primary",
-                                "auto-failover": true
-                            }, {
-                                "name": "server2",
-                                "url": "http://192.168.56.66:8000/",
-                                "role": "secondary",
-                                "auto-failover": true
-                            }, {
-                                "name": "server3",
-                                "url": "http://192.168.56.99:8000/",
-                                "role": "backup",
-                                "auto-failover": false
-                            }]
+                            "this-server-name": "%s",
+                            "mode": "%s",
+                            "peers": [ %s ]
                         }]
                     }
                 }
             ]
         }
     }`
+	configStr = fmt.Sprintf(configStr, rootName, thisServerName, mode, peersList)
+
+	// Convert the configuration from JSON to KeaConfig.
 	var config dbmodel.KeaConfig
 	_ = json.Unmarshal([]byte(configStr), &config)
 	return &config
 }
 
+// Multi step test verifying that services can be gradually created from the
+// Kea apps being added to the database.
 func TestDetectHAServices(t *testing.T) {
-	dbApp := dbmodel.App{
+	db, _, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	// Add the first machine.
+	m := &dbmodel.Machine{
+		ID:        0,
+		Address:   "localhost",
+		AgentPort: 8080,
+	}
+	err := dbmodel.AddMachine(db, m)
+	require.NoError(t, err)
+
+	// Add the first Kea being a DHCPv4 secondary in load-balancing configuration
+	// and the DHCPv6 standby in the standby configuration.
+	app := dbmodel.App{
+		MachineID:   m.ID,
+		Type:        dbmodel.KeaAppType,
+		CtrlAddress: "192.0.2.66",
+		CtrlPort:    8000,
 		Details: dbmodel.AppKea{
 			Daemons: []*dbmodel.KeaDaemon{
 				{
-					Name:   "dhcp4",
-					Config: getTestConfigDHCP4(),
+					Name: "dhcp4",
+					Config: getTestConfig("Dhcp4", "server2", "load-balancing",
+						"server1", "server2", "server4"),
+				},
+				{
+					Name: "dhcp6",
+					Config: getTestConfig("Dhcp6", "server3", "hot-standby",
+						"server1", "server3"),
 				},
 			},
 		},
 	}
 
-	services := DetectHAServices(&dbApp)
+	// Add the app to the database so as it gets its ID.
+	err = dbmodel.AddApp(db, &app)
+	require.NoError(t, err)
+	require.NotZero(t, app.ID)
+
+	// Run service detection for this app.
+	services := DetectHAServices(db, &app)
+
+	// There should be two services returned, one for DHCPv4 and one for DHCPv6.
+	require.Len(t, services, 2)
+
+	// Check the DHCPv4 service first.
+	require.True(t, services[0].IsNew())
+	require.NotNil(t, services[0].HAService)
+	require.Equal(t, "dhcp4", services[0].HAService.HAType)
+	require.Equal(t, "load-balancing", services[0].HAService.HAMode)
+	require.Zero(t, services[0].HAService.PrimaryID)
+	require.Equal(t, app.ID, services[0].HAService.SecondaryID)
+	require.Empty(t, services[0].HAService.BackupID)
+	require.True(t, services[0].HAService.PrimaryStatusTime.IsZero())
+	require.True(t, services[0].HAService.SecondaryStatusTime.IsZero())
+	require.Empty(t, services[0].HAService.PrimaryLastState)
+	require.Empty(t, services[0].HAService.SecondaryLastState)
+
+	// Check the DHCPv6 service.
+	require.True(t, services[1].IsNew())
+	require.NotNil(t, services[1].HAService)
+	require.Equal(t, "dhcp6", services[1].HAService.HAType)
+	require.Equal(t, "hot-standby", services[1].HAService.HAMode)
+	require.Zero(t, services[1].HAService.PrimaryID)
+	require.Equal(t, app.ID, services[1].HAService.SecondaryID)
+	require.Empty(t, services[1].HAService.BackupID)
+	require.True(t, services[1].HAService.PrimaryStatusTime.IsZero())
+	require.True(t, services[1].HAService.SecondaryStatusTime.IsZero())
+	require.Empty(t, services[1].HAService.PrimaryLastState)
+	require.Empty(t, services[1].HAService.SecondaryLastState)
+
+	// These are new services, so the app should have been added to them.
+	require.Len(t, services[0].Apps, 1)
+	require.Len(t, services[1].Apps, 1)
+
+	// Add the services to the database.
+	err = dbmodel.AddService(db, &services[0])
+	require.NoError(t, err)
+	err = dbmodel.AddService(db, &services[1])
+	require.NoError(t, err)
+
+	// Run service detection again. The existing services should be returned.
+	services = DetectHAServices(db, &app)
+	require.Len(t, services, 2)
+
+	// This is no longer a new service.
+	require.False(t, services[0].IsNew())
+	require.Equal(t, "dhcp4", services[0].HAService.HAType)
+	require.Equal(t, "load-balancing", services[0].HAService.HAMode)
+	require.Zero(t, services[0].HAService.PrimaryID)
+	require.Equal(t, app.ID, services[0].HAService.SecondaryID)
+	require.Empty(t, services[0].HAService.BackupID)
+
+	require.False(t, services[1].IsNew())
+	require.Equal(t, "dhcp6", services[1].HAService.HAType)
+	require.Equal(t, "hot-standby", services[1].HAService.HAMode)
+	require.Zero(t, services[1].HAService.PrimaryID)
+	require.Equal(t, app.ID, services[1].HAService.SecondaryID)
+	require.Empty(t, services[1].HAService.BackupID)
+
+	// The apps should be already associated with the services.
+	require.Len(t, services[0].Apps, 1)
+	require.Len(t, services[1].Apps, 1)
+
+	// Add machine and app for the DHCPv4 backup server.
+	m = &dbmodel.Machine{
+		ID:        0,
+		Address:   "backup1",
+		AgentPort: 8080,
+	}
+	err = dbmodel.AddMachine(db, m)
+	require.NoError(t, err)
+
+	app = dbmodel.App{
+		MachineID:   m.ID,
+		Type:        dbmodel.KeaAppType,
+		CtrlAddress: "192.0.2.133",
+		CtrlPort:    8000,
+		Details: dbmodel.AppKea{
+			Daemons: []*dbmodel.KeaDaemon{
+				{
+					Name: "dhcp4",
+					Config: getTestConfig("Dhcp4", "server4", "load-balancing",
+						"server1", "server2", "server4"),
+				},
+			},
+		},
+	}
+	err = dbmodel.AddApp(db, &app)
+	require.NoError(t, err)
+	require.NotZero(t, app.ID)
+
+	// This time, we've added the server with only a DHCPv4 configuration.
+	// Therefore, only a DHCPv4 service should be returned for this app.
+	services = DetectHAServices(db, &app)
 	require.Len(t, services, 1)
+	require.False(t, services[0].IsNew())
+	require.NotNil(t, services[0].HAService)
+	require.Equal(t, "dhcp4", services[0].HAService.HAType)
+	require.Equal(t, "load-balancing", services[0].HAService.HAMode)
+	require.Zero(t, services[0].HAService.PrimaryID)
+	require.NotZero(t, services[0].HAService.SecondaryID)
+	require.Len(t, services[0].HAService.BackupID, 1)
+	require.Equal(t, app.ID, services[0].HAService.BackupID[0])
+
+	require.Len(t, services[0].Apps, 1)
+
+	// We have to update the service in the database according to the value
+	// returned by the detection routine.
+	err = dbmodel.UpdateBaseHAService(db, services[0].HAService)
+	require.NoError(t, err)
+
+	// We also have to associate the app with the service.
+	err = dbmodel.AddAppToService(db, services[0].ID, &app)
+	require.NoError(t, err)
+
+	// Add machine and app for the primary server.
+	m = &dbmodel.Machine{
+		ID:        0,
+		Address:   "primary",
+		AgentPort: 8080,
+	}
+	err = dbmodel.AddMachine(db, m)
+	require.NoError(t, err)
+
+	// The primary server includes both DHCPv4 and DHCPv6 confgurations.
+	app = dbmodel.App{
+		MachineID:   m.ID,
+		Type:        dbmodel.KeaAppType,
+		CtrlAddress: "192.0.2.33",
+		CtrlPort:    8000,
+		Details: dbmodel.AppKea{
+			Daemons: []*dbmodel.KeaDaemon{
+				{
+					Name: "dhcp4",
+					Config: getTestConfig("Dhcp4", "server1", "load-balancing",
+						"server1", "server2", "server4"),
+				},
+				{
+					Name: "dhcp6",
+					Config: getTestConfig("Dhcp6", "server1", "hot-standby",
+						"server1", "server3"),
+				},
+			},
+		},
+	}
+	err = dbmodel.AddApp(db, &app)
+	require.NoError(t, err)
+	require.NotZero(t, app.ID)
+
+	// Since we have added two HA configurations for this app, there should
+	// be two services returned, one for DHCPv4 and one for DHCPv6.
+	services = DetectHAServices(db, &app)
+	require.Len(t, services, 2)
+	require.False(t, services[0].IsNew())
+	require.NotNil(t, services[0].HAService)
+	require.Equal(t, "dhcp4", services[0].HAService.HAType)
+	require.Equal(t, "load-balancing", services[0].HAService.HAMode)
+	require.Equal(t, app.ID, services[0].HAService.PrimaryID)
+	require.NotZero(t, services[0].HAService.SecondaryID)
+	require.Len(t, services[0].HAService.BackupID, 1)
+
+	require.False(t, services[1].IsNew())
+	require.NotNil(t, services[1].HAService)
+	require.Equal(t, "dhcp6", services[1].HAService.HAType)
+	require.Equal(t, "hot-standby", services[1].HAService.HAMode)
+	require.Equal(t, app.ID, services[1].HAService.PrimaryID)
+	require.NotZero(t, services[1].HAService.SecondaryID)
+	require.Empty(t, services[1].HAService.BackupID)
+
+	// The DHCPv4 service should have two apps associated with it, one for
+	// the secondary server and one for the backup. We will have to associate
+	// the primary server with this service on our own.
+	require.Len(t, services[0].Apps, 2)
+
+	// The DHCPv6 service should have only one app associated, i.e. the
+	// secondary server, because we didn't add the backup server to it.
+	require.Len(t, services[1].Apps, 1)
+
+	// Update the services in the database.
+	err = dbmodel.UpdateBaseHAService(db, services[0].HAService)
+	require.NoError(t, err)
+	err = dbmodel.AddAppToService(db, services[0].ID, &app)
+	require.NoError(t, err)
+
+	err = dbmodel.UpdateBaseHAService(db, services[1].HAService)
+	require.NoError(t, err)
+	err = dbmodel.AddAppToService(db, services[1].ID, &app)
+	require.NoError(t, err)
+
+	// Add machine and app for another DHCPv4 backup server.
+	m = &dbmodel.Machine{
+		ID:        0,
+		Address:   "backup2",
+		AgentPort: 8080,
+	}
+	err = dbmodel.AddMachine(db, m)
+	require.NoError(t, err)
+
+	app = dbmodel.App{
+		MachineID:   m.ID,
+		Type:        dbmodel.KeaAppType,
+		CtrlAddress: "192.0.2.166",
+		CtrlPort:    8000,
+		Details: dbmodel.AppKea{
+			Daemons: []*dbmodel.KeaDaemon{
+				{
+					Name: "dhcp4",
+					Config: getTestConfig("Dhcp4", "server5", "load-balancing",
+						"server1", "server2", "server4", "server5"),
+				},
+			},
+		},
+	}
+	err = dbmodel.AddApp(db, &app)
+	require.NoError(t, err)
+	require.NotZero(t, app.ID)
+
+	services = DetectHAServices(db, &app)
+	require.Len(t, services, 1)
+	require.False(t, services[0].IsNew())
+	require.NotNil(t, services[0].HAService)
+	require.Equal(t, "dhcp4", services[0].HAService.HAType)
+	require.Equal(t, "load-balancing", services[0].HAService.HAMode)
+	require.NotZero(t, services[0].HAService.PrimaryID)
+	require.NotZero(t, services[0].HAService.SecondaryID)
+	require.Len(t, services[0].HAService.BackupID, 2)
+	require.Contains(t, services[0].HAService.BackupID, app.ID)
+
+	require.Len(t, services[0].Apps, 3)
 }
