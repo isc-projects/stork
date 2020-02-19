96584b2b05a325920109e5fc9a58c737af6a116e
Michal Nowikowski <godfryd@isc.org>
2020-02-19 16:22:17 +0100

    [#166] added test for kea stats puller with empty results

--- backend/server/apps/kea/statspuller_test.go
+++ backend/server/apps/kea/statspuller_test.go
@@ -3,7 +3,7 @@ package kea
 import (
 	"testing"
 
-	log "github.com/sirupsen/logrus"
+	//log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 
 	"isc.org/stork/server/agentcomm"
@@ -25,8 +25,8 @@ func TestStatsPullerBasic(t *testing.T) {
 	sp.Shutdown()
 }
 
-// Check if gathering stats works.
-func TestStatsPullerGatherStats(t *testing.T) {
+// Check if pulling stats works.
+func TestStatsPullerPullStats(t *testing.T) {
 	db, _, teardown := dbtest.SetupDatabaseTestCase(t)
 	defer teardown()
 
@@ -109,8 +109,83 @@ func TestStatsPullerGatherStats(t *testing.T) {
 	// prepare stats puller
 	sp := NewStatsPuller(db, fa)
 
-	// invoke gathering stats
-	appsOkCnt, err := sp.gatherLeaseStats()
+	// invoke pulling stats
+	appsOkCnt, err := sp.pullLeaseStats()
+	require.NoError(t, err)
+	require.Equal(t, 1, appsOkCnt)
+
+	// TODO: check collected stats
+
+	// shutdown stats puller
+	sp.Shutdown()
+}
+
+// Check if empty stats response is handled correctly.
+func TestStatsPullerEmptyResponse(t *testing.T) {
+	db, _, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	// prepare fake agents
+	fa := storktest.NewFakeAgents(func(callNo int, cmdResponses []interface{}) {
+		// DHCPv4
+		daemons, _ := agentcomm.NewKeaDaemons("dhcp4")
+		command, _ := agentcomm.NewKeaCommand("stat-lease4-get", daemons, nil)
+		json := `[{
+                            "result": 0,
+                            "text": "Everything is fine",
+                            "arguments": {}
+                         }]`
+		agentcomm.UnmarshalKeaResponseList(command, json, cmdResponses[0])
+
+		// DHCPv6
+		daemons, _ = agentcomm.NewKeaDaemons("dhcp6")
+		command, _ = agentcomm.NewKeaCommand("stat-lease6-get", daemons, nil)
+		json = `[{
+                           "result": 2,
+                           "text": "'stat-lease6-get' command not supported."
+                        }]`
+		agentcomm.UnmarshalKeaResponseList(command, json, cmdResponses[1])
+	})
+
+	// add one machine with one kea app
+	m := &dbmodel.Machine{
+		ID:        0,
+		Address:   "localhost",
+		AgentPort: 8080,
+	}
+	err := dbmodel.AddMachine(db, m)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, m.ID)
+	a := &dbmodel.App{
+		ID:          0,
+		MachineID:   m.ID,
+		Type:        dbmodel.KeaAppType,
+		CtrlAddress: "cool.example.org",
+		CtrlPort:    1234,
+		CtrlKey:     "",
+		Active:      true,
+		Details: dbmodel.AppKea{
+			Daemons: []*dbmodel.KeaDaemon{
+				{
+					Active: true,
+					Name:   "dhcp4",
+				},
+				{
+					Active: true,
+					Name:   "dhcp6",
+				},
+			},
+		},
+	}
+	err = dbmodel.AddApp(db, a)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, a.ID)
+
+	// prepare stats puller
+	sp := NewStatsPuller(db, fa)
+
+	// invoke pulling stats
+	appsOkCnt, err := sp.pullLeaseStats()
 	require.NoError(t, err)
 	require.Equal(t, 1, appsOkCnt)
 
