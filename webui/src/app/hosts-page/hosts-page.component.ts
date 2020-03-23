c5d2b1b7cdd10853fe46ffe33dafd612bd43734c
Marcin Siodelski <marcin@isc.org>
2020-03-23 18:33:42 +0100

    [#210] Display reservations in the UI

--- webui/src/app/hosts-page/hosts-page.component.ts
+++ webui/src/app/hosts-page/hosts-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 
+import { DHCPService } from '../backend/api/api'
+
 /**
  * This component implemnents a page which displays hosts along with
  * their DHCP identifiers and IP reservations. The list of hosts is
@@ -14,7 +16,41 @@ import { Component, OnInit } from '@angular/core'
     styleUrls: ['./hosts-page.component.sass'],
 })
 export class HostsPageComponent implements OnInit {
-    constructor() {}
+    // hosts
+    hosts: any[]
+    totalHosts = 0
+
+    // filters
+    filterText = ''
+
+    constructor(private dhcpApi: DHCPService) {}
 
     ngOnInit() {}
+
+    /**
+     * Loads hosts from the database into the component.
+     *
+     * @param event Event object containing an index if the first row, maximum
+     *              number of rows to be returned and a text for hosts filtering.
+     */
+    loadHosts(event) {
+        let text
+        if (event.filters.text) {
+            text = event.filters.text.value
+        }
+
+        this.dhcpApi.getHosts(event.first, event.rows, null, text).subscribe(data => {
+            this.hosts = data.items
+            this.totalHosts = data.total
+        })
+    }
+
+    /**
+     * Filters the list of hosts by text. Filtering is performed on the server
+     */
+    keyDownFilterText(hostsTable, event) {
+        if (this.filterText.length >= 3 || event.key === 'Enter') {
+            hostsTable.filter(this.filterText, 'text', 'equals')
+        }
+    }
 }
