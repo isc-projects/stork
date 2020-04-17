616a042fd59e0f6bd7bde6032cae7273f8a640ca
Tomek Mrugalski <tomasz@isc.org>
2020-04-17 13:29:35 +0200

    [#164] SettingService test fixed

--- webui/src/app/setting.service.spec.ts
+++ webui/src/app/setting.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing'
 
 import { SettingService } from './setting.service'
+import { SettingsService } from './backend'
+import { HttpClient, HttpHandler } from '@angular/common/http'
 
 describe('SettingService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}))
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [SettingsService, HttpClient, HttpHandler]
+    }))
 
     it('should be created', () => {
         const service: SettingService = TestBed.inject(SettingService)
