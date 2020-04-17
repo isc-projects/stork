616be8d0fefe5207371593043a09dd93fd1099b9
Tomek Mrugalski <tomasz@isc.org>
2020-04-17 15:26:24 +0200

    [#164] AuthGuard test fixed

--- webui/src/app/auth.guard.spec.ts
+++ webui/src/app/auth.guard.spec.ts
@@ -1,11 +1,20 @@
 import { TestBed, async, inject } from '@angular/core/testing'
 
 import { AuthGuard } from './auth.guard'
+import { RouterModule, Router } from '@angular/router'
+import { HttpClient, HttpHandler } from '@angular/common/http'
+import { UsersService } from './backend'
+import { MessageService } from 'primeng/api'
 
 describe('AuthGuard', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [AuthGuard],
+            imports: [RouterModule],
+            providers: [AuthGuard, HttpClient, HttpHandler, UsersService, MessageService, {
+                provide: Router,
+                useValue: {}
+            }
+            ]
         })
     })
 
