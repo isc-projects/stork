c7331de9575b7a106423420814a121e18dacd44c
Tomek Mrugalski <tomasz@isc.org>
2020-04-17 16:38:58 +0200

    [#164] AppsPageComponent improved
     - still failing, but import problems fixed

--- webui/src/app/apps-page/apps-page.component.spec.ts
+++ webui/src/app/apps-page/apps-page.component.spec.ts
@@ -1,6 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { AppsPageComponent } from './apps-page.component'
+import { TabMenuModule } from 'primeng/tabmenu'
+import { MenuModule } from 'primeng/menu'
+import { FormsModule } from '@angular/forms'
+import { TableModule } from 'primeng/table'
+import { Bind9AppTabComponent } from '../bind9-app-tab/bind9-app-tab.component'
+import { KeaAppTabComponent } from '../kea-app-tab/kea-app-tab.component'
+import { LocaltimePipe } from '../localtime.pipe'
+import { TooltipModule } from 'primeng/tooltip'
+import { TabPanel, TabViewModule } from 'primeng/tabview'
+import { HaStatusComponent } from '../ha-status/ha-status.component'
+import { PanelModule } from 'primeng/panel'
+import { MessageModule } from 'primeng/message'
+import { ActivatedRoute, Router, RouterModule } from '@angular/router'
+import { ServicesService } from '../backend'
+import { HttpClient, HttpHandler } from '@angular/common/http'
+import { MessageService } from 'primeng/api'
 
 describe('AppsPageComponent', () => {
     let component: AppsPageComponent
@@ -8,7 +24,15 @@ describe('AppsPageComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [AppsPageComponent],
+            providers: [ServicesService, HttpClient, HttpHandler, MessageService, {
+                provide: ActivatedRoute,
+                useValue: {}
+            }, {
+                provide: Router,
+                useValue: {}
+            }],
+            imports: [TabMenuModule, MenuModule, FormsModule, TableModule, TooltipModule, TabViewModule, PanelModule, MessageModule, RouterModule],
+            declarations: [AppsPageComponent, Bind9AppTabComponent, KeaAppTabComponent, LocaltimePipe, HaStatusComponent]
         }).compileComponents()
     }))
 
