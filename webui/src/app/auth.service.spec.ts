63c7fa74535a74eb9e290fcf8ed5f5177a42d538
Tomek Mrugalski <tomasz@isc.org>
2020-04-17 15:19:34 +0200

    [#164] AuthService test fixed

--- webui/src/app/auth.service.spec.ts
+++ webui/src/app/auth.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing'
 
 import { AuthService } from './auth.service'
+import { HttpClient, HttpHandler } from '@angular/common/http'
+import { UsersService } from './backend'
+import { RouterModule, Router } from '@angular/router'
+import { MessageService } from 'primeng/api'
 
 describe('AuthService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}))
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [HttpClient, HttpHandler, UsersService, {
+            provide: Router, useValue: {}
+        }, MessageService]
+    }))
 
     it('should be created', () => {
         const service: AuthService = TestBed.inject(AuthService)
