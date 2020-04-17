35f3a618798000d60c5b0fe68f5a2e4474f4cdec
Tomek Mrugalski <tomasz@isc.org>
2020-04-17 16:39:34 +0200

    [#164] Bind9AppTabComponent improved
     - still failing, imports fixed

--- webui/src/app/bind9-app-tab/bind9-app-tab.component.spec.ts
+++ webui/src/app/bind9-app-tab/bind9-app-tab.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { Bind9AppTabComponent } from './bind9-app-tab.component'
+import { RouterLink, Router, RouterModule, ActivatedRoute } from '@angular/router'
+import { TooltipModule } from 'primeng/tooltip'
+import { TabViewModule } from 'primeng/tabview'
+import { LocaltimePipe } from '../localtime.pipe'
+import { LocationStrategy } from '@angular/common'
 
 describe('Bind9AppTabComponent', () => {
     let component: Bind9AppTabComponent
@@ -8,7 +13,16 @@ describe('Bind9AppTabComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [Bind9AppTabComponent],
+            imports: [TooltipModule, TabViewModule, RouterModule],
+            declarations: [Bind9AppTabComponent, LocaltimePipe],
+            providers: [ LocationStrategy, {
+                provide: Router,
+                useValue: {}
+            }, {
+                provide: ActivatedRoute,
+                useValue: {}
+
+            }]
         }).compileComponents()
     }))
 
