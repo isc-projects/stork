b2cef3b0e0f70d148da5f50e33ce6c299a367496
Thomas Markwalder <tmark@isc.org>
2020-05-19 16:44:09 -0400

    [#213] Added command line switches to agent
    backend/cmd/stork-agent/main.go
        Added top level settings which define --prometheusOnly
        and --storkOnly command line flags

--- backend/cmd/stork-agent/main.go
+++ backend/cmd/stork-agent/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
 
 	flags "github.com/jessevdk/go-flags"
 	log "github.com/sirupsen/logrus"
@@ -10,6 +12,12 @@ import (
 	"isc.org/stork/agent"
 	storkutil "isc.org/stork/util"
 )
+
+// Global Agent settings.
+type AgentSettings struct {
+    PrometheusOnly bool `long:"prometheusOnly" description:"agent does not listen for stork only exports to prometheus" env:"STORK_AGENT_PROMETHEUS_ONLY"`
+    StorkOnly      bool `long:"storkOnly" description:"agent only listens for stork does not export to prometheus" env:"STORK_AGENT_PROMETHEUS_ONLY"`
+}
 
 func main() {
 	// Setup logging
@@ -27,11 +35,17 @@ func main() {
 	promBind9Exporter := agent.NewPromBind9Exporter(appMonitor)
 
 	// Prepare parse for command line flags.
-	parser := flags.NewParser(&storkAgent.Settings, flags.Default)
+    var agentSettings AgentSettings
+	parser := flags.NewParser(&agentSettings, flags.Default)
 	parser.ShortDescription = "Stork Agent"
 	parser.LongDescription = "Stork Agent"
 
-	_, err := parser.AddGroup("Prometheus Kea Exporter flags", "", &promKeaExporter.Settings)
+	_, err := parser.AddGroup("Stork Server flags", "", &storkAgent.Settings)
+	if err != nil {
+		log.Fatalf("FATAL error: %+v", err)
+	}
+
+	_, err = parser.AddGroup("Prometheus Kea Exporter flags", "", &promKeaExporter.Settings)
 	if err != nil {
 		log.Fatalf("FATAL error: %+v", err)
 	}
@@ -42,20 +56,31 @@ func main() {
 	}
 
 	if _, err := parser.Parse(); err != nil {
-		code := 1
 		if fe, ok := err.(*flags.Error); ok {
 			if fe.Type == flags.ErrHelp {
-				code = 0
+		        os.Exit(0)
 			}
 		}
-		os.Exit(code)
+		log.Fatalf("FATAL error: %+v", err)
 	}
 
-	promKeaExporter.Start()
-	defer promKeaExporter.Shutdown()
+    // Only start the exporters if they're enabled.
+    if !agentSettings.StorkOnly {
+	    promKeaExporter.Start()
+	    defer promKeaExporter.Shutdown()
 
-	promBind9Exporter.Start()
-	defer promBind9Exporter.Shutdown()
+	    promBind9Exporter.Start()
+	    defer promBind9Exporter.Shutdown()
+    }
 
-	storkAgent.Serve()
+    // Only start the agent service if it's enabled.
+    if !agentSettings.PrometheusOnly {
+	    storkAgent.Serve()
+    } else {
+        // We wait for ctl-c
+        log.Infof("Agent is running without listening for Stork")
+        c := make(chan os.Signal, 1)
+        signal.Notify(c, os.Interrupt, syscall.SIGINT)
+        <-c
+    }
 }
