b9224f468c697d48984973b586ac6a2d5968dc5c
Marcin Siodelski <marcin@isc.org>
2020-05-29 16:48:34 +0200

    [#303] Added db migration no 24

---
+++ backend/server/database/migrations/24_hostname_reservations.go
@@ -0,0 +1,19 @@
+package dbmigs
+
+import (
+	"github.com/go-pg/migrations/v7"
+)
+
+func init() {
+	migrations.MustRegisterTx(func(db migrations.DB) error {
+		_, err := db.Exec(`
+            ALTER TABLE host ADD COLUMN IF NOT EXISTS hostname TEXT;
+        `)
+		return err
+	}, func(db migrations.DB) error {
+		_, err := db.Exec(`
+            ALTER TABLE host DROP COLUMN IF EXISTS hostname;
+        `)
+		return err
+	})
+}
--- backend/server/database/migrations_test.go
+++ backend/server/database/migrations_test.go
@@ -117,7 +117,7 @@ func TestAvailableVersion(t *testing.T) {
 	defer cleanupDb(t, db)
 
 	avail := AvailableVersion()
-	require.GreaterOrEqual(t, avail, int64(23))
+	require.GreaterOrEqual(t, avail, int64(24))
 }
 
 // Test that current version is returned from the database.
