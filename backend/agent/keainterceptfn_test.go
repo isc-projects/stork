378c3edd4657f1998dca25fb5b7b2302c6cea08c
Marcin Siodelski <marcin@isc.org>
2020-08-27 13:45:47 +0200

    [#348] Allow log files from intercepted config
    Implemented interceptor function which gathers log files from the config-get
    response and allows viewing them in the logTailer.

--- backend/agent/keainterceptfn_test.go
+++ backend/agent/keainterceptfn_test.go
@@ -8,10 +8,57 @@ import (
 )
 
 // Tests that config-get is intercepted and loggers found in the returned
-// configuration are recorded. The log viewer is permitted to access only
+// configuration are recorded. The log tailer is permitted to access only
 // those log files.
 func TestIcptConfigGetLoggers(t *testing.T) {
-	response := &keactrl.Response{}
-	err := icptConfigGetLoggers(nil, response)
-	require.Nil(t, err)
+	sa, _ := setupAgentTest(nil)
+
+	response := &keactrl.Response{
+		ResponseHeader: keactrl.ResponseHeader{
+			Result: 0,
+			Text:   "Everything is fine",
+			Daemon: "dhcp4",
+		},
+		Arguments: &map[string]interface{}{
+			"Dhcp4": map[string]interface{}{
+				"loggers": []interface{}{
+					map[string]interface{}{
+						"output_options": []interface{}{
+							map[string]interface{}{
+								"output": "/tmp/kea-dhcp4.log",
+							},
+							map[string]interface{}{
+								"output": "stderr",
+							},
+						},
+					},
+					map[string]interface{}{
+						"output_options": []interface{}{
+							map[string]interface{}{
+								"output": "stdout",
+							},
+						},
+					},
+					map[string]interface{}{
+						"output_options": []interface{}{
+							map[string]interface{}{
+								"output": "/tmp/kea-dhcp4-allocations.log",
+							},
+							map[string]interface{}{
+								"output": "syslog:1",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	err := icptConfigGetLoggers(sa, response)
+	require.NoError(t, err)
+	require.NotNil(t, sa.logTailer)
+	require.True(t, sa.logTailer.allowed("/tmp/kea-dhcp4.log"))
+	require.True(t, sa.logTailer.allowed("/tmp/kea-dhcp4-allocations.log"))
+	require.False(t, sa.logTailer.allowed("stdout"))
+	require.False(t, sa.logTailer.allowed("stderr"))
+	require.False(t, sa.logTailer.allowed("syslog:1"))
 }
