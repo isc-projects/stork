dca3a35f8df41780bdc8e67ea4e1ced7169cc0e8
Marcin Siodelski <marcin@isc.org>
2021-02-02 13:00:33 +0100

    [#458] App name displayed in global search
    When app is found with the global search, its name is displayed instead of
    app id.

--- webui/src/app/global-search/global-search.component.spec.ts
+++ webui/src/app/global-search/global-search.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { GlobalSearchComponent } from './global-search.component'
 import { SearchService } from '../backend/api/api'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { By } from '@angular/platform-browser'
 
 describe('GlobalSearchComponent', () => {
     let component: GlobalSearchComponent
@@ -24,5 +25,26 @@ describe('GlobalSearchComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy()
+    })
+
+    it('should display app name and proper link to an app in the results', () => {
+        component.searchResults = {
+            subnets: { items: [] },
+            sharedNetworks: { items: [] },
+            hosts: { items: [] },
+            machines: { items: [] },
+            apps: { items: [{ type: 'kea', id: 1, name: 'dhcp-server' }] },
+            users: { items: [] },
+            groups: { items: [] },
+        }
+        fixture.detectChanges()
+        const appsDiv = fixture.debugElement.query(By.css('#apps-div'))
+        expect(appsDiv.children.length).toBe(2)
+        const appDiv = appsDiv.children[1]
+        expect(appDiv.children.length).toBe(1)
+        const appAnchor = appDiv.children[0]
+        expect(appAnchor.nativeElement.innerText).toBe('dhcp-server')
+        expect(appAnchor.properties.hasOwnProperty('routerLink')).toBeTrue()
+        expect(appAnchor.properties.routerLink).toBe('/apps/kea/1')
     })
 })
