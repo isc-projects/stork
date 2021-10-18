1cdf60bdc2f39881236ca8f7f05995f7379c2bfe
Marcin Siodelski <marcin@isc.org>
2021-10-18 15:31:33 +0200

    [#461] Added config_report db model

---
+++ backend/server/database/model/configreport.go
@@ -0,0 +1,139 @@
+package dbmodel
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/go-pg/pg/v9"
+	"github.com/go-pg/pg/v9/orm"
+	pkgerrors "github.com/pkg/errors"
+	dbops "isc.org/stork/server/database"
+)
+
+// Structure representing a single config report generated during
+// the daemons configuration review.
+type ConfigReport struct {
+	ID           int64
+	CreatedAt    time.Time
+	ProducerName string
+	Contents     string
+
+	DaemonID int64
+	Daemon   *Daemon
+
+	RefDaemons []*Daemon `pg:"many2many:daemon_to_config_report,fk:config_report_id,joinFK:daemon_id"`
+}
+
+// Structure representing a many-to-many relationship between daemons
+// and config reports.
+type DaemonToConfigReport struct {
+	DaemonID       int64 `pg:",pk"`
+	ConfigReportID int64 `pg:",pk"`
+	OrderIndex     int64
+}
+
+// Adds a single configuration report and its relationships with the
+// daemons to the database.
+func AddConfigReport(dbIface interface{}, configReport *ConfigReport) error {
+	// Start transaction if it hasn't been started yet.
+	tx, rollback, commit, err := dbops.Transaction(dbIface)
+	if err != nil {
+		return err
+	}
+	defer rollback()
+
+	// Insert the config_report entry.
+	_, err = tx.Model(configReport).Insert()
+
+	if err == nil {
+		// Insert associations between the configuration report and
+		// the daemons.
+		var assocs []DaemonToConfigReport
+		for i := range configReport.RefDaemons {
+			d := configReport.RefDaemons[i]
+			assocs = append(assocs, DaemonToConfigReport{
+				DaemonID:       d.ID,
+				ConfigReportID: configReport.ID,
+				OrderIndex:     int64(i),
+			})
+		}
+
+		if len(assocs) > 0 {
+			// Insert the associations.
+			_, err = tx.Model(&assocs).OnConflict("DO NOTHING").Insert()
+		}
+	}
+
+	if err != nil {
+		// The error message is formatted differently depending on whether we
+		// have one or more daemons associated with the config report.
+		if len(configReport.RefDaemons) == 1 {
+			err = pkgerrors.Wrapf(err, "problem with inserting the configuration report for daemon %d",
+				configReport.RefDaemons[0].ID)
+		} else {
+			var daemonIds []string
+			for _, d := range configReport.RefDaemons {
+				daemonIds = append(daemonIds, fmt.Sprintf("%d", d.ID))
+			}
+			err = pkgerrors.Wrapf(err, "problem with inserting the configuration report for daemons %s",
+				strings.Join(daemonIds, ", "))
+		}
+		return err
+	}
+
+	// All done.
+	err = commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Select all config reports for the specified daemon.
+func GetConfigReportsByDaemonID(db *pg.DB, daemonID int64) ([]ConfigReport, error) {
+	var configReports []ConfigReport
+	err := db.Model(&configReports).
+		//		Join("INNER JOIN daemon_to_config_report AS dtcr ON dtcr.config_report_id = config_report.id").
+		//		Join("INNER JOIN daemon AS d ON d.id = dtcr.daemon_id").
+		Where("config_report.daemon_id = ?", daemonID).
+		Relation("Daemon.App").
+		Relation("RefDaemons", func(q *orm.Query) (*orm.Query, error) {
+			return q.Order("daemon_to_config_report.order_index ASC"), nil
+		}).
+		Select()
+
+	if err != nil && !errors.Is(err, pg.ErrNoRows) {
+		err = pkgerrors.Wrapf(err, "problem with selecting config reports for daemon %d", daemonID)
+		return configReports, err
+	}
+	return configReports, nil
+}
+
+// Delete all config reports for the specified daemon.
+func DeleteConfigReportsByDaemonID(dbIface interface{}, daemonID int64) error {
+	// Start transaction if it hasn't been started yet.
+	tx, rollback, commit, err := dbops.Transaction(dbIface)
+	if err != nil {
+		return err
+	}
+	defer rollback()
+
+	_, err = tx.Model((*ConfigReport)(nil)).
+		Where("daemon_id = ?", daemonID).
+		Delete()
+
+	if err != nil && !errors.Is(err, pg.ErrNoRows) {
+		err = pkgerrors.Wrapf(err, "problem with deleting config reports for daemon %d", daemonID)
+		return err
+	}
+
+	err = commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ backend/server/database/model/configreport_test.go
@@ -0,0 +1,216 @@
+package dbmodel
+
+import (
+	"testing"
+
+	require "github.com/stretchr/testify/require"
+	dbtest "isc.org/stork/server/database/test"
+)
+
+// Test inserting, selecting and deleting configuration reports
+// shared by two daemons.
+func TestConfigReportSharingDaemons(t *testing.T) {
+	db, _, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	// Add a machine.
+	machine := &Machine{
+		Address:   "localhost",
+		AgentPort: 8080,
+	}
+	err := AddMachine(db, machine)
+	require.NoError(t, err)
+
+	// Add an app with two daemons.
+	app := &App{
+		Type:      AppTypeKea,
+		MachineID: machine.ID,
+		Daemons: []*Daemon{
+			NewKeaDaemon("dhcp4", true),
+			NewKeaDaemon("dhcp6", true),
+		},
+	}
+	daemons, err := AddApp(db, app)
+	require.NoError(t, err)
+	require.Len(t, daemons, 2)
+
+	// Add a configuration report shared by both daemons.
+	configReport := &ConfigReport{
+		ProducerName: "test",
+		Contents:     "Here is the test report",
+		DaemonID:     daemons[0].ID,
+		RefDaemons:   daemons,
+	}
+	err = AddConfigReport(db, configReport)
+	require.NoError(t, err)
+
+	// Try to get the configuration report.
+	configReports, err := GetConfigReportsByDaemonID(db, daemons[0].ID)
+	require.NoError(t, err)
+	require.Len(t, configReports, 1)
+	require.NotNil(t, configReports[0].Daemon)
+	require.Len(t, configReports[0].RefDaemons, 2)
+	require.Equal(t, "dhcp4", configReports[0].RefDaemons[0].Name)
+	require.Equal(t, "dhcp6", configReports[0].RefDaemons[1].Name)
+	require.Equal(t, "Here is the test report", configReports[0].Contents)
+
+	// Delete the configuration report.
+	err = DeleteConfigReportsByDaemonID(db, daemons[0].ID)
+	require.NoError(t, err)
+
+	// The report is no longer returned.
+	configReports, err = GetConfigReportsByDaemonID(db, daemons[0].ID)
+	require.NoError(t, err)
+	require.Empty(t, configReports)
+}
+
+// Test inserting, selecting and deleting configuration reports associated
+// with distinct daemons.
+func TestConfigReportDistinctDaemons(t *testing.T) {
+	db, _, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	// Add a machine.
+	machine := &Machine{
+		Address:   "localhost",
+		AgentPort: 8080,
+	}
+	err := AddMachine(db, machine)
+	require.NoError(t, err)
+
+	// Add an app.
+	app := &App{
+		Type:      AppTypeKea,
+		MachineID: machine.ID,
+		Daemons: []*Daemon{
+			NewKeaDaemon("dhcp4", true),
+			NewKeaDaemon("dhcp6", true),
+		},
+	}
+	daemons, err := AddApp(db, app)
+	require.NoError(t, err)
+	require.Len(t, daemons, 2)
+
+	// Associate configuration reports with distinct daemons.
+	configReports := []ConfigReport{
+		{
+			ProducerName: "test",
+			Contents:     "Here is the first test report",
+			DaemonID:     daemons[0].ID,
+			RefDaemons: []*Daemon{
+				daemons[0],
+			},
+		},
+		{
+			ProducerName: "test",
+			Contents:     "Here is the second test report",
+			DaemonID:     daemons[1].ID,
+			RefDaemons: []*Daemon{
+				daemons[1],
+			},
+		},
+	}
+	for i := range configReports {
+		err = AddConfigReport(db, &configReports[i])
+		require.NoError(t, err)
+	}
+
+	// Select configuration reports for both daemons.
+	for i, daemon := range daemons {
+		returnedConfigReports, err := GetConfigReportsByDaemonID(db, daemon.ID)
+		require.NoError(t, err)
+		require.Len(t, returnedConfigReports, 1)
+		require.Len(t, returnedConfigReports[0].RefDaemons, 1)
+		require.Equal(t, configReports[i].Contents, returnedConfigReports[0].Contents)
+	}
+
+	// Delete configuration reports for the first daemon.
+	err = DeleteConfigReportsByDaemonID(db, daemons[0].ID)
+	require.NoError(t, err)
+
+	// The configuration report for the first daemon no longer exists.
+	configReports, err = GetConfigReportsByDaemonID(db, daemons[0].ID)
+	require.NoError(t, err)
+	require.Empty(t, configReports)
+
+	// It should not affect the report for the second daemon.
+	configReports, err = GetConfigReportsByDaemonID(db, daemons[1].ID)
+	require.NoError(t, err)
+	require.Len(t, configReports, 1)
+}
+
+// Test different cases of malformed configuration reports.
+func TestInvalidConfigReport(t *testing.T) {
+	db, _, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	// Add a machine.
+	machine := &Machine{
+		Address:   "localhost",
+		AgentPort: 8080,
+	}
+	err := AddMachine(db, machine)
+	require.NoError(t, err)
+
+	// Add an app.
+	app := &App{
+		Type:      AppTypeKea,
+		MachineID: machine.ID,
+		Daemons: []*Daemon{
+			NewKeaDaemon("dhcp4", true),
+		},
+	}
+	daemons, err := AddApp(db, app)
+	require.NoError(t, err)
+	require.Len(t, daemons, 1)
+
+	testCases := []string{
+		"empty producer name",
+		"empty contents",
+		"invalid daemon id",
+	}
+	configReports := []*ConfigReport{
+		{
+			ProducerName: "",
+			Contents:     "Here is the first test report",
+			DaemonID:     daemons[0].ID,
+			RefDaemons: []*Daemon{
+				daemons[0],
+			},
+		},
+		{
+			ProducerName: "test",
+			Contents:     "",
+			DaemonID:     daemons[0].ID,
+			RefDaemons: []*Daemon{
+				daemons[0],
+			},
+		},
+		{
+			ProducerName: "test",
+			Contents:     "contents",
+			DaemonID:     111111,
+			RefDaemons: []*Daemon{
+				daemons[0],
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		tcIndex := i
+		t.Run(tc, func(t *testing.T) {
+			err = AddConfigReport(db, configReports[tcIndex])
+			require.Error(t, err)
+		})
+	}
+}
+
+// This test verifies that deleting configuration reports for non-existing
+// daemon does not return an error.
+func TestConfigReportDeleteNonExisting(t *testing.T) {
+	db, _, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	err := DeleteConfigReportsByDaemonID(db, 12345)
+	require.NoError(t, err)
+}
