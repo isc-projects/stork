f68116c9949993e03cd03e76543d54ed7a125ff4
Marcin Siodelski <marcin@isc.org>
2021-12-21 18:24:00 +0100

    [#672] Squash flag tested for mapstructure decoder

--- backend/appcfg/kea/decoder_test.go
+++ backend/appcfg/kea/decoder_test.go
@@ -6,19 +6,26 @@ import (
 	require "github.com/stretchr/testify/require"
 )
 
-// Test that the decoder's matcher function removes hyphens
-// and uses case-insensitive matching.
+// Test that the decoder's matcher function removes hyphens,
+// uses case-insensitive matching and the decoder respects
+// embedded structures.
 func TestDecode(t *testing.T) {
 	input := map[string]interface{}{
-		"abc-def": "foo",
-		"abc":     "baz",
+		"abc-def":      "foo",
+		"abc":          "baz",
+		"nested-value": "squash",
+	}
+	type NestedStruct struct {
+		NestedValue string
 	}
 	output := struct {
 		ABCDef string
 		Abc    string
+		NestedStruct
 	}{}
 	err := decode(input, &output)
 	require.NoError(t, err)
 	require.Equal(t, "foo", output.ABCDef)
 	require.Equal(t, "baz", output.Abc)
+	require.Equal(t, "squash", output.NestedStruct.NestedValue)
 }
