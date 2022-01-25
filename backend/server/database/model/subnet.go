dfa8900a2488e55ef1a2e3a56d4634362b2e3a4e
Slawek Figiel <slawek@isc.org>
2022-01-25 18:34:07 +0100

    [#560] Rephrase some sentences

--- backend/server/database/model/subnet.go
+++ backend/server/database/model/subnet.go
@@ -719,7 +719,7 @@ func CalculateOutOfPoolAddressReservations(dbi dbops.DBI) (map[int64]uint64, err
 		Where("address_pool.subnet_id = host.subnet_id").
 		// Is it in a pool? - from lower to upper bands inclusively
 		Where("ip_reservation.address BETWEEN address_pool.lower_bound AND address_pool.upper_bound").
-		// We want to know if the address is in at least one pool
+		// We want only to know if the address is in at least one pool
 		Limit(1)
 
 	// Find out-of-pool host reservations.
@@ -780,8 +780,9 @@ func CalculateOutOfPoolPrefixReservations(dbi dbops.DBI) (map[int64]uint64, erro
 		// (has the same 48 starting bits), but the mask lengths are less then 64.
 		// - Prefixes 3001::/64 and 3001::/80 are in the pool. They are in an expected network
 		// and the mask lengths are greater or equals 64.
+		// The `<<=` is an operator that check if the left CIDR is contained withing right CIDR.
 		Where("ip_reservation.address <<= prefix_pool.prefix AND masklen(ip_reservation.address) >= prefix_pool.delegated_len").
-		// We want to know if the address is in at least one pool
+		// We want only to know if the address is in at least one pool
 		Limit(1)
 
 	// Find out-of-pool host reservations.
