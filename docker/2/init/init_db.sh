6d0c926ef8407728290c4ba5c0d8098fb1253fdf
Slawek Figiel <slawek@isc.org>
2022-03-11 15:41:43 +0100

    [#709] Agent Kea 6

--- docker/2/init/init_db.sh
+++ docker/2/init/init_db.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 set -x
 echo "Database type: ${DB_TYPE}"
+if [ ${DB_TYPE} = 'mysql' ]; then
+    until mysqladmin ping -h"${DB_HOST}" --silent;
+    do
+    echo "Waiting for database connection..."
+    # wait for 5 seconds before check again
+    sleep 5
+    done
+elif [ ${DB_TYPE} = 'pgsql' ]; then
+    db_port=5432
+else
+    echo "Unsupported DB_TYPE, choose mysql or pgsql"
+    exit 1
+fi
+
 
 echo "CREATE USER"
-create_user_query="CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 if [ ${DB_TYPE} = 'mysql' ]; then
+    create_user_query="CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
     mysql \
-        --user=root \
+        --user=${DB_ROOT_USER} \
         --password=${DB_ROOT_PASSWORD} \
         --host=${DB_HOST} \
         -e "$create_user_query"
 else
-    PGPASSWORD=${DB_PASSWORD} psql \
-        -U ${DB_USER} \
+    create_user_query="CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}';"
+    PGPASSWORD=${DB_ROOT_PASSWORD} psql \
+        -U ${DB_ROOT_USER} \
         -h ${DB_HOST} \
-        -d ${DB_NAME} \
         -c "$create_user_query"
 fi
 
@@ -22,20 +36,17 @@ echo "Checking if the database exists"
 exist_query="select * from schema_version;"
 if [ ${DB_TYPE} = 'mysql' ]; then
     mysql \
-        --user=root \
+        --user=${DB_ROOT_USER} \
         --password=${DB_ROOT_PASSWORD} \
         --host=${DB_HOST} \
         -e "$exist_query" \
         ${DB_NAME}
-elif [ ${DB_TYPE} = 'pgsql']; then
-    PGPASSWORD=${DB_PASSWORD} psql \
-        -U ${DB_USER} \
+else
+    PGPASSWORD=${DB_ROOT_PASSWORD} psql \
+        -U ${DB_ROOT_USER} \
         -h ${DB_HOST} \
         -d ${DB_NAME} \
         -c "$exist_query"
-else
-    echo "Unsupported DB_TYPE, choose mysql or pgsql"
-    exit 1
 fi
 
 has_db=$?
@@ -43,35 +54,34 @@ set -e
 
 if [ $has_db -ne 0 ]
 then
-echo "Create the database"
-    create_db_query="CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+    echo "Create the database"
+    create_db_query="CREATE DATABASE ${DB_NAME};"
     if [ ${DB_TYPE} = 'mysql' ]; then
         mysql \
-            --user=root \
+            --user=${DB_ROOT_USER} \
             --password=${DB_ROOT_PASSWORD} \
             --host=${DB_HOST} \
             -e "$create_db_query"
     else
-        PGPASSWORD=${DB_PASSWORD} psql \
-            -U ${DB_USER} \
+        PGPASSWORD=${DB_ROOT_PASSWORD} psql \
+            -U ${DB_ROOT_USER} \
             -h ${DB_HOST} \
-            -d ${DB_NAME} \
             -c "$create_db_query"
     fi
 fi
 
-grant_query="GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
 if [ ${DB_TYPE} = 'mysql' ]; then
+    grant_query="GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
     mysql \
-        --user=root \
+        --user=${DB_ROOT_USER} \
         --password=${DB_ROOT_PASSWORD} \
         --host=${DB_HOST} \
         -e "$grant_query"
 else
-    PGPASSWORD=${DB_PASSWORD} psql \
-        -U ${DB_USER} \
+    grant_query="GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};"
+    PGPASSWORD=${DB_ROOT_PASSWORD} psql \
+        -U ${DB_ROOT_USER} \
         -h ${DB_HOST} \
-        -d ${DB_NAME} \
         -c "$grant_query"
 fi
 
@@ -91,13 +101,13 @@ echo "Seed database"
 seed_file="${BASH_SOURCE%/*}/init_query.sql"
 if [ ${DB_TYPE} = 'mysql' ]; then
     mysql \
-        --user=root \
+        --user=${DB_ROOT_USER} \
         --password=${DB_ROOT_PASSWORD} \
         --host=${DB_HOST} \
         ${DB_NAME} < $seed_file
 else
-    PGPASSWORD=${DB_PASSWORD} psql \
-        -U ${DB_USER} \
+    PGPASSWORD=${DB_ROOT_PASSWORD} psql \
+        -U ${DB_ROOT_USER} \
         -h ${DB_HOST} \
-        -d ${DB_NAME} < cat $seed_file
+        -d ${DB_NAME} < $seed_file
 fi
