657cce5a05f86e0149fdad29138cf4f10efd08f0
Slawek Figiel <slawek@isc.org>
2022-06-15 15:47:55 +0200

    [#709] Rename file

--- tests/system/core/wrappers/__init__.py
+++ tests/system/core/wrappers/__init__.py
@@ -1,5 +1,5 @@
 # Export main classes
 from core.wrappers.server import Server
 from core.wrappers.kea import Kea
-from core.wrappers.bind import Bind9
+from core.wrappers.bind9 import Bind9
 from core.wrappers.perfdhcp import Perfdhcp
