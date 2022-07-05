7a76c5f656393ca578f40f3ef049ac720d412fe9
Slawek Figiel <slawek@isc.org>
2022-07-05 13:11:40 +0200

    [#761] Add switch to stop the demo

--- start-demo.sh
+++ start-demo.sh
@@ -17,21 +17,35 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # Prompt for CloudSmith access token
 usage()
 {
-    echo "Usage: start-demo.sh [ -f | --no-prompt ] [ CLOUD_SMITH_ACCESS_TOKEN ]"
+    echo "Usage: start-demo.sh [ -f | --no-prompt ] [ -s | --stop ] [ CLOUD_SMITH_ACCESS_TOKEN ]"
     echo "You can also set the access token using environment variable CS_REPO_ACCESS_TOKEN."
     exit 2
 }
 
+logo()
+{
+    echo ""
+    echo "-----------------------------------------------"
+    echo " ____  _             _    "
+    echo "/ ___|| |_ ___  _ __| | __"
+    echo "\___ \| __/ _ \| '__| |/ /"
+    echo " ___) | || (_) | |  |   < "
+    echo "|____/ \__\___/|_|  |_|\_\\"
+    echo ""
+}
+
 # Parse arguments
-PARSED_ARGUMENTS=$(getopt -a -n start-demo -o f --long no-prompt -- "$@")
+PARSED_ARGUMENTS=$(getopt -a -n start-demo -o fs --long no-prompt,stop -- "$@")
 
 eval set -- "$PARSED_ARGUMENTS"
 NO_PROMPT=0
+STOP=0
 ACCESS_TOKEN=${CS_REPO_ACCESS_TOKEN}
 while :
 do
   case "$1" in
     -f | --no-prompt)   NO_PROMPT=1      ; shift   ;;
+    -s | --stop)        STOP=1           ; shift   ;;
     # -- means the end of the arguments; drop this, and break out of the while loop
     --) shift; break ;;
     # If invalid options were passed, then getopt should have reported an error,
@@ -40,6 +54,23 @@ do
        usage ;;
   esac
 done
+
+# Stop the demo
+if [ ${STOP} -eq 1 ]
+then
+    docker-compose \
+        --project-directory "${SCRIPT_DIR}" \
+        -f "${SCRIPT_DIR}/docker/docker-compose.yaml" \
+        -f "${SCRIPT_DIR}/docker/docker-compose-premium.yaml" \
+        down --volumes
+
+    if [ ${NO_PROMPT} -eq 0 ]
+    then
+        logo
+        echo "The demo was stopped."
+    fi
+    exit 0
+fi
 
 # Set access token from CMD
 if [ $# -gt 0 ]
@@ -63,10 +94,12 @@ then
     fi
 fi
 
-PREMIUM_COMPOSE_FILES=
+PREMIUM_COMPOSE_OPTION=
+PREMIUM_COMPOSE_FILE=
 if ! [ -z "${ACCESS_TOKEN}" ]
 then
-    PREMIUM_COMPOSE_FILES="-f \"${SCRIPT_DIR}/docker/docker-compose-premium.yaml\""
+    PREMIUM_COMPOSE_OPTION="-f"
+    PREMIUM_COMPOSE_FILE="${SCRIPT_DIR}/docker/docker-compose-premium.yaml"
 fi
 
 # Run the demo
@@ -77,27 +110,24 @@ CS_REPO_ACCESS_TOKEN=${ACCESS_TOKEN} \
 docker-compose \
     --project-directory "${SCRIPT_DIR}" \
     -f "${SCRIPT_DIR}/docker/docker-compose.yaml" \
-    ${PREMIUM_COMPOSE_FILES} \
+    ${PREMIUM_COMPOSE_OPTION} "${PREMIUM_COMPOSE_FILE}" \
     build
 # Start Docker containers
 docker-compose \
     --project-directory "${SCRIPT_DIR}" \
     -f "${SCRIPT_DIR}/docker/docker-compose.yaml" \
-    ${PREMIUM_COMPOSE_FILES} \
+    ${PREMIUM_COMPOSE_OPTION} "${PREMIUM_COMPOSE_FILE}" \
     up -d
 
 if [ ${NO_PROMPT} -eq 0 ]
 then
-    echo
-    echo "-----------------------------------------------"
-    echo " ____  _             _    "
-    echo "/ ___|| |_ ___  _ __| | __"
-    echo "\___ \| __/ _ \| '__| |/ /"
-    echo " ___) | || (_) | |  |   < "
-    echo "|____/ \__\___/|_|  |_|\_\\"
-    echo "                          "
+    logo
     echo "Open the demo in the browser:"
-    echo "Stork Server: http://127.0.0.1:8080"
-    echo "Grafana: http://127.0.0.1:3000"
+    echo "Stork Server:      http://127.0.0.1:8080"
+    echo "Grafana:           http://127.0.0.1:3000"
+    echo "Prometheus:        http://127.0.0.1:9090"
     echo "Traffic simulator: http://127.0.0.1:5000"
+    echo "Default username: admin password: admin"
+    echo ""
+    echo "Use './start-demo.sh --stop' to shutdown the demo"
 fi
