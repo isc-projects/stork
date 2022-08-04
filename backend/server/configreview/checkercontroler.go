f5732706466d5b56f4b9c240fe72288b193bec70
Slawek Figiel <slawek@isc.org>
2022-08-04 11:07:19 +0200

    [#610] Add checker controller

--- backend/server/configreview/checkercontroler.go
+++ backend/server/configreview/checkercontroler.go
@@ -1,5 +1,8 @@
 package configreview
 
+// Represents a state of config checker managed by the config checker controller.
+// Checker for a given condition can be enabled or disabled or inherit the
+// state from the higher order rule.
 type CheckerState int
 
 const (
@@ -11,8 +14,55 @@ const (
 // Represents a configuration checker controller. It manages the enable or
 // disable states of checkers for given conditions, e.g., only for a specific
 // daemon, selector, or globally.
+// The checkers are enabled by default.
 type checkerController interface {
 	SetStateGlobally(checkerName string, enabled bool)
 	SetStateForDaemon(daemonID int64, checkerName string, state CheckerState)
 	IsCheckerEnabledForDaemon(daemonID int64, checkerName string) bool
+}
+
+// Implementation of the checker controller interface.
+type checkerControllerImpl struct {
+	globalStates map[string]bool
+	daemonStates map[int64]map[string]bool
+}
+
+// Constructs the checker controller object.
+func NewCheckerController() checkerController {
+	return &checkerControllerImpl{
+		globalStates: make(map[string]bool),
+		daemonStates: make(map[int64]map[string]bool),
+	}
+}
+
+// Sets the global state for a given checker.
+func (c checkerControllerImpl) SetStateGlobally(checkerName string, enabled bool) {
+	c.globalStates[checkerName] = enabled
+}
+
+// Sets the state of config checker for a specific daemon.
+func (c checkerControllerImpl) SetStateForDaemon(daemonID int64, checkerName string, state CheckerState) {
+	if _, ok := c.daemonStates[daemonID]; !ok {
+		c.daemonStates[daemonID] = make(map[string]bool)
+	}
+
+	if state == CheckerStateInherit {
+		delete(c.daemonStates[daemonID], checkerName)
+	} else {
+		c.daemonStates[daemonID][checkerName] = state == CheckerStateEnabled
+	}
+}
+
+// Lookups for the state of config checker for a given daemon. It combines the
+// daemon state with a global one.
+func (c checkerControllerImpl) IsCheckerEnabledForDaemon(daemonID int64, checkerName string) bool {
+	if _, ok := c.daemonStates[daemonID]; ok {
+		if enabled, ok := c.daemonStates[daemonID][checkerName]; ok {
+			return enabled
+		}
+	}
+	if enabled, ok := c.globalStates[checkerName]; ok {
+		return enabled
+	}
+	return true
 }
---
+++ backend/server/configreview/checkercontroller_test.go
@@ -0,0 +1,98 @@
+package configreview
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Test that the checker controller is constructed properly.
+func TestNewCheckerController(t *testing.T) {
+	// Act
+	controller := NewCheckerController()
+
+	// Assert
+	require.NotNil(t, controller)
+}
+
+// Test that the checker state is set properly.
+func TestSetStateGlobally(t *testing.T) {
+	// Arrange
+	controller := NewCheckerController()
+
+	// Act
+	controller.SetStateGlobally("foo", true)
+	controller.SetStateGlobally("bar", false)
+
+	// Assert
+	require.True(t, controller.IsCheckerEnabledForDaemon(0, "foo"))
+	require.False(t, controller.IsCheckerEnabledForDaemon(0, "bar"))
+}
+
+// Test that the checker state for a specific daemon is set properly.
+func TestSetStateForDaemon(t *testing.T) {
+	// Arrange
+	controller := NewCheckerController()
+
+	// Act
+	controller.SetStateForDaemon(1, "foo", CheckerStateEnabled)
+	controller.SetStateForDaemon(2, "bar", CheckerStateDisabled)
+	controller.SetStateForDaemon(3, "baz", CheckerStateInherit)
+
+	// Assert
+	require.True(t, controller.IsCheckerEnabledForDaemon(1, "foo"))
+	require.False(t, controller.IsCheckerEnabledForDaemon(2, "bar"))
+	require.True(t, controller.IsCheckerEnabledForDaemon(3, "baz"))
+}
+
+// Test that the checker state is correctly inherited for a specific daemon.
+func TestSetInheritedStateForDaemon(t *testing.T) {
+	// Arrange
+	controller := NewCheckerController()
+	controller.SetStateGlobally("foo", true)
+	controller.SetStateGlobally("bar", false)
+	controller.SetStateGlobally("baz", false)
+
+	// Act
+	controller.SetStateForDaemon(1, "foo", CheckerStateInherit)
+	controller.SetStateForDaemon(2, "bar", CheckerStateInherit)
+	controller.SetStateForDaemon(3, "baz", CheckerStateInherit)
+	controller.SetStateGlobally("baz", true)
+
+	// Assert
+	require.True(t, controller.IsCheckerEnabledForDaemon(1, "foo"))
+	require.False(t, controller.IsCheckerEnabledForDaemon(2, "bar"))
+	require.True(t, controller.IsCheckerEnabledForDaemon(3, "baz"))
+}
+
+// Test that the checker states are merged properly.
+func TestIsCheckerEnabledForDaemon(t *testing.T) {
+	// Arrange
+	controller := NewCheckerController()
+	controller.SetStateGlobally("foo", true)
+	controller.SetStateGlobally("fee", false)
+	controller.SetStateGlobally("bar", true)
+	controller.SetStateForDaemon(1, "bar", CheckerStateEnabled)
+	controller.SetStateGlobally("baz", true)
+	controller.SetStateForDaemon(1, "baz", CheckerStateDisabled)
+	controller.SetStateGlobally("biz", false)
+	controller.SetStateForDaemon(1, "biz", CheckerStateEnabled)
+	controller.SetStateGlobally("boz", false)
+	controller.SetStateForDaemon(1, "boz", CheckerStateDisabled)
+
+	// Act
+	foo := controller.IsCheckerEnabledForDaemon(1, "foo")
+	fee := controller.IsCheckerEnabledForDaemon(1, "fee")
+	bar := controller.IsCheckerEnabledForDaemon(1, "bar")
+	baz := controller.IsCheckerEnabledForDaemon(1, "baz")
+	biz := controller.IsCheckerEnabledForDaemon(1, "biz")
+	boz := controller.IsCheckerEnabledForDaemon(1, "boz")
+
+	// Assert
+	require.True(t, foo)
+	require.False(t, fee)
+	require.True(t, bar)
+	require.False(t, baz)
+	require.True(t, biz)
+	require.False(t, boz)
+}
