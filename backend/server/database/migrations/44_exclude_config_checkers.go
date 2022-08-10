459e265ff37c0e578f3d1d738bfda19b50bd538b
Slawek Figiel <slawek@isc.org>
2022-08-10 13:38:18 +0200

    [#610] Fix DB table name

--- backend/server/database/migrations/44_exclude_config_checkers.go
+++ backend/server/database/migrations/44_exclude_config_checkers.go
@@ -5,26 +5,26 @@ import "github.com/go-pg/migrations/v8"
 func init() {
 	migrations.MustRegisterTx(func(db migrations.DB) error {
 		_, err := db.Exec(`
-			CREATE TABLE config_daemon_checker_preference (
+			CREATE TABLE config_checker_preference (
 				id BIGSERIAL PRIMARY KEY,
 				daemon_id BIGINT,
 				checker_name TEXT NOT NULL,
 				excluded BOOLEAN NOT NULL,
-				CONSTRAINT config_daemon_checker_preference_daemon_id_fk FOREIGN KEY (daemon_id)
+				CONSTRAINT config_checker_preference_daemon_id_fk FOREIGN KEY (daemon_id)
 					REFERENCES daemon (id)
                     ON UPDATE CASCADE
                     ON DELETE CASCADE
 			);
 
-			CREATE UNIQUE INDEX config_daemon_checker_preference_non_null_idx ON config_daemon_checker_preference (daemon_id, checker_name) WHERE daemon_id IS NOT NULL;
-			CREATE UNIQUE INDEX config_daemon_checker_preference_nullable_idx ON config_daemon_checker_preference (checker_name) WHERE daemon_id IS NULL;
+			CREATE UNIQUE INDEX config_checker_preference_non_null_idx ON config_checker_preference (daemon_id, checker_name) WHERE daemon_id IS NOT NULL;
+			CREATE UNIQUE INDEX config_checker_preference_nullable_idx ON config_checker_preference (checker_name) WHERE daemon_id IS NULL;
 		`)
 		return err
 	}, func(db migrations.DB) error {
 		_, err := db.Exec(`
-			DROP INDEX config_daemon_checker_preference_nullable_idx;
-			DROP INDEX config_daemon_checker_preference_non_null_idx;
-			DROP TABLE config_daemon_checker_preference;
+			DROP INDEX config_checker_preference_nullable_idx;
+			DROP INDEX config_checker_preference_non_null_idx;
+			DROP TABLE config_checker_preference;
         `)
 		return err
 	})
