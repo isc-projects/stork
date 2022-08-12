23da0a6ba9d1af19408e107ef0615a0c851f02fa
Marcin Siodelski <marcin@isc.org>
2022-08-12 17:48:16 +0200

    [#827] Added example options to DHCP demo hosts

--- docker/config/agent-kea-premium/init_mysql_query.sql
+++ docker/config/agent-kea-premium/init_mysql_query.sql
@@ -10,3 +10,13 @@ insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp4_subnet_id, ipv4_a
 insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp4_subnet_id, ipv4_address) values (unhex('0a0a0a0a'), 2, 123, inet_aton('192.0.10.239'));
 insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp4_subnet_id, ipv4_address) values (unhex('080808080808'), 0, 0, inet_aton('192.0.10.240'));
 insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp4_subnet_id, ipv4_address) values (unhex('090909090909'), 0, 0, inet_aton('192.0.10.241'));
+insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp6_subnet_id) values (unhex('abc76efabdeaae'), 1, 1);
+
+select host_id from hosts where ipv4_address = inet_aton('192.0.10.230') into @selected_host;
+insert into dhcp4_options(code, formatted_value, space, persistent, host_id, scope_id) values(14, '/tmp/dump/dhcp', 'dhcp4', 0, @selected_host, 3);
+insert into dhcp4_options(code, formatted_value, space, persistent, host_id, scope_id) values(3, '10.2.12.1', 'dhcp4', 1, @selected_host, 3);
+insert into dhcp4_options(code, formatted_value, space, persistent, host_id, scope_id) values(20, 'true', 'dhcp4', 0, @selected_host, 3);
+
+select host_id from hosts where hex(dhcp_identifier) = 'abc76efabdeaae' into @selected_host;
+insert into dhcp6_options(code, formatted_value, space, persistent, host_id, scope_id) values(23, '2001:db8:1::1,2001:db8:1::1', 'dhcp6', 1, @selected_host, 3);
+insert into dhcp6_options(code, formatted_value, space, persistent, host_id, scope_id) values(51, 'foo.example.org.', 'dhcp6', 1, @selected_host, 3);
