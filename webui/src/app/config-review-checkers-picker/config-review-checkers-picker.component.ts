d3b38cc56ee093a946cf7e7755cf9fd158c2cacc
Slawek Figiel <slawek@isc.org>
2022-08-24 11:35:07 +0200

    [#610] Add missing comments

--- webui/src/app/config-review-checkers-picker/config-review-checkers-picker.component.ts
+++ webui/src/app/config-review-checkers-picker/config-review-checkers-picker.component.ts
@@ -23,6 +23,12 @@ export class ConfigReviewCheckersPickerComponent {
      */
     @Input() allowInheritState: boolean = false
 
+    /**
+     * It cycles the checker states. The order is enabled - disabled - inherit.
+     * It skips the inherit state if the component is configured to disallow it.
+     * @param state Checker state
+     * @returns Next checker state
+     */
     private _getNextState(state: ConfigChecker.StateEnum): ConfigChecker.StateEnum {
         if (state === ConfigChecker.StateEnum.Inherit) {
             return ConfigChecker.StateEnum.Enabled
@@ -37,6 +43,12 @@ export class ConfigReviewCheckersPickerComponent {
         }
     }
 
+    /**
+     * Returns a fancy icon for the checker trigger. If the trigger is unknown
+     * then returns no icon.
+     * @param trigger The checker trigger
+     * @returns Icon CSS classes
+     */
     getTriggerIcon(trigger: string): string {
         switch (trigger) {
             case "internal":
@@ -52,6 +64,29 @@ export class ConfigReviewCheckersPickerComponent {
         }
     }
 
+    /**
+     * Returns a fancy icon for the checker selector. If the selector is unknown
+     * then returns no icon. 
+     * We don't have specialized icons for our daemons, and FontAwesome doesn't
+     * contain any icons related to DHCP or DNS. But the chips with icons look
+     * better than those without. The dices aren't the first thing you associate
+     * with the Stork-supported daemons, but it has a little sense:
+     * - Kea DHCPv4 is a die with a single 4-dots-side visible  
+     * - Kea DHCPv6 is a die with a single 6-dots-side visible
+     * - Kea D2 daemon is a die with a single 2-dots-side visible
+     * - General DHCP daemon is two dice with a single side visible
+     * - General Kea daemon is a 6-side die in isometric projection
+     * - Kea Control daemon is a 6-side die in isometric projection with
+     *   highlighted one side
+     * - General daemon is a fancy representation of 20-side die
+     * - Bind 9 daemon is a circle with a single dot in the center because it
+     *   has a dot similar to Kea DHCP dice (Bind 9 is a specific daemon), but
+     *   the circle is opposite of a square (DNS is an entirely different thing
+     *   than DHCP). Additionally, the circle is similar to the 20-side dice in
+     *   the same way as the square. (20-side dice is a generalization of daemon).
+     * @param selector The checker selector
+     * @returns Icon CSS classes
+     */
     getSelectorIcon(selector: string): string {
         switch (selector) {
             case "each-daemon":
@@ -75,7 +110,13 @@ export class ConfigReviewCheckersPickerComponent {
         }
     }
 
-    onCheckerPreferenceChange(event, checker: ConfigChecker) {
+    /**
+     * Callback called on change the checker state. It emits an Angular event
+     * with changed checker preference.
+     * @param event Generic change input event
+     * @param checker Affected checker
+     */
+    onCheckerStateChange(event, checker: ConfigChecker) {
         checker.state = this._getNextState(checker.state)
         this.onChangePreference.emit({
             name: checker.name,
