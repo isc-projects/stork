3c8b48fabf00ed5497e4f8ee588f4de75e6e4685
Slawek Figiel <slawek@isc.org>
2022-08-31 15:52:37 +0200

    [#610] More unit tests

--- webui/src/app/config-checker-preference-updater/config-checker-preference-updater.component.spec.ts
+++ webui/src/app/config-checker-preference-updater/config-checker-preference-updater.component.spec.ts
@@ -1,5 +1,6 @@
+import { HttpEvent } from '@angular/common/http'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing'
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { fail } from 'assert'
 import { MessageService } from 'primeng/api'
@@ -8,7 +9,8 @@ import { ChipModule } from 'primeng/chip'
 import { OverlayPanelModule } from 'primeng/overlaypanel'
 import { TableModule } from 'primeng/table'
 import { ToastModule } from 'primeng/toast'
-import { ServicesService } from '../backend'
+import { of, throwError } from 'rxjs'
+import { ConfigChecker, ConfigCheckers, ServicesService } from '../backend'
 import { ConfigCheckerPreferencePickerComponent } from '../config-checker-preference-picker/config-checker-preference-picker.component'
 import { HelpTipComponent } from '../help-tip/help-tip.component'
 
@@ -17,6 +19,7 @@ import { ConfigCheckerPreferenceUpdaterComponent } from './config-checker-prefer
 describe('ConfigCheckerPreferenceUpdaterComponent', () => {
     let component: ConfigCheckerPreferenceUpdaterComponent
     let fixture: ComponentFixture<ConfigCheckerPreferenceUpdaterComponent>
+    let servicesApi: ServicesService
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -32,59 +35,128 @@ describe('ConfigCheckerPreferenceUpdaterComponent', () => {
             declarations: [HelpTipComponent, ConfigCheckerPreferenceUpdaterComponent, ConfigCheckerPreferencePickerComponent],
             providers: [MessageService, ServicesService],
         }).compileComponents()
+
+        servicesApi = TestBed.inject(ServicesService)
     })
 
     beforeEach(() => {
+        spyOn(servicesApi, 'getGlobalConfigCheckers').and.returnValue(
+            of({
+                total: 2,
+                items: [
+                    {
+                        name: 'reservations_out_of_pool',
+                        selectors: ['each-daemon', 'kea-daemon'],
+                        state: ConfigChecker.StateEnum.Disabled,
+                        triggers: ['manual', 'config change'],
+                        globalEnabled: false,
+                    },
+                    {
+                        name: 'subnet_dispensable',
+                        selectors: ['each-daemon'],
+                        state: ConfigChecker.StateEnum.Enabled,
+                        triggers: ['manual', 'config change'],
+                        globalEnabled: true,
+                    },
+                ],
+            } as ConfigCheckers & HttpEvent<ConfigCheckers>)
+        )
+
+        spyOn(servicesApi, 'getDaemonConfigCheckers').and.returnValue(
+            throwError({
+                ok: false,
+                status: 500,
+                statusText: "Error"
+            } as HttpEvent<ConfigCheckers>)
+        )
+
+        spyOn(servicesApi, 'putGlobalConfigCheckerPreferences').and.returnValue(
+            throwError({
+                ok: false,
+                status: 500,
+                statusText: "Error"
+            } as HttpEvent<ConfigCheckers>)
+        )
+
+        spyOn(servicesApi, 'putDaemonConfigCheckerPreferences').and.returnValue(of({
+            total: 0,
+            items: []
+        } as ConfigCheckers & HttpEvent<ConfigCheckers>))
+
         fixture = TestBed.createComponent(ConfigCheckerPreferenceUpdaterComponent)
         component = fixture.componentInstance
-        fixture.detectChanges()
     })
 
     it('should create', () => {
+        fixture.detectChanges()
         expect(component).toBeTruthy()
     })
 
     it('should fetch global preferences for an empty daemon ID', () => {
-        fail('not implemented')
+        component.daemonID = null
+        fixture.detectChanges()
+        expect(servicesApi.getGlobalConfigCheckers).toHaveBeenCalled()
+        expect(servicesApi.getDaemonConfigCheckers).not.toHaveBeenCalled()
     })
 
     it('should fetch daemon preferences for an non-empty daemon ID', () => {
-        fail('not implemented')
+        component.daemonID = 42
+        fixture.detectChanges()
+        expect(servicesApi.getGlobalConfigCheckers).not.toHaveBeenCalled()
+        expect(servicesApi.getDaemonConfigCheckers).toHaveBeenCalled()
     })
 
     it('should handle fetching preferences errors', () => {
-        fail('not implemented')
+        component.daemonID = 42
+        fixture.detectChanges()
+        expect(component.checkers).not.toBeNull()
+        expect(component.checkers).not.toBeUndefined()
+        expect(component.checkers.length).toBe(0)
+        expect(component.loading).toBeFalse()
     })
 
     it('should set non-loading state after fetching preferences', () => {
-        fail('not implemented')
-    })
-
-    it('should unsubscribe on destroy', () => {
-        fail('not implemented')
+        expect(component.loading).toBeTrue()
+        fixture.detectChanges()
+        expect(component.loading).toBeFalse()
     })
 
     it('should set loading state on submit', () => {
         fail('not implemented')
     })
 
-    it('should update the global preferences if the daemon ID is empty', () => {
-        fail('not implemented')
-    })
+    it('should update the global preferences if the daemon ID is empty', fakeAsync(() => {
+        fixture.detectChanges()
+        component.onChangePreferences([{
+            name: "foo",
+            state: 'disabled'
+        }])
+        tick()
+        fixture.detectChanges()
+        expect(servicesApi.putGlobalConfigCheckerPreferences).toHaveBeenCalled()
+        expect(servicesApi.putDaemonConfigCheckerPreferences).not.toHaveBeenCalled()
+        expect(component.loading).toBeFalse()
+    }))
 
-    it('should update the daemon preferences if the daemon ID is not empty', () => {
-        fail('not implemented')
-    })
+    it('should update the daemon preferences if the daemon ID is not empty', fakeAsync(() => {
+        component.daemonID = 42
+        fixture.detectChanges()
+        component.onChangePreferences([{
+            name: "foo",
+            state: 'disabled'
+        }])
+        tick()
+        fixture.detectChanges()
+        expect(servicesApi.putGlobalConfigCheckerPreferences).not.toHaveBeenCalled()
+        expect(servicesApi.putDaemonConfigCheckerPreferences).toHaveBeenCalled()
+        expect(component.loading).toBeFalse()
+    }))
 
     it('should create toast on successful update', () => {
         fail('not implemented')
     })
 
     it('should create toast on failed update', () => {
-        fail('not implemented')
-    })
-
-    it('should set non-loading state after update', () => {
         fail('not implemented')
     })
 })
