0fea0b746460da48c4af2dd7b03e5fe997f11087
Slawek Figiel <slawek@isc.org>
2022-08-31 11:59:22 +0200

    [#610] Next unit tests

--- webui/src/app/config-checker-preference-picker/config-checker-preference-picker.component.spec.ts
+++ webui/src/app/config-checker-preference-picker/config-checker-preference-picker.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { ButtonModule } from 'primeng/button'
 import { ChipModule } from 'primeng/chip'
@@ -28,11 +29,17 @@ describe('ConfigCheckerPreferencePickerComponent', () => {
         expect(component).toBeTruthy()
     })
 
-    it('should handle loading state', () => {
-        fail("not implemented")
+    it('should display a spinner on loading state', () => {
+        component.loading = true
+        fixture.detectChanges()
+
+        // Displays a spinner icon.
+        const icon = fixture.debugElement.query(By.css('.pi-spinner'))
+        expect(icon).not.toBeNull()
+        expect(icon.classes["pi-spin"]).toBeDefined()
     })
 
-    it('should handle an empty state and display no buttons', async () => {
+    it('should display a message on empty state', async () => {
         component.checkers = []
         component.loading = false
         fixture.detectChanges()
@@ -41,11 +48,78 @@ describe('ConfigCheckerPreferencePickerComponent', () => {
     })
 
     it('should display full layout by default', () => {
-        fail("not implemented")
+        expect(component.minimal).toBeFalse()
     })
 
     it('should display only necessary columns in a minimal layout', () => {
-        fail("not implemented")
+        component.minimal = true
+        component.checkers = [
+            {
+                name: "n1",
+                selectors: ["s1", "s2"],
+                globalEnabled: true,
+                state: 'disabled',
+                triggers: ["t1", "t2", "t3"]
+            },
+            {
+                name: "n2",
+                selectors: ["s3"],
+                globalEnabled: false,
+                state: 'enabled',
+                triggers: ["t4"]
+            }
+        ]
+
+        fixture.detectChanges()
+        
+        const hiddenClasses = [
+            ".picker__description-column",
+            ".picker__selector-column",
+            ".picker__trigger-column"
+        ]
+
+        for (const hiddenClass of hiddenClasses) {
+            const elements = fixture.debugElement.queryAll(By.css(hiddenClass))
+            // Header and content
+            expect(elements.length).toBe(3)
+            for (const element of elements) {
+                expect(element.nativeElement.clientWidth).toBe(0)
+                expect(element.nativeElement.clientHeight).toBe(0)
+            }
+        }
+
+        // Find all headers and cells.
+        const headers = fixture.debugElement.queryAll(By.css("th"))
+        const cells = fixture.debugElement.queryAll(By.css("td"))
+        const elements = [...headers, ...cells]
+            
+        // Filter out elements containing the hidden classes.
+        const candidates = []
+        for (const element of elements) {
+            let isHidden = false
+            for (const [elementClass, enabled] of Object.entries(element.classes)) {
+                if (!enabled) {
+                    continue
+                }
+                if (hiddenClasses.includes('.' + elementClass)) {
+                    isHidden = true
+                    break
+                }
+            }
+            if (!isHidden) {
+                candidates.push(element)
+            }
+        }
+
+        // Two visible columns for header and content rows.
+        expect(candidates.length).toBe(2*(1+2))
+
+        // Check if headers are visible
+        for (const element of candidates) {
+            expect(element.nativeElement.clientWidth).not.toBe(0)
+            expect(element.nativeElement.clientHeight).not.toBe(0)
+        }
+
     })
 
     it('should correctly cycle the checker state', () => {
