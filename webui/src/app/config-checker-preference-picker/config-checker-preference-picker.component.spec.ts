df753c1459a28e1fae23a6cbd301bc5f2885a828
Slawek Figiel <slawek@isc.org>
2022-08-31 12:59:27 +0200

    [#610] Next unit tests

--- webui/src/app/config-checker-preference-picker/config-checker-preference-picker.component.spec.ts
+++ webui/src/app/config-checker-preference-picker/config-checker-preference-picker.component.spec.ts
@@ -5,6 +5,7 @@ import { ButtonModule } from 'primeng/button'
 import { ChipModule } from 'primeng/chip'
 import { OverlayPanelModule } from 'primeng/overlaypanel'
 import { TableModule } from 'primeng/table'
+import { ConfigChecker } from '../backend'
 import { HelpTipComponent } from '../help-tip/help-tip.component'
 import { ConfigCheckerPreferencePickerComponent } from './config-checker-preference-picker.component'
 
@@ -114,7 +115,7 @@ describe('ConfigCheckerPreferencePickerComponent', () => {
         // Two visible columns for header and content rows.
         expect(candidates.length).toBe(2*(1+2))
 
-        // Check if headers are visible
+        // Check if the elements are visible
         for (const element of candidates) {
             expect(element.nativeElement.clientWidth).not.toBe(0)
             expect(element.nativeElement.clientHeight).not.toBe(0)
@@ -123,23 +124,136 @@ describe('ConfigCheckerPreferencePickerComponent', () => {
     })
 
     it('should correctly cycle the checker state', () => {
-        fail("not implemented")
+        const checker: ConfigChecker = {
+            name: 'foo',
+            state: 'disabled'
+        }
+
+        component.checkers = [
+            checker    
+        ]
+        component.allowInheritState = true
+
+        component.onCheckerStateChanged(checker)
+        expect(component.getActualState(checker)).toBe('inherit')
+        component.onCheckerStateChanged(checker)
+        expect(component.getActualState(checker)).toBe('enabled')
+        component.onCheckerStateChanged(checker)
+        expect(component.getActualState(checker)).toBe('disabled')
+
+        component.allowInheritState = false
+
+        component.onCheckerStateChanged(checker)
+        expect(component.getActualState(checker)).toBe('enabled')
+        component.onCheckerStateChanged(checker)
+        expect(component.getActualState(checker)).toBe('disabled')
     })
 
     it('should display the checker description', () => {
-        fail("not implemented")
+        component.checkers = [
+            {
+                globalEnabled: true,
+                name: "host_cmds_presence",
+                selectors: [],
+                state: 'enabled',
+                triggers: []
+            }
+        ]
+        fixture.detectChanges()
+
+        const element = fixture.debugElement.query(By.css("td.picker__description-column"))
+        expect(element).not.toBeNull()
+        const content = (element.nativeElement as HTMLElement).innerText
+        expect(content).toContain('The checker verifying if the host_cmds hooks library is loaded')
     })
 
     it('should display the checker selectors', () => {
-        fail("not implemented")
+        component.checkers = [
+            {
+                globalEnabled: true,
+                name: "foo",
+                selectors: ['each-daemon', 'foobar'],
+                state: 'enabled',
+                triggers: []
+            }
+        ]
+        fixture.detectChanges()
+
+        const element = fixture.debugElement.query(By.css("td.picker__selector-column"))
+        expect(element).not.toBeNull()
+        const content = (element.nativeElement as HTMLElement).innerText
+        expect(content).toContain('each-daemon')
+        expect(content).toContain('foobar')
     })
 
     it('should display the checker triggers', () => {
-        fail("not implemented")
+        component.checkers = [
+            {
+                globalEnabled: true,
+                name: "foo",
+                selectors: [],
+                state: 'enabled',
+                triggers: ['host reservations change', 'barfoo']
+            }
+        ]
+        fixture.detectChanges()
+
+        const element = fixture.debugElement.query(By.css("td.picker__trigger-column"))
+        expect(element).not.toBeNull()
+        const content = (element.nativeElement as HTMLElement).innerText
+        expect(content).toContain('host reservations change')
+        expect(content).toContain('barfoo')
     })
 
     it('should activate the submit button only if any changes were provided', () => {
-        fail("not implemented")
+        component.checkers = [
+            {
+                globalEnabled: true,
+                name: "foo",
+                selectors: [],
+                state: 'enabled',
+                triggers: []
+            }
+        ]
+        component.allowInheritState = true
+        fixture.detectChanges()
+
+        const checker = component.checkers[0]
+        let submitButton = fixture.debugElement.query(By.css('button[label=Submit]'))
+        expect(submitButton).not.toBeNull()
+        
+        // No changes
+        expect(component.hasChanges).toBeFalse()
+        expect(submitButton.attributes).toEqual(jasmine.objectContaining({
+            disabled: ''
+        }))
+
+        // Significant changes.
+        // Disabled state.
+        component.onCheckerStateChanged(checker)
+        fixture.detectChanges()
+        expect(component.hasChanges).toBeTrue()
+        submitButton = fixture.debugElement.query(By.css('button[label=Submit]'))
+        expect(submitButton.attributes).not.toEqual(jasmine.objectContaining({
+            disabled: ''
+        }))
+        // Inherit state.
+        component.onCheckerStateChanged(checker)
+        fixture.detectChanges()
+        expect(component.hasChanges).toBeTrue()
+        submitButton = fixture.debugElement.query(By.css('button[label=Submit]'))
+        expect(submitButton.attributes).not.toEqual(jasmine.objectContaining({
+            disabled: ''
+        }))
+
+        // Revert changes.
+        component.onCheckerStateChanged(checker)
+        fixture.detectChanges()
+        expect(component.hasChanges).toBeFalse()
+        submitButton = fixture.debugElement.query(By.css('button[label=Submit]'))
+        expect(submitButton.attributes).toEqual(jasmine.objectContaining({
+            disabled: ''
+        }))
     })
 
     it('should detect reverting changes', () => {
