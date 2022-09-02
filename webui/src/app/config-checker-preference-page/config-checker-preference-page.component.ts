2aceeffd3bc1b9c0228436ef18ef66e052999d4a
Slawek Figiel <slawek@isc.org>
2022-09-02 18:17:00 +0200

    [#610] Add missing comment

--- webui/src/app/config-checker-preference-page/config-checker-preference-page.component.ts
+++ webui/src/app/config-checker-preference-page/config-checker-preference-page.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core'
 
+/**
+ * Displays the global settings page of the configuration review checkers.
+ * It contains the checkers' picker and some help text.
+ */
 @Component({
     selector: 'app-config-checker-preference-page',
     templateUrl: './config-checker-preference-page.component.html',
