0a1e11548069eb0c21d8994fc332b4fcea88cfea
Slawek Figiel <slawek@isc.org>
2022-10-13 14:40:30 +0200

    [#779] Add more unit tests

--- backend/hooksutil/executor_test.go
+++ backend/hooksutil/executor_test.go
@@ -1,1 +1,42 @@
 package hooksutil
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type mockCallout interface{}
+
+// Test that the hook executor is constructed properly.
+func TestNewHookExecutor(t *testing.T) {
+	// Arrange & Act
+	emptyExecutor := NewHookExecutor([]reflect.Type{})
+	nilExecutor := NewHookExecutor(nil)
+	executor := NewHookExecutor([]reflect.Type{
+		reflect.TypeOf((*mockCallout)(nil)).Elem(),
+	})
+
+	// Assert
+	require.NotNil(t, emptyExecutor)
+	require.NotNil(t, nilExecutor)
+	require.NotNil(t, executor)
+
+	require.Contains(t, executor.registeredCallouts, reflect.TypeOf((*mockCallout)(nil)).Elem())
+}
+
+// Test that the hook executor constructor panics on an invalid type (it's a
+// programming bug).
+func TestNewHookExecutorInvalidType(t *testing.T) {
+	// Arrange
+	// Missing .Elem() call
+	invalidType := reflect.TypeOf((*mockCallout)(nil))
+
+	// Assert
+	require.Panics(t, func() {
+		// Act
+		_ = NewHookExecutor([]reflect.Type{invalidType})
+	})
+
+}
