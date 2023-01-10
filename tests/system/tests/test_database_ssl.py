0c43d00197eceb6cf33a6ad39cab98c736daf5a5
Slawek Figiel <slawek@isc.org>
2023-01-10 17:37:21 +0100

    [#934] Disable not working system test

--- tests/system/tests/test_database_ssl.py
+++ tests/system/tests/test_database_ssl.py
@@ -1,3 +1,5 @@
+import pytest
+
 from core.wrappers import Server
 from core.fixtures import server_parametrize
 
@@ -7,6 +9,7 @@ def test_server_enable_sslmode_require(server_service: Server):
     server_service.log_in_as_admin()
 
 
+@pytest.mark.skip("Missing trusted SSL certificates")
 @server_parametrize("server-db-ssl-verify-ca")
 def test_server_enable_sslmode_verify_ca(server_service: Server):
     server_service.log_in_as_admin()
