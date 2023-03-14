7977e21cca78aa433ae149f4940680bbabbaec10
Slawek Figiel <slawek@isc.org>
2023-03-14 17:36:44 +0100

    [#1004] Allow access for users with no groups

--- backend/server/auth/auth.go
+++ backend/server/auth/auth.go
@@ -1,9 +1,10 @@
 package auth
 
 import (
+	"fmt"
 	"net/http"
-	"regexp"
-	"strconv"
+	"path"
+	"strings"
 
 	dbmodel "isc.org/stork/server/database/model"
 )
@@ -13,10 +14,9 @@ import (
 // resources. The admin-user can access all resources except those related
 // to users management.
 func Authorize(user *dbmodel.SystemUser, req *http.Request) (ok bool, err error) {
-	// If there is no user (possibly the user has not signed in), the user
-	// does not belong to any groups or the request is nil, reject access to
-	// the resource.
-	if user == nil || len(user.Groups) == 0 || req == nil {
+	// If there is no user (possibly the user has not signed in) or the
+	// request is nil, reject access to the resource.
+	if user == nil || req == nil {
 		return false, nil
 	}
 
@@ -25,12 +25,20 @@ func Authorize(user *dbmodel.SystemUser, req *http.Request) (ok bool, err error)
 		return true, nil
 	}
 
-	// If the user does not belong to the super-admin group and trying to
-	// access the user specific information, check if the data the user
-	// is trying to access belong to this user. If not, reject access.
-	if ok, _ := regexp.Match(`users/{0,}`, []byte(req.URL.Path)); ok {
-		ok, _ := regexp.Match(`users/{1,}`+strconv.Itoa(user.ID)+`/*`, []byte(req.URL.Path))
-		return ok, nil
+	urlPath := req.URL.Path
+	urlPath = path.Clean(urlPath)
+	if !strings.HasSuffix(urlPath, "/") {
+		urlPath += "/"
+	}
+
+	if strings.HasPrefix(urlPath, "/api/users/") {
+		// If the user does not belong to the super-admin group and trying to
+		// access the user specific information, check if the data the user
+		// is trying to access belong to this user. If not, reject access.
+		return strings.HasPrefix(urlPath, fmt.Sprintf("/api/users/%d/", user.ID)), nil
+	} else if strings.HasPrefix(urlPath, "/api/sessions/") && req.Method == "DELETE" {
+		// Log out is available for all users.
+		return true, nil
 	}
 
 	// All other resources can be accessed by the admin user.
