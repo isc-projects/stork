591937cd71b297ae11b746cdecbfa1be5432ded5
Slawek Figiel <slawek@isc.org>
2023-03-14 17:25:40 +0100

    [#1004] Add test that doesn't pass

--- backend/server/auth/auth_test.go
+++ backend/server/auth/auth_test.go
@@ -11,7 +11,7 @@ import (
 
 // Helper function checking if the user belonging to the specified group
 // has access to the resource.
-func authorizeAccept(t *testing.T, groupID int, path string) bool {
+func authorizeAccept(t *testing.T, groupID int, path, method string) bool {
 	// Create user with ID 5 and specified group id if the group id is
 	// positive.
 	user := &dbmodel.SystemUser{
@@ -26,7 +26,7 @@ func authorizeAccept(t *testing.T, groupID int, path string) bool {
 	}
 
 	// Create request with the specified path and authorize.
-	req, _ := http.NewRequestWithContext(context.Background(), "GET", "http://example.org/api"+path, nil)
+	req, _ := http.NewRequestWithContext(context.Background(), method, "http://example.org/api"+path, nil)
 	ok, err := Authorize(user, req)
 	require.NoError(t, err)
 
@@ -37,30 +37,33 @@ func authorizeAccept(t *testing.T, groupID int, path string) bool {
 // has appropriate access privileges.
 func TestAuthorize(t *testing.T) {
 	// admin group have limited access to the users' management
-	require.False(t, authorizeAccept(t, 2, "/users?start=0&limit=10"))
-	require.False(t, authorizeAccept(t, 2, "/users/list"))
-	require.False(t, authorizeAccept(t, 2, "/users/4/password"))
-	require.False(t, authorizeAccept(t, 2, "/users//4/password/"))
-	require.True(t, authorizeAccept(t, 2, "/users/5"))
-	require.True(t, authorizeAccept(t, 2, "/users/5/password"))
-	require.True(t, authorizeAccept(t, 2, "/users//5//password"))
+	require.False(t, authorizeAccept(t, 2, "/users?start=0&limit=10", "GET"))
+	require.False(t, authorizeAccept(t, 2, "/users/list", "GET"))
+	require.False(t, authorizeAccept(t, 2, "/users/4/password", "GET"))
+	require.False(t, authorizeAccept(t, 2, "/users//4/password/", "GET"))
+	require.True(t, authorizeAccept(t, 2, "/users/5", "GET"))
+	require.True(t, authorizeAccept(t, 2, "/users/5/password", "GET"))
+	require.True(t, authorizeAccept(t, 2, "/users//5//password", "GET"))
 
 	// super-admin has no such restrictions
-	require.True(t, authorizeAccept(t, 1, "/users?start=0&limit=10"))
-	require.True(t, authorizeAccept(t, 1, "/users/list"))
-	require.True(t, authorizeAccept(t, 1, "/users/4/password"))
-	require.True(t, authorizeAccept(t, 1, "/users//4/password/"))
-	require.True(t, authorizeAccept(t, 1, "/users/5"))
-	require.True(t, authorizeAccept(t, 1, "/users/5/password"))
-	require.True(t, authorizeAccept(t, 1, "/users//5//password"))
+	require.True(t, authorizeAccept(t, 1, "/users?start=0&limit=10", "GET"))
+	require.True(t, authorizeAccept(t, 1, "/users/list", "GET"))
+	require.True(t, authorizeAccept(t, 1, "/users/4/password", "GET"))
+	require.True(t, authorizeAccept(t, 1, "/users//4/password/", "GET"))
+	require.True(t, authorizeAccept(t, 1, "/users/5", "GET"))
+	require.True(t, authorizeAccept(t, 1, "/users/5/password", "GET"))
+	require.True(t, authorizeAccept(t, 1, "/users//5//password", "GET"))
 
 	// admin group have no restriction on machines
-	require.True(t, authorizeAccept(t, 2, "/machines/1/"))
+	require.True(t, authorizeAccept(t, 2, "/machines/1/", "GET"))
 
 	// but someone who belongs to no groups would not be able
 	// to access machines
-	require.False(t, authorizeAccept(t, 0, "/machines/1/"))
+	require.False(t, authorizeAccept(t, 0, "/machines/1/", "GET"))
 
 	// the same in case of someone belonging to non existing group
-	require.False(t, authorizeAccept(t, 3, "/machines/1/"))
+	require.False(t, authorizeAccept(t, 3, "/machines/1/", "GET"))
+
+	// Test that users belonging to none group have privilege to log out.
+	require.True(t, authorizeAccept(t, 3, "/sessions", "DELETE"))
 }
