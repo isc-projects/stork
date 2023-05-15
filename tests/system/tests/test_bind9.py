1b12acd149669e8b852bf489f1b4a7fa2cfdf78f
Slawek Figiel <slawek@isc.org>
2023-05-15 14:06:11 +0200

    [#1031] Add test that doesn't passes

--- tests/system/tests/test_bind9.py
+++ tests/system/tests/test_bind9.py
@@ -32,7 +32,6 @@ def test_bind9(server_service: Server, bind9_service: Bind9):
 def test_bind9_rndc(server_service: Server, bind9_service: Bind9):
     """Check if Stork Agent can communicate with BIND 9 while RNDC
     authentication is enabled."""
-
     server_service.log_in_as_admin()
     server_service.authorize_all_machines()
     state, *_ = server_service.wait_for_next_machine_states()
@@ -46,3 +45,34 @@ def test_bind9_rndc(server_service: Server, bind9_service: Bind9):
     metrics = bind9_service.read_prometheus_metrics()
     assert metrics is not None
     assert len(list(metrics)) > 0
+
+
+@bind9_parametrize("agent-bind9-package")
+def test_bind9_package(server_service: Server, bind9_service: Bind9):
+    """Check if Stork Agent can communicate with BIND 9 using an initial
+    configuration installed from BIND9 binary package."""
+    server_service.log_in_as_admin()
+    server_service.authorize_all_machines()
+    state, *_ = server_service.wait_for_next_machine_states()
+
+    assert len(state['apps']) == 1
+    app = state['apps'][0]
+    assert app['type'] == "bind9"
+    assert len(app['access_points']) == 1  # Missing statistics
+    assert app['access_points'][0]['address'] == '127.0.0.1'
+
+
+@bind9_parametrize("agent-bind9-rndc-custom")
+def test_bind9_rndc_custom(server_service: Server, bind9_service: Bind9):
+    """Check if Stork Agent can communicate with BIND 9 when RNDC uses a custom
+    configuration file and the required key isn't specified in the rndc.key
+    file."""
+    server_service.log_in_as_admin()
+    server_service.authorize_all_machines()
+    state, *_ = server_service.wait_for_next_machine_states()
+
+    assert len(state['apps']) == 1
+    app = state['apps'][0]
+    assert app['type'] == "bind9"
+    assert len(app['access_points']) == 2
+    assert app['access_points'][0]['address'] == '127.0.0.1'
