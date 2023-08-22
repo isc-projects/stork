4f60148c3f561d78b43ad5128a5b3f493af548f2
Slawek Figiel <slawek@isc.org>
2023-08-22 12:35:20 +0200

    [#977] Extend Kea configuration in Docker

--- docker/config/agent-kea-premium-two/init_mysql_query.sql
+++ docker/config/agent-kea-premium-two/init_mysql_query.sql
@@ -10,6 +10,7 @@ insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp4_subnet_id, ipv4_a
 insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp4_subnet_id, ipv4_address) values (unhex('0a0a0a0a'), 2, 123, inet_aton('192.110.111.239'));
 insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp4_subnet_id, ipv4_address) values (unhex('080808080808'), 0, 0, inet_aton('192.110.111.240'));
 insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp4_subnet_id, ipv4_address) values (unhex('090909090909'), 0, 0, inet_aton('192.110.111.241'));
+insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp4_subnet_id, ipv4_address) values (unhex('0a0a0a0a0a0a'), 0, 0, inet_aton('192.110.111.242'));
 insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp6_subnet_id) values (unhex('abc76efabdeaae'), 1, 1);
 insert into hosts(dhcp_identifier, dhcp_identifier_type, dhcp6_subnet_id) values (unhex('13342ab5efaac4'), 1, 1);
 
@@ -17,6 +18,9 @@ select host_id from hosts where ipv4_address = inet_aton('192.110.111.230') into
 insert into dhcp4_options(code, formatted_value, space, persistent, host_id, scope_id) values(14, '/tmp/dump/dhcp', 'dhcp4', 0, @selected_host, 3);
 insert into dhcp4_options(code, formatted_value, space, persistent, host_id, scope_id) values(3, '10.2.12.1', 'dhcp4', 1, @selected_host, 3);
 insert into dhcp4_options(code, formatted_value, space, persistent, host_id, scope_id) values(20, 'true', 'dhcp4', 0, @selected_host, 3);
+
+select host_id from hosts where ipv4_address = inet_aton('192.110.111.242') into @selected_host;
+insert into dhcp4_options(code, formatted_value, space, persistent, host_id, scope_id) values(20, 'false', 'dhcp4', 0, @selected_host, 3);
 
 select host_id from hosts where hex(dhcp_identifier) = 'abc76efabdeaae' into @selected_host;
 insert into dhcp6_options(code, formatted_value, space, persistent, host_id, scope_id) values(23, '2001:db8:1::1,2001:db8:1::1', 'dhcp6', 1, @selected_host, 3);
