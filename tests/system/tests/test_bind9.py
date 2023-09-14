2dff309ae7c106916fa13b3169365735cb3a83cb
Slawek Figiel <slawek@isc.org>
2023-09-14 13:27:32 +0200

    [#974] Add test that not passing

--- tests/system/tests/test_bind9.py
+++ tests/system/tests/test_bind9.py
@@ -74,3 +74,15 @@ def test_bind9_rndc_custom(server_service: Server, bind9_service: Bind9):
     assert app["type"] == "bind9"
     assert len(app["access_points"]) == 2
     assert app["access_points"][0]["address"] == "127.0.0.1"
+
+
+@bind9_parametrize("agent-bind9-chroot")
+def test_bind9_chroot(server_service: Server, bind9_service: Bind9):
+    """Check if Stork Agent can monitor BIND 9 running in the chroot
+    environment."""
+    server_service.log_in_as_admin()
+    server_service.authorize_all_machines()
+    state, *_ = server_service.wait_for_next_machine_states()
+    assert len(state["apps"]) == 1
+    app = state["apps"][0]
+    assert app["type"] == "bind9"
