c318c257ce997adade45b4a2f1455cf4f88d8760
Slawek Figiel <slawek@isc.org>
2024-03-07 11:12:13 +0100

    [#1193] BigInt marshaller

---
+++ backend/util/bigintjson.go
@@ -0,0 +1,41 @@
+package storkutil
+
+import (
+	"math/big"
+
+	"github.com/pkg/errors"
+)
+
+// A wrapper for big.Int that can be marshaled and unmarshaled to JSON. It is
+// intended to handle big numbers from Kea API without losing precision.
+// It doesn't prepend the `n` literal as it isn't supported by the Kea parser
+// and JSON standard.
+//
+// Warning: This type SHOULD NOT be used to serialize big numbers returned by
+// the Stork API. If the serialized value by this method is processed by the
+// JSON-compliant parser, it is cast to a float64, so the precision is lost.
+//
+// See: https://stackoverflow.com/a/53991836
+type BigIntJSON struct {
+	big.Int
+}
+
+// Serializes the big integer to JSON as a numeric literal.
+func (b BigIntJSON) MarshalJSON() ([]byte, error) {
+	return []byte(b.String()), nil
+}
+
+// Deserializes the big integer from JSON numeric literal without casting it
+// intermediate to float64.
+func (b *BigIntJSON) UnmarshalJSON(p []byte) error {
+	if string(p) == "null" {
+		return nil
+	}
+	var z big.Int
+	_, ok := z.SetString(string(p), 10)
+	if !ok {
+		return errors.Errorf("not a valid big integer: %s", p)
+	}
+	b.Int = z
+	return nil
+}
---
+++ backend/util/bigintjson_test.go
@@ -0,0 +1,108 @@
+package storkutil
+
+import (
+	"encoding/json"
+	"math"
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Test that the big integer is marshaled to JSON as a numeric literal.
+func TestBigIntJSONMarshal(t *testing.T) {
+	// Arrange
+	bigInts := []*big.Int{
+		// Small integers.
+		big.NewInt(0),
+		big.NewInt(42),
+		big.NewInt(-1),
+		// Max standard integer value.
+		big.NewInt(0).SetUint64(math.MaxUint64),
+		// Exceed the uint64 range.
+		big.NewInt(0).Add(big.NewInt(0).SetUint64(math.MaxUint64), big.NewInt(1)),
+		// Really big number.
+		big.NewInt(0).Add(
+			big.NewInt(0).Add(
+				big.NewInt(0).SetUint64(math.MaxUint64),
+				big.NewInt(0).SetUint64(math.MaxUint64),
+			),
+			big.NewInt(0).Add(
+				big.NewInt(0).SetUint64(math.MaxUint64),
+				big.NewInt(0).SetUint64(math.MaxUint64),
+			),
+		),
+	}
+
+	strings := []string{
+		"0",
+		"42",
+		"-1",
+		"18446744073709551615",
+		"18446744073709551616",
+		"73786976294838206460",
+	}
+
+	for i := range bigInts {
+		t.Run(strings[i], func(t *testing.T) {
+			bigInt := BigIntJSON{*bigInts[i]}
+
+			// Act
+			bytes, err := json.Marshal(bigInt)
+
+			// Assert
+			require.NoError(t, err)
+			require.Equal(t, strings[i], string(bytes))
+		})
+	}
+}
+
+// Test that the big integer is unmarshaled from JSON numeric literal without
+// casting it intermediate to float64 and losing precision.
+func TestBigIntJSONUnmarshal(t *testing.T) {
+	// Arrange
+	bigInts := []*big.Int{
+		// Small integers.
+		big.NewInt(0),
+		big.NewInt(42),
+		big.NewInt(-1),
+		// Max standard integer value.
+		big.NewInt(0).SetUint64(math.MaxUint64),
+		// Exceed the uint64 range.
+		big.NewInt(0).Add(big.NewInt(0).SetUint64(math.MaxUint64), big.NewInt(1)),
+		// Really big number.
+		big.NewInt(0).Add(
+			big.NewInt(0).Add(
+				big.NewInt(0).SetUint64(math.MaxUint64),
+				big.NewInt(0).SetUint64(math.MaxUint64),
+			),
+			big.NewInt(0).Add(
+				big.NewInt(0).SetUint64(math.MaxUint64),
+				big.NewInt(0).SetUint64(math.MaxUint64),
+			),
+		),
+	}
+
+	strings := []string{
+		"0",
+		"42",
+		"-1",
+		"18446744073709551615",
+		"18446744073709551616",
+		"73786976294838206460",
+	}
+
+	for i := range bigInts {
+		t.Run(strings[i], func(t *testing.T) {
+			bytes := []byte(strings[i])
+
+			// Act
+			var bigInt BigIntJSON
+			err := json.Unmarshal(bytes, &bigInt)
+
+			// Assert
+			require.NoError(t, err)
+			require.Equal(t, bigInts[i], &bigInt.Int)
+		})
+	}
+}
