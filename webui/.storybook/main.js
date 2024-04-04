caac7e71b886cf0a4625b8ba401f17075b7a7434
Slawek Figiel <slawek@isc.org>
2024-04-04 13:34:04 +0200

    [#1353] Upgrade Storybook

--- webui/.storybook/main.js
+++ webui/.storybook/main.js
@@ -1,5 +1,5 @@
 const config = {
-    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 
     addons: ['@storybook/addon-links', '@storybook/addon-interactions', 'storybook-addon-mock'],
 
--- webui/.storybook/preview.js
+++ webui/.storybook/preview.js
@@ -4,7 +4,6 @@ setCompodocJson(docJson)
 
 const preview = {
     parameters: {
-        actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
             matchers: {
                 color: /(background|color)$/i,
