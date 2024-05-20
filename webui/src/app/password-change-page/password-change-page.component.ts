79cb33bc4bb64378e44845c39ba50567bf3beaff
Piotrek Zadroga <piotrek@isc.org>
2024-05-20 11:29:18 +0200

    [#1275] maxLen validation in password-change-page
    - also displaying of the validation feedback reworked

--- webui/src/app/password-change-page/password-change-page.component.ts
+++ webui/src/app/password-change-page/password-change-page.component.ts
@@ -21,6 +21,11 @@ export class PasswordChangePageComponent implements OnInit {
 
     passwordChangeForm: UntypedFormGroup
 
+    /**
+     * Max input length allowed to be provided by a user. This is used in form validation.
+     */
+    maxInputLen = 120
+
     constructor(
         private formBuilder: UntypedFormBuilder,
         private usersApi: UsersService,
@@ -31,9 +36,12 @@ export class PasswordChangePageComponent implements OnInit {
     ngOnInit() {
         this.passwordChangeForm = this.formBuilder.group(
             {
-                oldPassword: ['', Validators.required],
-                newPassword: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
-                confirmPassword: ['', Validators.required],
+                oldPassword: ['', [Validators.required, Validators.maxLength(this.maxInputLen)]],
+                newPassword: [
+                    '',
+                    [Validators.required, Validators.minLength(8), Validators.maxLength(this.maxInputLen)],
+                ],
+                confirmPassword: ['', [Validators.required, Validators.maxLength(this.maxInputLen)]],
             },
             { validators: [matchPasswords('newPassword', 'confirmPassword')] }
         )
@@ -90,5 +98,56 @@ export class PasswordChangePageComponent implements OnInit {
                 })
             }
         )
+    }
+
+    /**
+     * Utility function which checks if feedback for given FormControl shall be displayed.
+     *
+     * @param name FormControl name for which the check is done
+     * @param comparePasswords when true, passwords mismatch is also checked; defaults to false
+     */
+    isFeedbackNeeded(name: string, comparePasswords = false): boolean {
+        return (
+            (this.passwordChangeForm.get(name).invalid ||
+                (comparePasswords && this.passwordChangeForm.errors?.['mismatchedPasswords'])) &&
+            (this.passwordChangeForm.get(name).dirty || this.passwordChangeForm.get(name).touched)
+        )
+    }
+
+    /**
+     * Utility function which builds feedback message when form field validation failed.
+     *
+     * @param name FormControl name for which the feedback is to be generated
+     * @param formatFeedback optional feedback message when pattern validation failed
+     * @param comparePasswords when true, feedback about passwords mismatch is also appended; defaults to false
+     */
+    buildFeedbackMessage(name: string, formatFeedback?: string, comparePasswords = false): string | null {
+        const errors: string[] = []
+
+        if (this.passwordChangeForm.get(name).errors?.['required']) {
+            errors.push('This field is required.')
+        }
+
+        if (this.passwordChangeForm.get(name).errors?.['minlength']) {
+            errors.push('This field value is too short.')
+        }
+
+        if (this.passwordChangeForm.get(name).errors?.['maxlength']) {
+            errors.push('This field value is too long.')
+        }
+
+        if (this.passwordChangeForm.get(name).errors?.['email']) {
+            errors.push('Email is incorrect.')
+        }
+
+        if (this.passwordChangeForm.get(name).errors?.['pattern']) {
+            errors.push(formatFeedback ?? 'This field value is wrong.')
+        }
+
+        if (comparePasswords && this.passwordChangeForm.errors?.['mismatchedPasswords']) {
+            errors.push('Passwords must match.')
+        }
+
+        return errors.join(' ')
     }
 }
