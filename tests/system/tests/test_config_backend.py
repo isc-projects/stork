2ecc6e9b27175ab89ebe4ee6620c5428d39836ec
Slawek Figiel <slawek@isc.org>
2024-06-13 13:33:41 +0200

    [#1350] Remove RADIUS stuff

--- tests/system/tests/test_config_backend.py
+++ tests/system/tests/test_config_backend.py
@@ -1,4 +1,4 @@
-from core.wrappers import Server, Kea, Perfdhcp
+from core.wrappers import Server, Kea
 from core.fixtures import kea_parametrize
 
 
@@ -119,46 +119,3 @@ def test_update_host_reservation(kea_service: Kea, server_service: Server):
     identifier = host.host_identifiers[0]
     assert identifier.id_type == "client-id"
     assert identifier.id_hex_value == "06:05:04:03:02:01"
-
-
-@kea_parametrize("agent-kea-premium-radius")
-def test_get_host_reservations_from_radius(
-    kea_service: Kea, server_service: Server, perfdhcp_service: Perfdhcp
-):
-    """
-    Tests that the RADIUS hook configured to an accounting doesn't cause to
-    generate false disconnect events and doesn't interrupt fetching
-    reservations from other host backends.
-    """
-    server_service.log_in_as_admin()
-    server_service.authorize_all_machines()
-
-    # For Kea below 2.5 RADIUS backend is initialized on the first call of the
-    # "selectSubnet" callout. Perfdhcp generates the network traffic that
-    # triggers this call. It doesn't matter for Kea 2.5+.
-    perfdhcp_service.generate_ipv4_traffic(
-        ip_address=kea_service.get_internal_ip_address("subnet_00", family=4),
-        mac_prefix="00:00",
-    )
-
-    # Waits for send the "reservation-get-page" command to Kea.
-    server_service.wait_for_host_reservation_pulling()
-
-    # There should be no communication failed events.
-    events = server_service.list_events("dhcp4")
-    assert len(events.items) > 0
-    for event in events.items:
-        text: str = event.text.strip()
-        assert not (
-            text.startswith("Communication with <daemon") and text.endswith("failed")
-        )
-
-    # Fetches the host reservations properly.
-    hosts = server_service.list_hosts("192.0.2.42")
-    assert hosts is not None
-    assert len(hosts.items) == 1
-    host = hosts.items[0]
-    local_hosts = host.local_hosts
-    assert len(local_hosts) == 1
-    local_host = local_hosts[0]
-    assert local_host.data_source == "api"
