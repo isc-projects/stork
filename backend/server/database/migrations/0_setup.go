59f18f5b0762e5b9e5dad1144d449b3ffde33c49
Slawek Figiel <slawek@isc.org>
2024-07-11 10:57:54 +0200

    [#1439] Disable autodiscovery

---
+++ backend/server/database/migrations/0_setup.go
@@ -0,0 +1,16 @@
+package dbmigs
+
+import (
+	"github.com/go-pg/migrations/v8"
+)
+
+// This init function is called first in this package because the filename
+// is first in the lexical order. It should be used to configure the migration
+// framework.
+func init() {
+	// Disable autodiscover for SQL migration files. It prevents the migration
+	// framework from looking for Stork source files in production
+	// environments. This is necessary because the look-up process can fail
+	// due to unsufficient permissions to read particular directories.
+	migrations.DefaultCollection.DisableSQLAutodiscover(true)
+}
