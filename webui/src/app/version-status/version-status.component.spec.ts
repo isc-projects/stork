b5cf69a7392bde31f908a93302a0f2576750e61c
Piotrek Zadroga <piotrek@isc.org>
2024-10-11 15:02:41 +0200

    [#222] version-status UTs

--- webui/src/app/version-status/version-status.component.spec.ts
+++ webui/src/app/version-status/version-status.component.spec.ts
@@ -6,13 +6,17 @@ import { MessageService } from 'primeng/api'
 import { TooltipModule } from 'primeng/tooltip'
 import { Severity, VersionService } from '../version.service'
 import { of } from 'rxjs'
+import { MessagesModule } from 'primeng/messages'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 describe('VersionStatusComponent', () => {
     let component: VersionStatusComponent
     let fixture: ComponentFixture<VersionStatusComponent>
     let versionService: VersionService
+    let messageService: MessageService
     let getCurrentDataSpy: jasmine.Spy<any>
-    let getSoftwareVersionFeedback: jasmine.Spy<any>
+    let getSoftwareVersionFeedbackSpy: jasmine.Spy<any>
+    let messageAddSpy: jasmine.Spy<any>
     let fakeResponse = {
         bind9: {
             currentStable: [
@@ -80,41 +84,47 @@ describe('VersionStatusComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule, TooltipModule],
+            imports: [HttpClientTestingModule, TooltipModule, MessagesModule, BrowserAnimationsModule],
             declarations: [VersionStatusComponent],
             providers: [MessageService],
         }).compileComponents()
 
         fixture = TestBed.createComponent(VersionStatusComponent)
         versionService = TestBed.inject(VersionService)
+        messageService = TestBed.inject(MessageService)
         component = fixture.componentInstance
-        getCurrentDataSpy = spyOn(versionService, 'getCurrentData')
-        getCurrentDataSpy.and.returnValue(of(fakeResponse))
-        getSoftwareVersionFeedback = spyOn(versionService, 'getSoftwareVersionFeedback')
-        getSoftwareVersionFeedback.and.callThrough()
+        getCurrentDataSpy = spyOn(versionService, 'getCurrentData').and.returnValue(of(fakeResponse))
+        getSoftwareVersionFeedbackSpy = spyOn(versionService, 'getSoftwareVersionFeedback').and.callThrough()
+        messageAddSpy = spyOn(messageService, 'add').and.callThrough()
+    })
+
+    /**
+     * Sets correct values of required input properties.
+     */
+    function setCorrectInputs() {
         fixture.componentRef.setInput('app', 'kea')
         fixture.componentRef.setInput('version', '2.6.1')
         fixture.detectChanges()
-    })
+    }
 
     it('should create', () => {
+        setCorrectInputs()
         expect(component).toBeTruthy()
     })
 
     it('should return app name', () => {
         // Arrange
-        // Act
-        // Assert
+        setCorrectInputs()
+
+        // Act & Assert
         expect(component.appName).toBe('Kea')
 
         fixture.componentRef.setInput('app', 'stork')
-        fixture.detectChanges()
         component.ngOnInit()
         fixture.detectChanges()
         expect(component.appName).toBe('Stork agent')
 
         fixture.componentRef.setInput('app', 'bind9')
-        fixture.detectChanges()
         component.ngOnInit()
         fixture.detectChanges()
         expect(component.appName).toBe('Bind9')
@@ -122,12 +132,92 @@ describe('VersionStatusComponent', () => {
 
     it('should get current versions data', () => {
         // Arrange
-        // Act
-        // Assert
+        setCorrectInputs()
+
+        // Act & Assert
         expect(getCurrentDataSpy).toHaveBeenCalledTimes(1)
-        expect(getSoftwareVersionFeedback).toHaveBeenCalledOnceWith('2.6.1', 'kea', fakeResponse)
+        expect(getSoftwareVersionFeedbackSpy).toHaveBeenCalledOnceWith('2.6.1', 'kea', fakeResponse)
         expect(component.severity).toBe(Severity.success)
         expect(component.feedbackMessages).toBeTruthy()
         expect(component.feedbackMessages.length).toBeGreaterThan(0)
+        expect(Object.keys(component.iconClasses).length).toBeGreaterThan(1)
+    })
+
+    it('should not get current versions data for invalid semver', () => {
+        // Arrange
+        fixture.componentRef.setInput('app', 'kea')
+        fixture.componentRef.setInput('version', 'a.b.c')
+        fixture.detectChanges()
+
+        // Act & Assert
+        expect(getCurrentDataSpy).toHaveBeenCalledTimes(0)
+        expect(getSoftwareVersionFeedbackSpy).toHaveBeenCalledTimes(0)
+        expect(messageAddSpy).toHaveBeenCalledOnceWith({
+            severity: 'error',
+            summary: 'Error parsing software version',
+            detail: `Couldn't parse valid semver from given a.b.c version!`,
+            life: 10000,
+        })
+        expect(component.severity).toBeUndefined()
+        expect(component.feedbackMessages).toBeTruthy()
+        expect(component.feedbackMessages.length).toBe(0)
+        expect(Object.keys(component.iconClasses).length).toBe(0)
+    })
+
+    it('should display an error when version service observable emits error', () => {
+        // Arrange
+        getSoftwareVersionFeedbackSpy.and.throwError(new Error('internal error'))
+        setCorrectInputs()
+
+        // Act & Assert
+        expect(getCurrentDataSpy).toHaveBeenCalledTimes(1)
+        expect(getSoftwareVersionFeedbackSpy).toHaveBeenCalledTimes(1)
+        expect(messageAddSpy).toHaveBeenCalledOnceWith({
+            severity: 'error',
+            summary: 'Error fetching software versions data',
+            detail: 'Error when fetching software versions data: internal error',
+            life: 10000,
+        })
+        expect(component.severity).toBeUndefined()
+        expect(component.feedbackMessages).toBeTruthy()
+        expect(component.feedbackMessages.length).toBe(0)
+        expect(Object.keys(component.iconClasses).length).toBe(0)
+    })
+
+    it('should display app name', () => {
+        // Arrange
+        fixture.componentRef.setInput('showAppName', true)
+
+        // Act & Assert
+        setCorrectInputs()
+        let span = fixture.nativeElement.querySelector('span')
+        expect(span).toBeTruthy()
+        expect(span.textContent).toContain('Kea 2.6.1')
+    })
+
+    it('should not display app name', () => {
+        // Arrange
+        // Act & Assert
+        setCorrectInputs()
+        let span = fixture.nativeElement.querySelector('span')
+        expect(span).toBeNull()
+    })
+
+    it('should not display block message', () => {
+        // Arrange
+        // Act & Assert
+        setCorrectInputs()
+        let messagesDiv = fixture.nativeElement.querySelector('div.p-messages')
+        expect(messagesDiv).toBeNull()
+    })
+
+    it('should display block message', () => {
+        // Arrange
+        fixture.componentRef.setInput('inline', false)
+
+        // Act & Assert
+        setCorrectInputs()
+        let messagesDiv = fixture.nativeElement.querySelector('div.p-messages')
+        expect(messagesDiv).toBeTruthy()
     })
 })
