4de01877d1c15d8d4d31fa03862bbd0f37a83810
Slawek Figiel <slawek@isc.org>
2025-01-15 12:53:42 +0100

    [#1589] Extend a function to parse version

--- tests/system/core/version.py
+++ tests/system/core/version.py
@@ -21,10 +21,37 @@ def get_version():
 
 
 def parse_version_info(version):
-    """Parses the version string to version tuple."""
-    return tuple(int(x) for x in version.split("."))
+    """
+    Parses the version string to version tuple. Returns a tuple of integers
+    and a version suffix if it exists. Otherwise, None.
+    
+    Accepted values:
+    - Major only: "1" -> (1,), None
+    - Major, minor: "1.2" -> (1, 2), None
+    - Major, minor, patch: "1.2.3" -> (1, 2, 3), None
+    - Major, minor, patch, revision: "1.2.3.4" -> (1, 2, 3, 4), None
+    - Version with suffix: "1.2.3-dev" -> (1, 2, 3), "dev"
+    - Version with wildcard: "1.2.*" -> (1, 2), None
+    - Version with suffix and wildcard: "1.2.*-dev" -> (1, 2), "dev"
+    """
+    parts = version.split(".")
+    
+    # The last part may contain a suffix after a dash.
+    last_part = parts[-1]
+    suffix = None
+    if "-" in str(last_part):
+        last_part, suffix = last_part.split("-", 1)
+        parts[-1] = last_part
+    
+    # The last version part may be a wildcard. We skip it.
+    if last_part == "*":
+        parts = parts[:-1]
+
+    # Convert the version parts to integers.
+    return tuple(map(int, parts)), suffix
 
 
 def get_version_info():
     """Returns the current Stork version tuple from the GO file."""
-    return parse_version_info(get_version())
+    version_info, _ = parse_version_info(get_version())
+    return version_info
--- tests/system/tests/core/test_version.py
+++ tests/system/tests/core/test_version.py
@@ -1,5 +1,8 @@
 import re
 from unittest.mock import patch
+
+import pytest
+
 import core.version
 
 
@@ -21,6 +24,34 @@ def test_get_version_info(_):
     assert info == (1, 2, 3)
 
 
-def test_parse_version_info():
-    info = core.version.parse_version_info("11.22.33")
-    assert info == (11, 22, 33)
+def test_parse_version_info_positive():
+    test_cases = (
+        # Input and expected output.
+        ("1", ((1,), None)),  # Major only.
+        ("1.2", ((1, 2), None)),  # Major, minor.
+        ("1.2.3", ((1, 2, 3), None)),  # Major, minor, patch.
+        ("1.2.3.4", ((1, 2, 3, 4), None)),  # Major, minor, patch, revision.
+        ("1.2.3-dev", ((1, 2, 3), "dev")),  # Version with suffix.
+        ("1.2.*", ((1, 2), None)),  # Version with wildcard.
+        ("1.2.*-dev", ((1, 2), "dev")),  # Version with suffix and wildcard.
+        ("1.2.3-dev-rc2", ((1, 2, 3), "dev-rc2")),  # Suffix with many dashes.
+    )
+
+    for version, (expected_info, expected_suffix) in test_cases:
+        info, suffix = core.version.parse_version_info(version)
+        assert info == expected_info
+        assert suffix == expected_suffix
+
+
+def test_parse_version_info_negative():
+    test_cases = (
+        "",  # Empty string.
+        "foobar",  # Non-numeric string.
+        "1.2.",  # Missing patch.
+        "1.2.foo",  # Non-numeric patch.
+        "1..2.3",  # Double dots.
+    )
+
+    for version in test_cases:
+        with pytest.raises(Exception):
+            core.version.parse_version_info(version)
