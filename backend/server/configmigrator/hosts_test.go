dedc7b7941d089287fe6d7d3831b29a7eaccbe71
Slawek Figiel <slawek@isc.org>
2025-01-23 12:34:36 +0100

    [#800] Refactor test case

--- backend/server/configmigrator/hosts_test.go
+++ backend/server/configmigrator/hosts_test.go
@@ -1,6 +1,8 @@
 package configmigrator
 
 import (
+	"bytes"
+	"encoding/binary"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,11 +18,115 @@ import (
 // Test that the hosts are migrated and all errors are collected.
 func TestMigrate(t *testing.T) {
 	// Arrange
+	// Mocks and basic data.
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	agentMock := NewMockConnectedAgents(ctrl)
 	lookup := dbmodel.NewDHCPOptionDefinitionLookup()
+
+	migrator := NewHostMigrator(
+		dbmodel.HostsByPageFilters{}, nil, agentMock,
+		dbmodel.NewDHCPOptionDefinitionLookup(),
+	).(*hostMigrator)
+
+	// Assertion helpers.
+	expectReservationAddCommandWithError := func(daemon *dbmodel.Daemon, host *dbmodel.Host, result *agentcomm.KeaCmdsResult) {
+		reservation, _ := keaconfig.CreateHostCmdsReservation(
+			daemon.ID, lookup, host,
+		)
+
+		agentMock.EXPECT().ForwardToKeaOverHTTP(
+			gomock.Any(),          // Context.
+			gomock.Eq(daemon.App), // App.
+			gomock.Eq([]keactrl.SerializableCommand{
+				keactrl.NewCommandReservationAdd(
+					reservation, daemon.Name,
+				),
+			}), // Commands.
+			gomock.Any(), // Responses.
+		).Return(result, nil)
+	}
+
+	expectReservationAddCommandNoError := func(daemon *dbmodel.Daemon, host *dbmodel.Host) {
+		expectReservationAddCommandWithError(daemon, host, &agentcomm.KeaCmdsResult{})
+	}
+
+	expectReservationDelCommandWithError := func(daemon *dbmodel.Daemon, host *dbmodel.Host, result *agentcomm.KeaCmdsResult) {
+		deletedReservation, _ := keaconfig.CreateHostCmdsDeletedReservation(
+			daemon.ID, host, keaconfig.HostCmdsOperationTargetMemory,
+		)
+		agentMock.EXPECT().ForwardToKeaOverHTTP(
+			gomock.Any(),          // Context.
+			gomock.Eq(daemon.App), // App.
+			gomock.Eq([]keactrl.SerializableCommand{
+				keactrl.NewCommandReservationDel(
+					deletedReservation, daemon.Name,
+				),
+			}), // Commands.
+			gomock.Any(), // Responses.
+		).Return(result, nil)
+	}
+
+	expectReservationDelCommandNoError := func(daemon *dbmodel.Daemon, host *dbmodel.Host) {
+		expectReservationDelCommandWithError(daemon, host, &agentcomm.KeaCmdsResult{})
+	}
+
+	expectConfigWriteCommandWithError := func(daemon *dbmodel.Daemon, result *agentcomm.KeaCmdsResult) {
+		agentMock.EXPECT().ForwardToKeaOverHTTP(
+			gomock.Any(),          // Context.
+			gomock.Eq(daemon.App), // App.
+			gomock.Eq([]keactrl.SerializableCommand{
+				keactrl.NewCommandBase(keactrl.ConfigWrite, daemon.Name),
+			}), // Commands.
+			gomock.Any(), // Responses.
+		).Return(result, nil)
+	}
+
+	expectConfigWriteCommandNoError := func(daemon *dbmodel.Daemon) {
+		expectConfigWriteCommandWithError(daemon, &agentcomm.KeaCmdsResult{})
+	}
+
+	// Entities.
+	nextHostID := int64(1)
+	nextLocalHostID := int64(1)
+
+	createHost := func(daemons ...*dbmodel.Daemon) *dbmodel.Host {
+		var localHosts []dbmodel.LocalHost
+		for _, daemon := range daemons {
+			localHosts = append(localHosts, dbmodel.LocalHost{
+				ID:         nextLocalHostID,
+				DaemonID:   daemon.ID,
+				Daemon:     daemon,
+				DataSource: dbmodel.HostDataSourceConfig,
+			})
+			nextLocalHostID++
+		}
+
+		var identifier bytes.Buffer
+		_ = binary.Write(&identifier, binary.LittleEndian, 1024+nextHostID)
+
+		host := &dbmodel.Host{
+			ID: nextHostID,
+			HostIdentifiers: []dbmodel.HostIdentifier{{
+				ID:     nextHostID,
+				Type:   "hw-address",
+				Value:  identifier.Bytes(),
+				HostID: 1,
+			}},
+			LocalHosts: localHosts,
+		}
+		nextHostID++
+
+		return host
+	}
+
+	inactiveDaemon := &dbmodel.Daemon{
+		ID:     0,
+		Name:   dbmodel.DaemonNameDHCPv4,
+		App:    &dbmodel.App{},
+		Active: false,
+	}
 
 	daemon1 := &dbmodel.Daemon{
 		ID:     1,
@@ -29,72 +135,34 @@ func TestMigrate(t *testing.T) {
 		Active: true,
 	}
 
-	hostInDaemon1 := &dbmodel.Host{
-		ID:       1,
-		SubnetID: 42,
-		HostIdentifiers: []dbmodel.HostIdentifier{
-			{
-				ID:     1,
-				Type:   "hw-address",
-				Value:  []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06},
-				HostID: 1,
-			},
-		},
-		LocalHosts: []dbmodel.LocalHost{
-			{
-				ID:         1,
-				DaemonID:   daemon1.ID,
-				Daemon:     daemon1,
-				DataSource: dbmodel.HostDataSourceConfig,
-			},
-		},
+	daemon2 := &dbmodel.Daemon{
+		ID:     2,
+		Name:   dbmodel.DaemonNameDHCPv4,
+		App:    &dbmodel.App{},
+		Active: true,
 	}
-
-	migrator := NewHostMigrator(
-		dbmodel.HostsByPageFilters{}, nil, agentMock,
-		dbmodel.NewDHCPOptionDefinitionLookup(),
-	).(*hostMigrator)
+	_ = daemon2
 
 	t.Run("single host, single daemon, all OK", func(t *testing.T) {
-		reservation, _ := keaconfig.CreateHostCmdsReservation(
-			daemon1.ID, lookup, hostInDaemon1,
-		)
+		host := createHost(daemon1)
 
-		agentMock.EXPECT().ForwardToKeaOverHTTP(
-			gomock.Any(),           // Context.
-			gomock.Eq(daemon1.App), // App.
-			gomock.Eq([]keactrl.SerializableCommand{
-				keactrl.NewCommandReservationAdd(
-					reservation, dbmodel.DaemonNameDHCPv4,
-				),
-			}), // Commands.
-			gomock.Any(), // Responses.
-		).Return(&agentcomm.KeaCmdsResult{}, nil)
+		expectReservationAddCommandNoError(daemon1, host)
+		expectReservationDelCommandNoError(daemon1, host)
+		expectConfigWriteCommandNoError(daemon1)
 
-		deletedReservation, _ := keaconfig.CreateHostCmdsDeletedReservation(
-			daemon1.ID, hostInDaemon1, keaconfig.HostCmdsOperationTargetMemory,
-		)
-		agentMock.EXPECT().ForwardToKeaOverHTTP(
-			gomock.Any(),           // Context.
-			gomock.Eq(daemon1.App), // App.
-			gomock.Eq([]keactrl.SerializableCommand{
-				keactrl.NewCommandReservationDel(
-					deletedReservation, "dhcp4",
-				),
-			}), // Commands.
-			gomock.Any(), // Responses.
-		).Return(&agentcomm.KeaCmdsResult{}, nil)
+		migrator.items = []dbmodel.Host{*host}
 
-		agentMock.EXPECT().ForwardToKeaOverHTTP(
-			gomock.Any(),           // Context.
-			gomock.Eq(daemon1.App), // App.
-			gomock.Eq([]keactrl.SerializableCommand{
-				keactrl.NewCommandBase(keactrl.ConfigWrite, "dhcp4"),
-			}), // Commands.
-			gomock.Any(), // Responses.
-		).Return(&agentcomm.KeaCmdsResult{}, nil)
+		// Act
+		errs := migrator.Migrate()
 
-		migrator.items = []dbmodel.Host{*hostInDaemon1}
+		// Assert
+		require.Empty(t, errs)
+	})
+
+	t.Run("inactive daemon", func(t *testing.T) {
+		host := createHost(inactiveDaemon)
+
+		migrator.items = []dbmodel.Host{*host}
 
 		// Act
 		errs := migrator.Migrate()
