93cdd3ea304c661dd3735f35f1b93b980d143407
Slawek Figiel <slawek@isc.org>
2025-01-27 14:04:59 +0100

    [#800] Add comments

--- backend/server/configmigrator/migrator.go
+++ backend/server/configmigrator/migrator.go
@@ -1,7 +1,14 @@
 package configmigrator
 
+// Interface implemented by the structs that know how to migrate the particular
+// entries from the Kea configuration to the database.
 type Migrator interface {
+	// Returns a total number of items to migrate.
 	CountTotal() (int64, error)
+	// Loads a chunk of items from the Kea configuration. Returns the number of
+	// loaded items and an error if any.
 	LoadItems(offset int64) (int64, error)
+	// Migrates the loaded items. Returns a map of errors that occurred during
+	// the migration. The key is the ID of the migrated item.
 	Migrate() map[int64]error
 }
