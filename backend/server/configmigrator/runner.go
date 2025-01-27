9e413fff943a09c78e9e4b22cb7030807435803b
Slawek Figiel <slawek@isc.org>
2025-01-27 14:29:10 +0100

    [#800] Add unit tests

--- backend/server/configmigrator/runner.go
+++ backend/server/configmigrator/runner.go
@@ -4,6 +4,12 @@ import (
 	"github.com/pkg/errors"
 )
 
+// It is a simple migration runner that is backed by the usual loop and do
+// everything in foreground.
+// It loads the items in chunks and migrates them until there are no more
+// items to migrate.
+// Returns a map of errors that occurred during the migration. The key is the
+// ID of the migrated item.
 func RunMigration(migrator Migrator) (map[int64]error, error) {
 	migrationErrs := make(map[int64]error)
 	totalCount := int64(0)
---
+++ backend/server/configmigrator/runner_test.go
@@ -0,0 +1,95 @@
+package configmigrator
+
+import (
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	gomock "go.uber.org/mock/gomock"
+)
+
+//go:generate mockgen -package=configmigrator -destination=migratormock_test.go isc.org/stork/server/configmigrator Migrator
+
+// Test that runner doesn't crash if there are no items to migrate.
+func TestRunMigrationEmpty(t *testing.T) {
+	// Arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := NewMockMigrator(ctrl)
+	mock.EXPECT().LoadItems(int64(0)).Return(int64(0), nil)
+
+	// Act
+	errs, err := RunMigration(mock)
+
+	// Assert
+	require.NoError(t, err)
+	require.Empty(t, errs)
+}
+
+// Test that runner migrates all items.
+func TestRunMigration(t *testing.T) {
+	// Arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := NewMockMigrator(ctrl)
+	mock.EXPECT().LoadItems(gomock.Eq(int64(0))).Return(int64(10), nil)
+	mock.EXPECT().LoadItems(gomock.Eq(int64(10))).Return(int64(10), nil)
+	mock.EXPECT().LoadItems(gomock.Eq(int64(20))).Return(int64(5), nil)
+	mock.EXPECT().LoadItems(gomock.Eq(int64(25))).Return(int64(0), nil)
+	mock.EXPECT().Migrate().Return(map[int64]error{}).Times(3)
+
+	// Act
+	errs, err := RunMigration(mock)
+
+	// Assert
+	require.NoError(t, err)
+	require.Empty(t, errs)
+}
+
+// Test that the migration errors are aggregated.
+func TestRunMigrationAggregatesErrors(t *testing.T) {
+	// Arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := NewMockMigrator(ctrl)
+	mock.EXPECT().LoadItems(gomock.Eq(int64(0))).Return(int64(10), nil)
+	mock.EXPECT().LoadItems(gomock.Eq(int64(10))).Return(int64(5), nil)
+	mock.EXPECT().LoadItems(gomock.Eq(int64(15))).Return(int64(0), nil)
+
+	callCount := 0
+	mock.EXPECT().Migrate().DoAndReturn(func() map[int64]error {
+		callCount++
+		return map[int64]error{
+			int64(callCount): errors.Errorf("error %d", callCount),
+		}
+	}).Times(2)
+
+	// Act
+	errs, err := RunMigration(mock)
+
+	// Assert
+	require.NoError(t, err)
+	require.Len(t, errs, 2)
+	require.EqualError(t, errs[1], "error 1")
+	require.EqualError(t, errs[2], "error 2")
+}
+
+// Test that the runner interrupts the migration after the loading error.
+func TestRunMigrationInterruptOnLoadingError(t *testing.T) {
+	// Arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := NewMockMigrator(ctrl)
+	mock.EXPECT().LoadItems(gomock.Eq(int64(0))).Return(int64(10), errors.New("loading error"))
+
+	// Act
+	errs, err := RunMigration(mock)
+
+	// Assert
+	require.ErrorContains(t, err, "loading error")
+	require.Empty(t, errs)
+}
