524dbd6cfc9df41d42bb8a645fcaeedc95e3e454
Slawek Figiel <slawek@isc.org>
2025-02-20 19:41:35 +0100

    [#800] Move methods

---
+++ backend/server/restservice/configmigrations.go
@@ -0,0 +1,111 @@
+package restservice
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/strfmt"
+	"isc.org/stork/server/configmigrator"
+	"isc.org/stork/server/gen/models"
+	dhcp "isc.org/stork/server/gen/restapi/operations/d_h_c_p"
+	storkutil "isc.org/stork/util"
+)
+
+// Converts the status returned by a service to the format used in REST API.
+func (r *RestAPI) convertMigrationStatusToRestAPI(status configmigrator.MigrationStatus) *models.MigrationStatus {
+	errs := []*models.MigrationError{}
+	for _, err := range status.Errors {
+		errs = append(errs, &models.MigrationError{
+			Error:  err.Err.Error(),
+			HostID: err.ID,
+			Label:  err.Label,
+		})
+	}
+	var generalError *string
+	if status.GeneralError != nil {
+		generalError = storkutil.Ptr(status.GeneralError.Error())
+	}
+
+	return &models.MigrationStatus{
+		Canceling:   status.Canceling,
+		Context:     status.Context,
+		ElapsedTime: strfmt.Duration(status.ElapsedTime),
+		EndDate:     convertToOptionalDatetime(status.EndDate),
+		EntityType:  string(status.EntityType),
+		Errors: &models.MigrationErrors{
+			Items: errs,
+			Total: int64(len(errs)),
+		},
+		EstimatedLeftTime: strfmt.Duration(status.EstimatedLeftTime),
+		GeneralError:      generalError,
+		ID:                string(status.ID),
+		Progress:          status.Progress,
+		StartDate:         strfmt.DateTime(status.StartDate),
+	}
+}
+
+// Implements the GET call to retrieve the statuses of all (ongoing or
+// completed) migrations.
+func (r *RestAPI) GetMigrations(ctx context.Context, params dhcp.GetMigrationsParams) middleware.Responder {
+	// Fetch migration statuses from the migration service.
+	statuses := r.MigrationService.GetMigrations()
+
+	// Convert statuses to REST API format.
+	respStatuses := []*models.MigrationStatus{}
+	for _, status := range statuses {
+		respStatuses = append(respStatuses, r.convertMigrationStatusToRestAPI(status))
+	}
+
+	// Send the statuses to the client.
+	rsp := dhcp.NewGetMigrationsOK().WithPayload(&models.MigrationStatuses{
+		Items: respStatuses,
+		Total: int64(len(respStatuses)),
+	})
+	return rsp
+}
+
+// Implements the DELETE call to remove all finished migrations.
+func (r *RestAPI) DeleteFinishedMigrations(ctx context.Context, params dhcp.DeleteFinishedMigrationsParams) middleware.Responder {
+	// Remove finished migrations from the migration service.
+	r.MigrationService.ClearFinishedMigrations()
+
+	// Send OK response to the client.
+	rsp := dhcp.NewDeleteFinishedMigrationsOK()
+	return rsp
+}
+
+// Implements the GET call to retrieve the status of a specific migration.
+func (r *RestAPI) GetMigration(ctx context.Context, params dhcp.GetMigrationParams) middleware.Responder {
+	// Fetch migration status from the migration service.
+	status, ok := r.MigrationService.GetMigration(configmigrator.MigrationIdentifier(params.ID))
+	if !ok {
+		msg := fmt.Sprintf("Cannot find migration status with ID %s", params.ID)
+		rsp := dhcp.NewGetMigrationDefault(http.StatusNotFound).WithPayload(&models.APIError{
+			Message: &msg,
+		})
+		return rsp
+	}
+
+	migrationStatus := r.convertMigrationStatusToRestAPI(status)
+	rsp := dhcp.NewGetMigrationOK().WithPayload(migrationStatus)
+	return rsp
+}
+
+// Implements the POST call to cancel an ongoing migration.
+func (r *RestAPI) CancelMigration(ctx context.Context, params dhcp.CancelMigrationParams) middleware.Responder {
+	// Attempt to cancel the migration.
+	status, ok := r.MigrationService.StopMigration(configmigrator.MigrationIdentifier(params.ID))
+	if !ok {
+		msg := fmt.Sprintf("Cannot find migration status with ID %s", params.ID)
+		rsp := dhcp.NewCancelMigrationDefault(http.StatusNotFound).WithPayload(&models.APIError{
+			Message: &msg,
+		})
+		return rsp
+	}
+
+	// Send OK response to the client.
+	rsp := dhcp.NewCancelMigrationOK().WithPayload(r.convertMigrationStatusToRestAPI(status))
+	return rsp
+}
---
+++ backend/server/restservice/configmigrations_test.go
@@ -0,0 +1,309 @@
+package restservice
+
+import (
+	context "context"
+	http "net/http"
+	"testing"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	gomock "go.uber.org/mock/gomock"
+	"isc.org/stork/server/configmigrator"
+	dbtest "isc.org/stork/server/database/test"
+	"isc.org/stork/server/gen/models"
+	dhcp "isc.org/stork/server/gen/restapi/operations/d_h_c_p"
+)
+
+// Test that the all migrations can be returned regardless of their status.
+func TestGetMigrations(t *testing.T) {
+	// Arrange
+	db, dbSettings, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	migrationService := NewMockMigrationService(ctrl)
+
+	rapi, err := NewRestAPI(dbSettings, db, migrationService)
+	require.NoError(t, err)
+
+	migrationErred := configmigrator.MigrationStatus{
+		ID:        "1234-1",
+		Context:   context.Background(),
+		StartDate: time.Date(2025, 2, 13, 10, 24, 45, 432000000, time.UTC),
+		EndDate:   time.Time{},
+		Canceling: false,
+		Progress:  0.2,
+		Errors: []configmigrator.MigrationError{
+			{Err: errors.New("foo"), ID: 4, Label: "host-4"},
+			{Err: errors.New("bar"), ID: 2, Label: "host-2"},
+		},
+		GeneralError:      nil,
+		EntityType:        "host",
+		ElapsedTime:       5 * time.Second,
+		EstimatedLeftTime: 1 * time.Minute,
+	}
+
+	migrationInProgress := configmigrator.MigrationStatus{
+		ID:                "1234-2",
+		Context:           context.Background(),
+		StartDate:         time.Date(2025, 2, 14, 11, 25, 46, 432000000, time.UTC),
+		EndDate:           time.Time{},
+		Canceling:         false,
+		Progress:          0.5,
+		Errors:            nil,
+		GeneralError:      nil,
+		EntityType:        "host",
+		ElapsedTime:       10 * time.Second,
+		EstimatedLeftTime: 2 * time.Minute,
+	}
+
+	migrationFinished := configmigrator.MigrationStatus{
+		ID:                "1234-3",
+		Context:           context.Background(),
+		StartDate:         time.Date(2025, 2, 15, 12, 26, 47, 432000000, time.UTC),
+		EndDate:           time.Date(2025, 2, 15, 12, 27, 48, 432000000, time.UTC),
+		Canceling:         false,
+		Progress:          1.0,
+		Errors:            nil,
+		GeneralError:      nil,
+		EntityType:        "host",
+		ElapsedTime:       15 * time.Second,
+		EstimatedLeftTime: 0 * time.Minute,
+	}
+
+	migrationService.EXPECT().GetMigrations().Return(
+		[]configmigrator.MigrationStatus{
+			migrationErred, migrationInProgress, migrationFinished,
+		},
+	)
+
+	// Act
+	rsp := rapi.GetMigrations(context.Background(), dhcp.GetMigrationsParams{})
+
+	// Assert
+	require.IsType(t, &dhcp.GetMigrationsOK{}, rsp)
+	okRsp := rsp.(*dhcp.GetMigrationsOK)
+
+	require.Len(t, okRsp.Payload.Items, 3)
+
+	status := okRsp.Payload.Items[0]
+	require.Equal(t, "1234-1", status.ID)
+	require.Equal(t, "2025-02-13T10:24:45.432Z", status.StartDate.String())
+	require.Equal(t, 0.2, status.Progress)
+	require.Equal(t, int64(2), status.Errors.Total)
+	require.Len(t, status.Errors.Items, 2)
+	require.ElementsMatch(t, []*models.MigrationError{
+		{Error: "foo", HostID: 4, Label: "host-4"},
+		{Error: "bar", HostID: 2, Label: "host-2"},
+	}, status.Errors.Items)
+
+	status = okRsp.Payload.Items[1]
+	require.Equal(t, "1234-2", status.ID)
+	require.Equal(t, "2025-02-14T11:25:46.432Z", status.StartDate.String())
+	require.Equal(t, 0.5, status.Progress)
+	require.Equal(t, int64(0), status.Errors.Total)
+	require.Len(t, status.Errors.Items, 0)
+
+	status = okRsp.Payload.Items[2]
+	require.Equal(t, "1234-3", status.ID)
+	require.Equal(t, "2025-02-15T12:26:47.432Z", status.StartDate.String())
+	require.Equal(t, 1.0, status.Progress)
+	require.Equal(t, int64(0), status.Errors.Total)
+	require.Len(t, status.Errors.Items, 0)
+}
+
+// Test that the endpoint correctly triggers the deletion of all finished
+// migrations.
+func TestDeleteFinishedMigrations(t *testing.T) {
+	// Arrange
+	db, dbSettings, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	migrationService := NewMockMigrationService(ctrl)
+
+	rapi, err := NewRestAPI(dbSettings, db, migrationService)
+	require.NoError(t, err)
+
+	migrationService.EXPECT().ClearFinishedMigrations()
+
+	// Act
+	rsp := rapi.DeleteFinishedMigrations(
+		context.Background(),
+		dhcp.DeleteFinishedMigrationsParams{},
+	)
+
+	// Assert
+	require.IsType(t, &dhcp.DeleteFinishedMigrationsOK{}, rsp)
+}
+
+// Test that the HTTP 404 status is returned when trying to get a migration
+// that does not exist.
+func TestGetMigrationNotFound(t *testing.T) {
+	// Arrange
+	db, dbSettings, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	migrationService := NewMockMigrationService(ctrl)
+
+	rapi, err := NewRestAPI(dbSettings, db, migrationService)
+	require.NoError(t, err)
+
+	migrationService.EXPECT().GetMigration(configmigrator.MigrationIdentifier("1234-1")).Return(configmigrator.MigrationStatus{}, false)
+
+	// Act
+	rsp := rapi.GetMigration(context.Background(), dhcp.GetMigrationParams{ID: "1234-1"})
+
+	// Assert
+	require.IsType(t, &dhcp.GetMigrationDefault{}, rsp)
+	defaultRsp := rsp.(*dhcp.GetMigrationDefault)
+	require.Equal(t, http.StatusNotFound, getStatusCode(*defaultRsp))
+}
+
+// Test that the particular migration is returned correctly if it exists.
+func TestGetMigration(t *testing.T) {
+	// Arrange
+	db, dbSettings, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	migrationService := NewMockMigrationService(ctrl)
+
+	rapi, err := NewRestAPI(dbSettings, db, migrationService)
+	require.NoError(t, err)
+
+	migrationStatus := configmigrator.MigrationStatus{
+		ID:        "1234-1",
+		Context:   context.Background(),
+		StartDate: time.Date(2025, 2, 13, 10, 24, 45, 432000000, time.UTC),
+		EndDate:   time.Time{},
+		Canceling: false,
+		Progress:  0.2,
+		Errors: []configmigrator.MigrationError{
+			{Err: errors.New("foo"), ID: 4, Label: "host-4"},
+			{Err: errors.New("bar"), ID: 2, Label: "host-2"},
+		},
+		GeneralError:      nil,
+		EntityType:        "host",
+		ElapsedTime:       5 * time.Second,
+		EstimatedLeftTime: 1 * time.Minute,
+	}
+
+	migrationService.EXPECT().GetMigration(configmigrator.MigrationIdentifier("1234-1")).Return(migrationStatus, true)
+
+	// Act
+	rsp := rapi.GetMigration(context.Background(), dhcp.GetMigrationParams{ID: "1234-1"})
+
+	// Assert
+	require.IsType(t, &dhcp.GetMigrationOK{}, rsp)
+	okRsp := rsp.(*dhcp.GetMigrationOK)
+
+	require.Equal(t, "1234-1", okRsp.Payload.ID)
+	require.Equal(t, "2025-02-13T10:24:45.432Z", okRsp.Payload.StartDate.String())
+	require.Equal(t, 0.2, okRsp.Payload.Progress)
+	require.Equal(t, int64(2), okRsp.Payload.Errors.Total)
+	require.Len(t, okRsp.Payload.Errors.Items, 2)
+	require.ElementsMatch(t, []*models.MigrationError{
+		{Error: "foo", HostID: 4, Label: "host-4"},
+		{Error: "bar", HostID: 2, Label: "host-2"},
+	}, okRsp.Payload.Errors.Items)
+}
+
+// Test that the cancellation endpoint correctly triggers the cancellation of
+// the migration.
+func TestCancelMigration(t *testing.T) {
+	// Arrange
+	db, dbSettings, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	migrationService := NewMockMigrationService(ctrl)
+
+	rapi, err := NewRestAPI(dbSettings, db, migrationService)
+	require.NoError(t, err)
+
+	migrationStatus := configmigrator.MigrationStatus{
+		ID:        "1234-1",
+		Context:   context.Background(),
+		StartDate: time.Date(2025, 2, 13, 10, 24, 45, 432000000, time.UTC),
+		EndDate:   time.Time{},
+		Canceling: true,
+		Progress:  0.2,
+		Errors: []configmigrator.MigrationError{
+			{Err: errors.New("foo"), ID: 4, Label: "host-4"},
+			{Err: errors.New("bar"), ID: 2, Label: "host-2"},
+		},
+		GeneralError:      nil,
+		EntityType:        "host",
+		ElapsedTime:       5 * time.Second,
+		EstimatedLeftTime: 1 * time.Minute,
+	}
+
+	migrationService.EXPECT().
+		StopMigration(configmigrator.MigrationIdentifier("1234-1")).
+		Return(migrationStatus, true)
+
+	// Act
+	rsp := rapi.CancelMigration(
+		context.Background(),
+		dhcp.CancelMigrationParams{ID: "1234-1"},
+	)
+
+	// Assert
+	require.IsType(t, &dhcp.CancelMigrationOK{}, rsp)
+	okRsp := rsp.(*dhcp.CancelMigrationOK)
+
+	require.Equal(t, "1234-1", okRsp.Payload.ID)
+	require.Equal(t, "2025-02-13T10:24:45.432Z", okRsp.Payload.StartDate.String())
+	require.Equal(t, 0.2, okRsp.Payload.Progress)
+	require.Equal(t, int64(2), okRsp.Payload.Errors.Total)
+	require.Len(t, okRsp.Payload.Errors.Items, 2)
+	require.ElementsMatch(t, []*models.MigrationError{
+		{Error: "foo", HostID: 4, Label: "host-4"},
+		{Error: "bar", HostID: 2, Label: "host-2"},
+	}, okRsp.Payload.Errors.Items)
+	require.True(t, okRsp.Payload.Canceling)
+}
+
+// Test that the cancellation endpoint returns a HTTP 404 Not Found status when
+// trying to cancel a migration that does not exist.
+func TestCancelMigrationNotFound(t *testing.T) {
+	// Arrange
+	db, dbSettings, teardown := dbtest.SetupDatabaseTestCase(t)
+	defer teardown()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	migrationService := NewMockMigrationService(ctrl)
+
+	rapi, err := NewRestAPI(dbSettings, db, migrationService)
+	require.NoError(t, err)
+
+	migrationService.EXPECT().
+		StopMigration(configmigrator.MigrationIdentifier("1234-1")).
+		Return(configmigrator.MigrationStatus{}, false)
+
+	// Act
+	rsp := rapi.CancelMigration(
+		context.Background(),
+		dhcp.CancelMigrationParams{ID: "1234-1"},
+	)
+
+	// Assert
+	require.IsType(t, &dhcp.CancelMigrationDefault{}, rsp)
+	defaultRsp := rsp.(*dhcp.CancelMigrationDefault)
+	require.Equal(t, http.StatusNotFound, getStatusCode(*defaultRsp))
+}
