cb88bcf868330100dde4364254810dba62947fe4
Slawek Figiel <slawek@isc.org>
2025-02-27 13:36:05 +0100

    [#1839] Disable an unstable test

--- tests/system/tests/test_overload.py
+++ tests/system/tests/test_overload.py
@@ -66,7 +66,11 @@ def test_two_same_big_configurations_at_time(
     state, *_ = server_service.wait_for_next_machine_states()
     assert state
 
-
+@pytest.mark.skip(
+    reason="""The test is unstable because the deadlocks may occur after the 
+migration. It causes the server only partially updates the host data in its
+database. In result, the test assertions fail."""
+)
 @ha_parametrize(
     "agent-kea-many-host-reservations-1",
     "agent-kea-many-host-reservations-2",
