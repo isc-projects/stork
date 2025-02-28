d93f032e5ede73e26c678ef49c650d3c0d145685
Slawek Figiel <slawek@isc.org>
2025-02-28 18:56:07 +0100

    [#800] Initial migration page

--- webui/src/app/config-migration-page/config-migration-page.component.spec.ts
+++ webui/src/app/config-migration-page/config-migration-page.component.spec.ts
@@ -1,23 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ConfigMigrationPageComponent } from './config-migration-page.component';
+import { ConfigMigrationPageComponent } from './config-migration-page.component'
+import { provideHttpClientTesting } from '@angular/common/http/testing'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 
 describe('ConfigMigrationPageComponent', () => {
-  let component: ConfigMigrationPageComponent;
-  let fixture: ComponentFixture<ConfigMigrationPageComponent>;
+    let component: ConfigMigrationPageComponent
+    let fixture: ComponentFixture<ConfigMigrationPageComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ConfigMigrationPageComponent]
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ConfigMigrationPageComponent],
+            providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()],
+        }).compileComponents()
+
+        fixture = TestBed.createComponent(ConfigMigrationPageComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
     })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ConfigMigrationPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
+})
