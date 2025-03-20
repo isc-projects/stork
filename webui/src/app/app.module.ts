4898c0f3bddf0dd08b33a79edf162a3a12be2f35
Slawek Figiel <slawek@isc.org>
2025-03-20 11:58:49 +0100

    [#1682] Add missing import

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -147,6 +147,7 @@ import { VersionPageComponent } from './version-page/version-page.component'
 import { BadgeModule } from 'primeng/badge'
 import { MachinesTableComponent } from './machines-table/machines-table.component'
 import { ZonesPageComponent } from './zones-page/zones-page.component'
+import { ByteCharacterComponent } from './byte-character/byte-character.component'
 
 /** Create the OpenAPI client configuration. */
 export function cfgFactory() {
@@ -244,6 +245,7 @@ export function cfgFactory() {
         VersionPageComponent,
         MachinesTableComponent,
         ZonesPageComponent,
+        ByteCharacterComponent,
     ],
     imports: [
         BrowserModule,
