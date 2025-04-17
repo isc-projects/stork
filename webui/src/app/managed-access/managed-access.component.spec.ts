2bcbaa6895f09c6831eb17eb677959eac597c019
Piotrek Zadroga <piotrek@isc.org>
2025-04-17 12:59:04 +0200

    [#157] add new component

---
+++ webui/src/app/managed-access/managed-access.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManagedAccessComponent } from './managed-access.component';
+
+describe('ManagedAccessComponent', () => {
+  let component: ManagedAccessComponent;
+  let fixture: ComponentFixture<ManagedAccessComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ManagedAccessComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ManagedAccessComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ webui/src/app/managed-access/managed-access.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-managed-access',
+  standalone: true,
+  imports: [],
+  templateUrl: './managed-access.component.html',
+  styleUrl: './managed-access.component.sass'
+})
+export class ManagedAccessComponent {
+
+}
