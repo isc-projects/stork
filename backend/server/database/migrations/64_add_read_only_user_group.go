2ebd3b1b4e49d158a587eafbe653443d3300da0e
Piotrek Zadroga <piotrek@isc.org>
2025-04-28 12:28:05 +0200

    [#157] improve db migration

--- backend/server/database/migrations/64_add_read_only_user_group.go
+++ backend/server/database/migrations/64_add_read_only_user_group.go
@@ -7,17 +7,21 @@ import "github.com/go-pg/migrations/v8"
 func init() {
 	migrations.MustRegisterTx(func(db migrations.DB) error {
 		_, err := db.Exec(`
+			-- Add UNIQUE constraint on group name. There shouldn't be more than one group with the same name.
+			ALTER TABLE system_group ADD CONSTRAINT system_group_name_unique_idx UNIQUE (name);
 			-- Stork uses hard-coded IDs for super-admin, admin and read-only groups.
-			-- Alter the sequence to have a pool of 100 reserved IDs for pre-defined system groups.
-			-- In the future a super-admin will be able to add/remove/update groups and set custom privileges for them.
-			-- Increasing the sequence will prevent ID conflicts when dynamically assigning for future groups.
-			ALTER SEQUENCE system_group_id_seq RESTART WITH 100;
+			-- Thus, insert new group with explicitly given PK ID=3.
+			-- Relying on a sequence could potentially end up with other ID value assigned to the read-only group.
 			INSERT INTO system_group (id, name, description) VALUES (3, 'read-only', 'This group of users can only have read access to system components and APIs. Users that belong to this group cannot perform Create, Update nor Delete actions.');
+			-- Reset the primary key sequence to be in sync with max ID in the table.
+			SELECT setval('system_group_id_seq', MAX(id)) FROM system_group;
 		`)
 		return err
 	}, func(db migrations.DB) error {
 		_, err := db.Exec(`
-			DELETE FROM system_group WHERE name = 'read-only' AND id = 3;
+			DELETE FROM system_group WHERE name = 'read-only';
+			-- Revert UNIQUE constraint on group name.
+			ALTER TABLE system_group DROP CONSTRAINT IF EXISTS system_group_name_unique_idx;
 			-- Reset the primary key sequence to be in sync with max ID in the table.
 			SELECT setval('system_group_id_seq', MAX(id)) FROM system_group;
 		`)
