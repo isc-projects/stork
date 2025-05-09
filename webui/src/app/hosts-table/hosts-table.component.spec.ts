88eb40f2892eee130040e3f9279c02ad9d41f110
Slawek Figiel <slawek@isc.org>
2025-05-09 18:01:00 +0200

    [#800] Add unit tests

--- webui/src/app/hosts-table/hosts-table.component.spec.ts
+++ webui/src/app/hosts-table/hosts-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing'
 
 import { HostsTableComponent } from './hosts-table.component'
 import { TableModule } from 'primeng/table'
@@ -16,13 +16,19 @@ import { PanelModule } from 'primeng/panel'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { TagModule } from 'primeng/tag'
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
-import { ConfirmDialogModule } from 'primeng/confirmdialog'
+import { ConfirmDialog, ConfirmDialogModule } from 'primeng/confirmdialog'
+import { DHCPService, Host } from '../backend'
+import { By } from '@angular/platform-browser'
+import { of } from 'rxjs'
 
 describe('HostsTableComponent', () => {
     let component: HostsTableComponent
     let fixture: ComponentFixture<HostsTableComponent>
+    let dhcpServiceSpy: jasmine.SpyObj<DHCPService>
 
     beforeEach(waitForAsync(() => {
+        const spy = jasmine.createSpyObj('DHCPService', ['startHostsMigration', 'getHosts'])
+
         TestBed.configureTestingModule({
             declarations: [HostsTableComponent, HelpTipComponent, PluralizePipe],
             imports: [
@@ -36,6 +42,10 @@ describe('HostsTableComponent', () => {
                     {
                         path: 'dhcp/hosts/:id',
                         component: HostsPageComponent,
+                    },
+                    {
+                        path: 'config-migrations/:id',
+                        redirectTo: 'dhcp/hosts/all',
                     },
                 ]),
                 ButtonModule,
@@ -52,8 +62,11 @@ describe('HostsTableComponent', () => {
                 ConfirmationService,
                 provideHttpClient(withInterceptorsFromDi()),
                 provideHttpClientTesting(),
+                { provide: DHCPService, useValue: spy },
             ],
         }).compileComponents()
+
+        dhcpServiceSpy = TestBed.inject(DHCPService) as jasmine.SpyObj<DHCPService>
     }))
 
     beforeEach(() => {
@@ -64,5 +77,135 @@ describe('HostsTableComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy()
+    })
+
+    it('should group the hosts by application', () => {
+        // Arrange
+        const hosts: Partial<Host>[] = [
+            { id: 1, localHosts: [{ appId: 11 }] },
+            { id: 2, localHosts: [{ appId: 22 }, { appId: 22 }, { appId: 33 }] },
+            { id: 3, localHosts: [{ appId: 11 }, { appId: 22 }] },
+        ]
+
+        // Act
+        component.hosts = hosts as Host[]
+
+        // Assert
+        expect(component.localHostsGroupedByApp[1].length).toBe(1)
+        expect(component.localHostsGroupedByApp[1][0].length).toBe(1)
+        expect(component.localHostsGroupedByApp[1][0][0].appId).toBe(11)
+
+        expect(component.localHostsGroupedByApp[2].length).toBe(2)
+        expect(component.localHostsGroupedByApp[2][0].length).toBe(2)
+        expect(component.localHostsGroupedByApp[2][0][0].appId).toBe(22)
+        expect(component.localHostsGroupedByApp[2][0][1].appId).toBe(22)
+        expect(component.localHostsGroupedByApp[2][1].length).toBe(1)
+        expect(component.localHostsGroupedByApp[2][1][0].appId).toBe(33)
+
+        expect(component.localHostsGroupedByApp[3][0].length).toBe(1)
+        expect(component.localHostsGroupedByApp[3][0][0].appId).toBe(11)
+        expect(component.localHostsGroupedByApp[3][1].length).toBe(1)
+        expect(component.localHostsGroupedByApp[3][1][0].appId).toBe(22)
+    })
+
+    it('should detect local hosts state', () => {
+        // Arrange
+        const zero = []
+
+        const single = [{ appId: 1, bootFields: { field1: 'value1' }, clientClasses: ['class1'], dhcpOptions: {} }]
+
+        const conflict = [
+            { appId: 1, bootFields: { field1: 'value1' }, clientClasses: ['class1'], dhcpOptions: {} },
+            { appId: 1, bootFields: { field1: 'value2' }, clientClasses: ['class2'], dhcpOptions: {} },
+        ]
+
+        const duplicate = [
+            { appId: 1, bootFields: { field1: 'value1' }, clientClasses: ['class1'], dhcpOptions: {} },
+            { appId: 1, bootFields: { field1: 'value1' }, clientClasses: ['class1'], dhcpOptions: {} },
+        ]
+
+        // Act
+        const zeroState = component.getLocalHostsState(zero)
+        const singleState = component.getLocalHostsState(single)
+        const conflictState = component.getLocalHostsState(conflict)
+        const duplicateState = component.getLocalHostsState(duplicate)
+
+        // Assert
+        expect(zeroState).toBeNull()
+        expect(singleState).toBeNull()
+        expect(conflictState).toBe('conflict')
+        expect(duplicateState).toBe('duplicate')
+    })
+
+    it('should ask for confirmation before migrating hosts', fakeAsync(() => {
+        dhcpServiceSpy.startHostsMigration.and.returnValue(of({}) as any)
+
+        component.validFilter = {
+            appId: 1,
+            isGlobal: true,
+            text: 'foo',
+        }
+
+        component.migrateToDatabaseAsk()
+        fixture.whenRenderingDone()
+
+        const dialog = fixture.debugElement.query(By.directive(ConfirmDialog))
+        expect(dialog).not.toBeNull()
+        const confirmDialog = dialog.componentInstance as ConfirmDialog
+        expect(confirmDialog).not.toBeNull()
+        confirmDialog.accept()
+        tick()
+
+        expect(dhcpServiceSpy.startHostsMigration).toHaveBeenCalledWith(
+            component.validFilter.appId,
+            component.validFilter.subnetId,
+            component.validFilter.keaSubnetId,
+            component.validFilter.text,
+            component.validFilter.isGlobal
+        )
+    }))
+
+    it('should extract filter entries properly', () => {
+        // Empty filter. Conflict is set to true by default.
+        component.validFilter = {}
+        expect(component.migrationFilterEntries).toEqual([['Conflict', 'false']])
+
+        component.validFilter = {
+            appId: 42,
+        }
+        expect(component.migrationFilterEntries).toEqual([
+            ['App Id', '42'],
+            ['Conflict', 'false'],
+        ])
+
+        component.validFilter = {
+            isGlobal: true,
+        }
+        expect(component.migrationFilterEntries).toEqual([
+            ['Conflict', 'false'],
+            ['Is Global', 'true'],
+        ])
+
+        component.validFilter = {
+            isGlobal: false,
+        }
+        expect(component.migrationFilterEntries).toEqual([
+            ['Conflict', 'false'],
+            ['Is Global', 'false'],
+        ])
+
+        component.validFilter = {
+            appId: 1,
+            subnetId: 1,
+            isGlobal: false,
+            text: 'foo',
+        }
+        expect(component.migrationFilterEntries).toEqual([
+            ['App Id', '1'],
+            ['Conflict', 'false'],
+            ['Is Global', 'false'],
+            ['Subnet Id', '1'],
+            ['Text', 'foo'],
+        ])
     })
 })
