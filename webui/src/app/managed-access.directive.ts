6d379872aa33fc4f021269c1fbed138c9d65b4a7
Piotrek Zadroga <piotrek@isc.org>
2025-05-12 15:25:28 +0200

    [#157] improve managed access directive

--- webui/src/app/managed-access.directive.ts
+++ webui/src/app/managed-access.directive.ts
@@ -50,25 +50,20 @@ export class ManagedAccessDirective implements AfterViewInit {
                 this.elementRef.nativeElement.classList.contains('p-element') ||
                 this.elementRef.nativeElement.nodeName.toUpperCase() === 'I'
             ) {
-                this.renderer.setProperty(this.elementRef.nativeElement, 'disabled', true)
                 this.renderer.addClass(this.elementRef.nativeElement, 'p-disabled')
-                this.renderer.setAttribute(this.elementRef.nativeElement, 'disabled', 'disabled')
-                this.renderer.setAttribute(this.elementRef.nativeElement, 'aria-disabled', 'disabled')
+                this.elementRef.nativeElement
+                    .querySelectorAll('.p-component')
+                    .forEach((el) => el.classList.add('p-disabled'))
+                this.elementRef.nativeElement
+                    .querySelectorAll('input,button')
+                    .forEach((el) => el.setAttribute('disabled', 'disabled'))
                 this.elementRef.nativeElement.querySelector('.p-checkbox')?.classList.add('p-checkbox-disabled')
-                this.elementRef.nativeElement.querySelector('.p-inputswitch')?.classList.add('p-disabled')
-                if (this.elementRef.nativeElement.querySelector('input')) {
-                    this.renderer.setAttribute(
-                        this.elementRef.nativeElement.querySelector('input'),
-                        'disabled',
-                        'disabled'
-                    )
-                }
             } else {
                 this.elementRef.nativeElement.innerHTML =
-                    '<div role="alert" class="p-messages p-component" aria-atomic="true" aria-live="assertive" data-pc-name="message">' +
+                    '<div role="alert" class="p-messages p-component">' +
                     '<div role="alert" class="p-message p-message-warn max-w-40rem">' +
-                    '<div class="p-message-wrapper" data-pc-section="wrapper">' +
-                    '<span class="p-message-summary" data-pc-section="summary" style="">You don\'t have ' +
+                    '<div class="p-message-wrapper">' +
+                    '<span class="p-message-summary">You don\'t have ' +
                     this.accessType +
                     ' privileges to display this UI component.<br>If you think this is unexpected, please contact your Stork system administrator.</span>' +
                     '</div>' +
