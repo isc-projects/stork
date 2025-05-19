374ea8c28746c84c2fdf7f84e37d204f89ba5c4b
Piotrek Zadroga <piotrek@isc.org>
2025-05-19 12:08:09 +0200

    [#157] add UTs

--- webui/src/app/managed-access.directive.spec.ts
+++ webui/src/app/managed-access.directive.spec.ts
@@ -6,26 +6,52 @@ import { AuthService } from './auth.service'
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { provideHttpClientTesting } from '@angular/common/http/testing'
 import { MessageService } from 'primeng/api'
+import { By } from '@angular/platform-browser'
 
 @Component({
     standalone: true,
-    template: `<p-button label="Create" appManagedAccess="subnet" accessType="create" />
-        <p-button label="Read" appManagedAccess="subnet" accessType="read" />
-        <p-button label="Update" appManagedAccess="subnet" accessType="update" />
-        <p-button label="Delete" appManagedAccess="subnet" accessType="delete" /> `,
+    template: `<p-button
+            label="Create"
+            appManagedAccess="subnet"
+            accessType="create"
+            (onClick)="wasCreated = true"
+            (hasAccess)="hasCreateAccess = $event"
+        />
+        <p-button
+            label="Read"
+            appManagedAccess="subnet"
+            accessType="read"
+            (onClick)="wasRead = true"
+            (hasAccess)="hasReadAccess = $event"
+        />
+        <p-button label="Update" appManagedAccess="subnet" accessType="update" (onClick)="wasUpdated = true" />
+        <p-button label="Delete" appManagedAccess="subnet" accessType="delete" (onClick)="wasDeleted = true" />
+        <div appManagedAccess="subnet" accessType="create">This is subnet creation form.</div>
+        <div appManagedAccess="subnet" accessType="delete" [hideOnNoAccess]="true">
+            This is subnet removal component.
+        </div> `,
     imports: [ManagedAccessDirective, Button],
 })
-class TestHostComponent {}
+class TestHostComponent {
+    wasCreated = false
+    wasRead = false
+    wasUpdated = false
+    wasDeleted = false
+    hasCreateAccess = false
+    hasReadAccess = false
+}
 
 describe('ManagedAccessDirective', () => {
     let hostComponent: TestHostComponent
     let fixture: ComponentFixture<TestHostComponent>
     let authService: AuthService
     let hasPrivilegeSpy: jasmine.Spy
-    let firstComponent: any
-    let secondComponent: any
-    let thirdComponent: any
-    let fourthComponent: any
+    let createBtn: any
+    let readBtn: any
+    let updateBtn: any
+    let deleteBtn: any
+    let createDiv: any
+    let deleteDiv: any
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -44,103 +70,59 @@ describe('ManagedAccessDirective', () => {
         hasPrivilegeSpy.withArgs('subnet', 'delete').and.returnValue(false)
 
         expect(fixture.debugElement.children).toBeTruthy()
-        expect(fixture.debugElement.children.length).toBeGreaterThanOrEqual(4)
-        firstComponent = fixture.debugElement.children[0].componentInstance as Button
-        secondComponent = fixture.debugElement.children[1].componentInstance as Button
-        thirdComponent = fixture.debugElement.children[2].componentInstance as Button
-        fourthComponent = fixture.debugElement.children[3].componentInstance as Button
+        expect(fixture.debugElement.children.length).toBeGreaterThanOrEqual(6)
+        createBtn = fixture.debugElement.children[0]
+        readBtn = fixture.debugElement.children[1]
+        updateBtn = fixture.debugElement.children[2]
+        deleteBtn = fixture.debugElement.children[3]
+        createDiv = fixture.debugElement.children[4]
+        deleteDiv = fixture.debugElement.children[5]
 
         fixture.detectChanges()
     })
 
     it('should create', () => {
         expect(hostComponent).toBeTruthy()
-        expect(firstComponent).toBeTruthy()
-        expect(secondComponent).toBeTruthy()
-        expect(thirdComponent).toBeTruthy()
-        expect(fourthComponent).toBeTruthy()
+        expect(createBtn).toBeTruthy()
+        expect(readBtn).toBeTruthy()
+        expect(updateBtn).toBeTruthy()
+        expect(deleteBtn).toBeTruthy()
+        expect(createDiv).toBeTruthy()
+        expect(deleteDiv).toBeTruthy()
     })
 
-    // it('should set access type', () => {
-    //     expect(firstComponent.accessType).toBe('write')
-    //     expect(secondComponent.accessType).toBe('read')
-    //     expect(thirdComponent.accessType).toBe('write')
-    // })
-    //
-    // it('should display content when no stork template was used', () => {
-    //     expect(firstComponent.templates.length).toBe(0)
-    //     expect(firstComponent.hasAccess).toBeTrue()
-    //     const headingDe = fixture.debugElement.query(By.css('h1'))
-    //     expect(headingDe).toBeTruthy()
-    //     expect(headingDe.nativeElement.textContent).toContain('Edit machine authorization')
-    // })
-    //
-    // it('should display hasAccess stork template content', () => {
-    //     expect(secondComponent.templates.length).toBe(1)
-    //     expect(secondComponent.hasAccess).toBeTrue()
-    //     const headingDe = fixture.debugElement.query(By.css('h2'))
-    //     expect(headingDe).toBeTruthy()
-    //     expect(headingDe.nativeElement.textContent).toContain('Full access')
-    // })
-    //
-    // it('should display no content when no access', () => {
-    //     hasPrivilegeSpy.withArgs('machine-authorization', 'write').and.returnValue(false)
-    //     firstComponent.ngOnInit()
-    //     fixture.detectChanges()
-    //     const headingDe = fixture.debugElement.query(By.css('h1'))
-    //     expect(headingDe).toBeFalsy()
-    // })
-    //
-    // it('should display default limited content when no access', () => {
-    //     hasPrivilegeSpy.withArgs('app-access-point-key', 'read').and.returnValue(false)
-    //     secondComponent.ngOnInit()
-    //     fixture.detectChanges()
-    //     const headingDe = fixture.debugElement.query(By.css('h2'))
-    //     expect(headingDe).toBeFalsy()
-    //     expect(fixture.debugElement.nativeElement.innerHTML).toContain("You don't have read privileges")
-    //     // Default template for limited access has pi-ban icon included.
-    //     expect(fixture.debugElement.query(By.css('.pi-ban'))).toBeTruthy()
-    // })
-    //
-    // it('should emit if user has access', () => {
-    //     expect(hostComponent.secondHasAccess).toBeTrue()
-    //     hasPrivilegeSpy.withArgs('app-access-point-key', 'read').and.returnValue(false)
-    //     secondComponent.ngOnInit()
-    //     fixture.detectChanges()
-    //     expect(hostComponent.secondHasAccess).toBeFalse()
-    // })
-    //
-    // it('should display stork template content when has access and template name does not match hasAccess name', () => {
-    //     // Third component uses appTemplate="foobar" stork template name.
-    //     expect(thirdComponent.templates.length).toBe(2)
-    //     expect(thirdComponent.hasAccess).toBeTrue()
-    //     const headingDe = fixture.debugElement.query(By.css('h3'))
-    //     expect(headingDe).toBeTruthy()
-    //     expect(headingDe.nativeElement.textContent).toContain('Write access')
-    // })
-    //
-    // it('should display limited content when noAccess template was provided even if hideOnNoAccess is true', () => {
-    //     hasPrivilegeSpy.withArgs('edit-host-reservation', 'write').and.returnValue(false)
-    //     thirdComponent.ngOnInit()
-    //     fixture.detectChanges()
-    //     expect(thirdComponent.hideOnNoAccess).toBeTrue()
-    //     const hasAccessHeadingDe = fixture.debugElement.query(By.css('h3'))
-    //     expect(hasAccessHeadingDe).toBeFalsy()
-    //     const headingDe = fixture.debugElement.query(By.css('h4'))
-    //     expect(headingDe).toBeTruthy()
-    //     expect(fixture.debugElement.nativeElement.innerHTML).toContain('Limited access')
-    // })
-    //
-    // it('should display dedicated noAccess template', () => {
-    //     hasPrivilegeSpy.withArgs('edit-host-reservation', 'write').and.returnValue(false)
-    //     thirdComponent.hideOnNoAccess = false
-    //     thirdComponent.ngOnInit()
-    //     fixture.detectChanges()
-    //     expect(thirdComponent.hideOnNoAccess).toBeFalse()
-    //     const hasAccessHeadingDe = fixture.debugElement.query(By.css('h3'))
-    //     expect(hasAccessHeadingDe).toBeFalsy()
-    //     const headingDe = fixture.debugElement.query(By.css('h4'))
-    //     expect(headingDe).toBeTruthy()
-    //     expect(fixture.debugElement.nativeElement.innerHTML).toContain('Limited access')
-    // })
+    it('should display no content when no access and hide on no access flag was used', () => {
+        expect(deleteDiv).toBeTruthy()
+        expect(deleteDiv.nativeElement.innerText).toBeFalsy()
+    })
+
+    it('should display feedback message about lack of privileges', () => {
+        expect(createDiv).toBeTruthy()
+        expect(createDiv.nativeElement.innerText).toContain("You don't have create privileges")
+    })
+
+    it('should emit if user has access', () => {
+        expect(hostComponent.hasReadAccess).toBeTrue()
+        expect(hostComponent.hasCreateAccess).toBeFalse()
+    })
+
+    it('should disable elements when no access', () => {
+        expect(createBtn.query(By.css('button'))).toBeTruthy()
+        expect(updateBtn.query(By.css('button'))).toBeTruthy()
+        expect(deleteBtn.query(By.css('button'))).toBeTruthy()
+        createBtn.query(By.css('button')).nativeElement.click()
+        updateBtn.query(By.css('button')).nativeElement.click()
+        deleteBtn.query(By.css('button')).nativeElement.click()
+        fixture.detectChanges()
+        expect(hostComponent.wasCreated).toBeFalse()
+        expect(hostComponent.wasUpdated).toBeFalse()
+        expect(hostComponent.wasDeleted).toBeFalse()
+    })
+
+    it('should not disable elements when has access', () => {
+        expect(readBtn.query(By.css('button'))).toBeTruthy()
+        readBtn.query(By.css('button')).nativeElement.click()
+        fixture.detectChanges()
+        expect(hostComponent.wasRead).toBeTrue()
+    })
 })
