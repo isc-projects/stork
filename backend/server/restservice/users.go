3f41a3f8f1ce2567b2d41297d25d13b7ca52d7ad
Piotrek Zadroga <piotrek@isc.org>
2025-05-22 06:43:37 +0000

    [#157] reword comment

--- backend/server/restservice/users.go
+++ backend/server/restservice/users.go
@@ -483,8 +483,8 @@ func (r *RestAPI) UpdateUser(ctx context.Context, params users.UpdateUserParams)
 		}
 	}
 
-	// TODO: consider a case where users privileges were changed. E.g. a user was moved from admin
-	// group to read-only group. Session could be destroyed for that user and the user would be
+	// TODO: Consider a case where a user's privileges were changed. For example, a user was moved from the admin
+	// group to the read-only group. The session could be destroyed for that user, who would be
 	// forced to authenticate again. New privileges would be applied for the new session.
 
 	return users.NewUpdateUserOK()
