747be36b672aaf560a465201b2c8c9b72ff3f3ba
Slawek Figiel <slawek@isc.org>
2025-05-23 18:50:15 +0200

    [#1363] Simplify migration file

--- backend/server/database/migrations/65_pool_stats.go
+++ backend/server/database/migrations/65_pool_stats.go
@@ -5,30 +5,22 @@ import "github.com/go-pg/migrations/v8"
 func init() {
 	migrations.MustRegisterTx(func(db migrations.DB) error {
 		_, err := db.Exec(`
-			-- Add new columns to store statistics for address and prefix pools.
-			ALTER TABLE public.address_pool
-				ADD COLUMN stats JSONB;
-			ALTER TABLE public.address_pool
-				ADD COLUMN stats_collected_at TIMESTAMP WITHOUT TIME ZONE;
-			ALTER TABLE public.prefix_pool
-				ADD COLUMN stats JSONB;
-			ALTER TABLE public.prefix_pool
-				ADD COLUMN stats_collected_at TIMESTAMP WITHOUT TIME ZONE;
-
-			-- Add new columns to store utilizations.
-			ALTER TABLE public.address_pool
-				ADD COLUMN utilization SMALLINT DEFAULT 0;
-			ALTER TABLE public.prefix_pool
-				ADD COLUMN utilization SMALLINT DEFAULT 0;
-
+			-- Add new columns to store statistics and utilizations for address
+			-- and prefix pools.
 			-- Make sure that when stats are stored then timestamp is also set.
-			ALTER TABLE address_pool
+			ALTER TABLE public.address_pool
+				ADD COLUMN stats JSONB,
+				ADD COLUMN stats_collected_at TIMESTAMP WITHOUT TIME ZONE,
+				ADD COLUMN utilization SMALLINT DEFAULT 0,
 				ADD CONSTRAINT stats_and_stats_collected_at_both_not_null CHECK (
 					(stats IS NOT NULL AND stats_collected_at IS NOT NULL)
 					OR
 					(stats IS NULL AND stats_collected_at IS NULL)
 				);
-			ALTER TABLE prefix_pool
+			ALTER TABLE public.prefix_pool
+				ADD COLUMN stats JSONB,
+				ADD COLUMN stats_collected_at TIMESTAMP WITHOUT TIME ZONE,
+				ADD COLUMN utilization SMALLINT DEFAULT 0,
 				ADD CONSTRAINT stats_and_stats_collected_at_both_not_null CHECK (
 					(stats IS NOT NULL AND stats_collected_at IS NOT NULL)
 					OR
@@ -39,22 +31,14 @@ func init() {
 	}, func(db migrations.DB) error {
 		_, err := db.Exec(`
 			ALTER TABLE public.address_pool
-				DROP CONSTRAINT IF EXISTS stats_and_stats_collected_at_both_not_null;
-			ALTER TABLE public.prefix_pool
-				DROP CONSTRAINT IF EXISTS stats_and_stats_collected_at_both_not_null;
-
-			ALTER TABLE public.address_pool
-				DROP COLUMN IF EXISTS utilization;
-			ALTER TABLE public.prefix_pool
-				DROP COLUMN IF EXISTS utilization;
-
-			ALTER TABLE public.address_pool
-				DROP COLUMN IF EXISTS stats;
-			ALTER TABLE public.address_pool
+				DROP CONSTRAINT IF EXISTS stats_and_stats_collected_at_both_not_null,
+				DROP COLUMN IF EXISTS utilization,
+				DROP COLUMN IF EXISTS stats,
 				DROP COLUMN IF EXISTS stats_collected_at;
 			ALTER TABLE public.prefix_pool
-				DROP COLUMN IF EXISTS stats;
-			ALTER TABLE public.prefix_pool
+				DROP CONSTRAINT IF EXISTS stats_and_stats_collected_at_both_not_null,
+				DROP COLUMN IF EXISTS utilization,
+				DROP COLUMN IF EXISTS stats,
 				DROP COLUMN IF EXISTS stats_collected_at;
 		`)
 		return err
