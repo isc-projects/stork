8c7fe40b3c6a544fcd49eff491756ab6938d89b2
Slawek Figiel <slawek@isc.org>
2025-05-29 12:12:08 +0200

    [#1816] Fix unit tests

--- webui/src/app/not-found-page/not-found-page.component.spec.ts
+++ webui/src/app/not-found-page/not-found-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { NotFoundPageComponent } from './not-found-page.component'
+import { MessageModule } from 'primeng/message'
 
 describe('NotFoundPageComponent', () => {
     let component: NotFoundPageComponent
@@ -8,6 +9,7 @@ describe('NotFoundPageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [MessageModule],
             declarations: [NotFoundPageComponent],
         }).compileComponents()
 
