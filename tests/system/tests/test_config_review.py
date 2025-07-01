74c13bb84e0e26798a1b1baec42f42b89900bb26
Slawek Figiel <slawek@isc.org>
2025-07-01 14:14:10 +0200

    [#1872] Fix linter issue

--- tests/system/tests/test_config_review.py
+++ tests/system/tests/test_config_review.py
@@ -53,9 +53,7 @@ def test_get_dhcp_config_review_reports(server_service: Server, kea_service: Kea
 
 
 @ha_parametrize("agent-kea-ha1-only-top-mt", "agent-kea-ha2-only-top-mt")
-def test_get_ha_only_top_mt_config_review_reports(
-    server_service: Server, ha_service
-):
+def test_get_ha_only_top_mt_config_review_reports(server_service: Server, ha_service):
     """Test that the Stork server suggests to enable the HA multi-threading
     if the Kea is running in the multi-threading mode."""
     server_service.log_in_as_admin()
