398f919e63cf33c40196586678f0a78e05b5eb80
Slawek Figiel <slawek@isc.org>
2025-07-16 12:09:39 +0200

    [#1872] Drop unused column

--- backend/server/database/migrations/66_drop_service_type.go
+++ backend/server/database/migrations/66_drop_service_type.go
@@ -9,10 +9,19 @@ func init() {
 			DROP TRIGGER ha_service_before_insert_update ON ha_service;
 			-- Drop the function that is no longer called.
 			DROP FUNCTION ha_service_type_set();
+			-- Drop the column that is no used anymore.
+			ALTER TABLE service DROP COLUMN service_type;
+			-- Drop the service type enum.
+			DROP TYPE SERVICETYPE;
 		`)
 		return err
 	}, func(db migrations.DB) error {
 		_, err := db.Exec(`
+			-- Recreate a service type column.
+			CREATE TYPE SERVICETYPE AS ENUM ('ha_dhcp');
+			ALTER TABLE service ADD COLUMN service_type SERVICETYPE;
+			UPDATE service SET service_type = 'ha_dhcp';
+
 			-- Create the function.
 			CREATE FUNCTION ha_service_type_set() RETURNS TRIGGER AS $$
 			BEGIN
