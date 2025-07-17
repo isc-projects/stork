10336a10ffffec70e72eb0e6621d14c13a1a2d33
Piotrek Zadroga <piotrek@isc.org>
2025-07-17 13:00:50 +0200

    [#1911] simplify component

--- webui/src/app/dhcp-client-class-set-form/dhcp-client-class-set-form.component.ts
+++ webui/src/app/dhcp-client-class-set-form/dhcp-client-class-set-form.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 import { UntypedFormControl } from '@angular/forms'
 import { v4 as uuidv4 } from 'uuid'
-import { OverlayPanel } from 'primeng/overlaypanel'
 import { SelectableClientClass } from '../forms/selectable-client-class'
 import { AutoCompleteCompleteEvent } from 'primeng/autocomplete'
 
@@ -19,12 +18,6 @@ import { AutoCompleteCompleteEvent } from 'primeng/autocomplete'
     styleUrls: ['./dhcp-client-class-set-form.component.sass'],
 })
 export class DhcpClientClassSetFormComponent implements OnInit {
-    /**
-     * Reference to the overlay panel holding a list of classes.
-     */
-    @ViewChild('op')
-    classSelectionPanel: OverlayPanel
-
     /**
      * A form bound to the "chips" input box holding the list of selected
      * class names.
@@ -46,11 +39,6 @@ export class DhcpClientClassSetFormComponent implements OnInit {
      * A sorted list of classes that can be selected in the overlay.
      */
     sortedClientClasses: SelectableClientClass[] = []
-
-    /**
-     * An array of the selected class names in the overlay panel.
-     */
-    selectedClientClasses: string[] = []
 
     /**
      * A list of classes to be displayed as suggested options in PrimeNG AutoComplete input component.
@@ -86,89 +74,6 @@ export class DhcpClientClassSetFormComponent implements OnInit {
         this.sortedClientClasses.sort((c1, c2) => {
             return c1.name.localeCompare(c2.name)
         })
-    }
-
-    /**
-     * Checks if the given class is in the "chips" input box.
-     *
-     * This function is called to determine whether the checkbox for the given
-     * class should be disabled in the overlay panel. It is not possible to
-     * remove a class from the input box by deselecting it in the overlay
-     * panel.
-     *
-     * @param clientClass client class name
-     * @returns true if the class is present in the input box, false otherwise.
-     */
-    isUsed(clientClass: string): boolean {
-        const value = this.classFormControl.value as Array<string>
-        return !!value && value.includes(clientClass)
-    }
-
-    /**
-     * Inserts a list of client classes selected in the overlay panel.
-     *
-     * It iterates over the list of selected classes. For each class, it
-     * checks if it is already present in the input box. If the class is
-     * not present, it is added to the input box. Finally, it clears the
-     * list of selected classes and hides the overlay panel.
-     */
-    mergeSelected(): void {
-        let value = (this.classFormControl.value as Array<string>) || []
-        for (let selectedClass of this.selectedClientClasses) {
-            if (value.indexOf(selectedClass) < 0) {
-                value.push(selectedClass)
-            }
-        }
-        this.classFormControl.patchValue(value)
-        this.selectedClientClasses = []
-        this.classSelectionPanel.hide()
-    }
-
-    /**
-     * Cancels selecting the classes from the overlay panel.
-     *
-     * It clears the selected classes list and hides the panel. No classes
-     * are inserted into the input box.
-     */
-    cancelSelected(): void {
-        this.selectedClientClasses = []
-        this.classSelectionPanel.hide()
-    }
-
-    /**
-     * Populates the list of selected classes in the overlay panel.
-     *
-     * It matches the class names present in the input box with the sorted
-     * list of classes. It inserts a class into the selected class list when
-     * it is present in the input box. It effectively checks these classes on
-     * the checkbox list.
-     */
-    private _fillSelected(): void {
-        const value = this.classFormControl.value as Array<string>
-        if (!value || value.length === 0) {
-            return
-        }
-        let selectedClasses: string[] = []
-        for (let clientClass of value) {
-            if (
-                this.sortedClientClasses.some((c) => c.name === clientClass) &&
-                selectedClasses.indexOf(clientClass) < 0
-            ) {
-                selectedClasses.push(clientClass)
-            }
-        }
-        this.selectedClientClasses = selectedClasses
-    }
-
-    /**
-     * Populates selected classes and shows the overlay panel.
-     *
-     * @param event event resulting from clicking the button to show the
-     * overlay panel.
-     */
-    showClassSelectionPanel(event): void {
-        this._fillSelected()
-        this.classSelectionPanel.toggle(event)
     }
 
     /**
