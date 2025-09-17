94b3b7ebd3617488b7fe5b4eb749019e66c9e8ed
Piotrek Zadroga <piotrek@isc.org>
2025-09-17 17:21:49 +0200

    [#1760] tri-state-checkbox refactor

--- webui/src/app/tri-state-checkbox/tri-state-checkbox.component.ts
+++ webui/src/app/tri-state-checkbox/tri-state-checkbox.component.ts
@@ -1,21 +1,47 @@
 import { Component, input, model } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { Checkbox } from 'primeng/checkbox'
+import { CheckIcon, TimesIcon } from 'primeng/icons'
 
+/**
+ * This component is an HTML input type=checkbox implementation which allows to hold and visualize 3 states:
+ * - true (checkbox is filled with a tick icon)
+ * - false (checkbox is filled with an x-cross icon)
+ * - null (checkbox is empty - not filled).
+ */
 @Component({
     selector: 'app-tri-state-checkbox',
     standalone: true,
-    imports: [FormsModule, Checkbox],
+    imports: [FormsModule, Checkbox, CheckIcon, TimesIcon],
     templateUrl: './tri-state-checkbox.component.html',
     styleUrl: './tri-state-checkbox.component.sass',
 })
 export class TriStateCheckboxComponent {
+    /**
+     * Holds input value. Also emits the value whenever value changes.
+     */
     value = model<boolean | null>(null)
+
+    /**
+     * ID of the html input.
+     */
     inputID = input<string | undefined>(undefined)
+
+    /**
+     * Flag controlling input disabled state. Defaults to false.
+     */
     disabled = input<boolean>(false)
+
+    /**
+     * Label of the checkbox.
+     */
     label = input<string | undefined>(undefined)
 
-    toggleValues() {
+    /**
+     * Toggles input value in a chain true->false->null->true->...
+     * @private
+     */
+    private toggleValue() {
         if (this.value() === true) {
             this.value.set(false)
         } else if (this.value() === false) {
@@ -25,17 +51,22 @@ export class TriStateCheckboxComponent {
         }
     }
 
-    onClick() {
-        this.toggleValues()
+    /**
+     * Callback called when the checkbox is clicked.
+     * @param event
+     */
+    onClick(event: MouseEvent) {
+        this.toggleValue()
+        event.preventDefault()
     }
 
     /**
-     *
+     * Callback called when the keyboard event happened while the focus was on the checkbox input.
      * @param event
      */
     onKeyDown(event: KeyboardEvent) {
         if (event.key === 'Enter' || event.key === 'Space') {
-            this.toggleValues()
+            this.toggleValue()
             event.preventDefault()
         }
     }
