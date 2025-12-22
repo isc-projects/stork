2edae571eb85213f4b55461a5eb4615ca5051fbf
Piotrek Zadroga <piotrek@isc.org>
2025-12-22 22:45:03 +0100

    [#2052] new zones page story

---
+++ webui/src/app/zones-page/zones-page.component.stories.ts
@@ -0,0 +1,450 @@
+import { ZonesPageComponent } from './zones-page.component'
+import { applicationConfig, Meta, moduleMetadata, StoryObj } from '@storybook/angular'
+import { ConfirmationService } from 'primeng/api'
+import { BreadcrumbsComponent } from '../breadcrumbs/breadcrumbs.component'
+import { provideRouter, RouterModule, withHashLocation } from '@angular/router'
+import { DialogModule } from 'primeng/dialog'
+import { ButtonModule } from 'primeng/button'
+import { TableModule } from 'primeng/table'
+import { ConfirmDialogModule } from 'primeng/confirmdialog'
+import { TabViewComponent } from '../tab-view/tab-view.component'
+import { BreadcrumbModule } from 'primeng/breadcrumb'
+import { HelpTipComponent } from '../help-tip/help-tip.component'
+import { PluralizePipe } from '../pipes/pluralize.pipe'
+import { MessageModule } from 'primeng/message'
+import { PopoverModule } from 'primeng/popover'
+import { PanelModule } from 'primeng/panel'
+import { TooltipModule } from 'primeng/tooltip'
+import { FloatLabelModule } from 'primeng/floatlabel'
+import { MultiSelectModule } from 'primeng/multiselect'
+import { InputNumberModule } from 'primeng/inputnumber'
+import { SelectModule } from 'primeng/select'
+import { InputIconModule } from 'primeng/inputicon'
+import { IconFieldModule } from 'primeng/iconfield'
+import { toastDecorator } from '../utils-stories'
+import { ToastModule } from 'primeng/toast'
+import { FormsModule } from '@angular/forms'
+import { InputTextModule } from 'primeng/inputtext'
+import { TagModule } from 'primeng/tag'
+import { LocaltimePipe } from '../pipes/localtime.pipe'
+import { PlaceholderPipe } from '../pipes/placeholder.pipe'
+import { UnrootPipe } from '../pipes/unroot.pipe'
+
+const meta: Meta<ZonesPageComponent> = {
+    title: 'App/ZonesPage',
+    component: ZonesPageComponent,
+    decorators: [
+        applicationConfig({
+            providers: [
+                provideRouter(
+                    [
+                        {
+                            path: 'dns/zones/:id',
+                            component: ZonesPageComponent,
+                        },
+                    ],
+                    withHashLocation()
+                ),
+            ],
+        }),
+        moduleMetadata({
+            declarations: [
+                BreadcrumbsComponent,
+                HelpTipComponent,
+                PluralizePipe,
+                LocaltimePipe,
+                PlaceholderPipe,
+                UnrootPipe,
+            ],
+            imports: [
+                RouterModule,
+                DialogModule,
+                ButtonModule,
+                TableModule,
+                ConfirmDialogModule,
+                TabViewComponent,
+                BreadcrumbModule,
+                MessageModule,
+                PopoverModule,
+                PanelModule,
+                TooltipModule,
+                FloatLabelModule,
+                MultiSelectModule,
+                InputNumberModule,
+                SelectModule,
+                InputIconModule,
+                IconFieldModule,
+                ToastModule,
+                FormsModule,
+                InputTextModule,
+                TagModule,
+            ],
+            providers: [ConfirmationService],
+        }),
+        toastDecorator,
+    ],
+}
+
+export default meta
+type Story = StoryObj<ZonesPageComponent>
+
+export const EmptyList: Story = {
+    parameters: {
+        mockData: [
+            {
+                url: 'http://localhost/api/dns-management/zones-fetch',
+                method: 'GET',
+                status: 200,
+                response: () => ({ total: 0, items: [] }),
+            },
+            {
+                url: 'http://localhost/api/dns-management/zones-fetch',
+                method: 'PUT',
+                status: 202,
+                response: () => {},
+            },
+            {
+                url: 'http://localhost/api/zones?start=:start&limit=:limit&zoneType=consumer&zoneType=delegation-only&zoneType=forward&zoneType=hint&zoneType=mirror&zoneType=native&zoneType=primary&zoneType=producer&zoneType=redirect&zoneType=secondary&zoneType=static-stub&zoneType=stub',
+                method: 'GET',
+                status: 200,
+                response: () => ({ total: 0, items: [] }),
+            },
+            {
+                url: 'http://localhost/api/zones?start=:start&limit=:limit&zoneType=builtin&zoneType=consumer&zoneType=delegation-only&zoneType=forward&zoneType=hint&zoneType=mirror&zoneType=native&zoneType=primary&zoneType=producer&zoneType=redirect&zoneType=secondary&zoneType=static-stub&zoneType=stub',
+                method: 'GET',
+                status: 200,
+                response: () => ({ total: 0, items: [] }),
+            },
+        ],
+    },
+}
+
+const builtinZones = [
+    {
+        id: 20,
+        localZones: [
+            {
+                appId: 2,
+                appName: 'bind9@agent-bind9-2',
+                class: 'IN',
+                daemonId: 2,
+                loadedAt: '2025-12-22T18:31:41.000Z',
+                serial: 0,
+                view: '_default',
+                zoneType: 'builtin',
+            },
+            {
+                appId: 4,
+                appName: 'bind9@agent-bind9',
+                class: 'IN',
+                daemonId: 4,
+                loadedAt: '2025-12-22T18:31:41.000Z',
+                serial: 0,
+                view: 'guest',
+                zoneType: 'builtin',
+            },
+            {
+                appId: 4,
+                appName: 'bind9@agent-bind9',
+                class: 'IN',
+                daemonId: 4,
+                loadedAt: '2025-12-22T18:31:41.000Z',
+                serial: 0,
+                view: 'trusted',
+                zoneType: 'builtin',
+            },
+        ],
+        name: 'EMPTY.AS112.ARPA',
+        rname: 'ARPA.AS112.EMPTY',
+    },
+    {
+        id: 21,
+        localZones: [
+            {
+                appId: 2,
+                appName: 'bind9@agent-bind9-2',
+                class: 'IN',
+                daemonId: 2,
+                loadedAt: '2025-12-22T18:31:41.000Z',
+                serial: 0,
+                view: '_default',
+                zoneType: 'builtin',
+            },
+            {
+                appId: 4,
+                appName: 'bind9@agent-bind9',
+                class: 'IN',
+                daemonId: 4,
+                loadedAt: '2025-12-22T18:31:41.000Z',
+                serial: 0,
+                view: 'guest',
+                zoneType: 'builtin',
+            },
+            {
+                appId: 4,
+                appName: 'bind9@agent-bind9',
+                class: 'IN',
+                daemonId: 4,
+                loadedAt: '2025-12-22T18:31:41.000Z',
+                serial: 0,
+                view: 'trusted',
+                zoneType: 'builtin',
+            },
+        ],
+        name: 'HOME.ARPA',
+        rname: 'ARPA.HOME',
+    },
+    {
+        id: 22,
+        localZones: [
+            {
+                appId: 2,
+                appName: 'bind9@agent-bind9-2',
+                class: 'IN',
+                daemonId: 2,
+                loadedAt: '2025-12-22T18:31:41.000Z',
+                serial: 0,
+                view: '_default',
+                zoneType: 'builtin',
+            },
+            {
+                appId: 4,
+                appName: 'bind9@agent-bind9',
+                class: 'IN',
+                daemonId: 4,
+                loadedAt: '2025-12-22T18:31:41.000Z',
+                serial: 0,
+                view: 'guest',
+                zoneType: 'builtin',
+            },
+            {
+                appId: 4,
+                appName: 'bind9@agent-bind9',
+                class: 'IN',
+                daemonId: 4,
+                loadedAt: '2025-12-22T18:31:41.000Z',
+                serial: 0,
+                view: 'trusted',
+                zoneType: 'builtin',
+            },
+        ],
+        name: '0.IN-ADDR.ARPA',
+        rname: 'ARPA.IN-ADDR.0',
+    },
+]
+
+const rootZone = {
+    id: 15,
+    localZones: [
+        {
+            appId: 2,
+            appName: 'bind9@agent-bind9-2',
+            class: 'IN',
+            daemonId: 2,
+            loadedAt: '1970-01-01T00:00:00.000Z',
+            serial: 0,
+            view: '_default',
+            zoneType: 'mirror',
+        },
+    ],
+    name: '.',
+    rname: '.',
+}
+
+const masterZones = [
+    {
+        id: 1,
+        localZones: [
+            {
+                appId: 1,
+                appName: 'pdns@agent-pdns',
+                class: 'IN',
+                daemonId: 1,
+                loadedAt: '2025-12-22T17:59:38.000Z',
+                serial: 2024031501,
+                view: 'localhost',
+                zoneType: 'master',
+            },
+        ],
+        name: '0.0.10.in-addr.arpa',
+        rname: 'arpa.in-addr.10.0.0',
+    },
+    {
+        id: 2,
+        localZones: [
+            {
+                appId: 1,
+                appName: 'pdns@agent-pdns',
+                class: 'IN',
+                daemonId: 1,
+                loadedAt: '2025-12-22T17:59:38.000Z',
+                serial: 2024031501,
+                view: 'localhost',
+                zoneType: 'master',
+            },
+        ],
+        name: '1.0.10.in-addr.arpa',
+        rname: 'arpa.in-addr.10.0.1',
+    },
+    {
+        id: 3,
+        localZones: [
+            {
+                appId: 1,
+                appName: 'pdns@agent-pdns',
+                class: 'IN',
+                daemonId: 1,
+                loadedAt: '2025-12-22T17:59:38.000Z',
+                serial: 2024031501,
+                view: 'localhost',
+                zoneType: 'master',
+            },
+        ],
+        name: '2.0.10.in-addr.arpa',
+        rname: 'arpa.in-addr.10.0.2',
+    },
+    {
+        id: 4,
+        localZones: [
+            {
+                appId: 1,
+                appName: 'pdns@agent-pdns',
+                class: 'IN',
+                daemonId: 1,
+                loadedAt: '2025-12-22T17:59:38.000Z',
+                serial: 2024031501,
+                view: 'localhost',
+                zoneType: 'master',
+            },
+        ],
+        name: '3.0.10.in-addr.arpa',
+        rname: 'arpa.in-addr.10.0.3',
+    },
+    {
+        id: 5,
+        localZones: [
+            {
+                appId: 1,
+                appName: 'pdns@agent-pdns',
+                class: 'IN',
+                daemonId: 1,
+                loadedAt: '2025-12-22T17:59:38.000Z',
+                serial: 2024031501,
+                view: 'localhost',
+                zoneType: 'master',
+            },
+        ],
+        name: '0.16.172.in-addr.arpa',
+        rname: 'arpa.in-addr.172.16.0',
+    },
+    {
+        id: 6,
+        localZones: [
+            {
+                appId: 1,
+                appName: 'pdns@agent-pdns',
+                class: 'IN',
+                daemonId: 1,
+                loadedAt: '2025-12-22T17:59:38.000Z',
+                serial: 2024031501,
+                view: 'localhost',
+                zoneType: 'master',
+            },
+        ],
+        name: '1.16.172.in-addr.arpa',
+        rname: 'arpa.in-addr.172.16.1',
+    },
+]
+
+export const ListZones: Story = {
+    parameters: {
+        mockData: [
+            {
+                url: 'http://localhost/api/dns-management/zones-fetch',
+                method: 'GET',
+                status: 200,
+                response: () => ({
+                    total: 3,
+                    items: [
+                        {
+                            appId: 1,
+                            appName: 'pdns@agent-pdns',
+                            builtinZonesCount: 0,
+                            createdAt: '2025-12-22T17:22:46.009Z',
+                            daemonId: 1,
+                            distinctZonesCount: 10,
+                            status: 'ok',
+                            zoneConfigsCount: 10,
+                        },
+                        {
+                            appId: 2,
+                            appName: 'bind9@agent-bind9-2',
+                            builtinZonesCount: 104,
+                            createdAt: '2025-12-22T17:22:46.022Z',
+                            daemonId: 2,
+                            distinctZonesCount: 109,
+                            status: 'ok',
+                            zoneConfigsCount: 109,
+                        },
+                        {
+                            appId: 4,
+                            appName: 'bind9@agent-bind9',
+                            builtinZonesCount: 104,
+                            createdAt: '2025-12-22T17:22:46.040Z',
+                            daemonId: 5,
+                            distinctZonesCount: 107,
+                            status: 'ok',
+                            zoneConfigsCount: 207,
+                        },
+                    ],
+                }),
+            },
+            {
+                url: 'http://localhost/api/dns-management/zones-fetch',
+                method: 'PUT',
+                status: 202,
+                response: () => {},
+            },
+            {
+                url: 'http://localhost/api/zones?start=:start&limit=:limit&zoneType=consumer&zoneType=delegation-only&zoneType=forward&zoneType=hint&zoneType=mirror&zoneType=native&zoneType=primary&zoneType=producer&zoneType=redirect&zoneType=secondary&zoneType=static-stub&zoneType=stub',
+                method: 'GET',
+                status: 200,
+                response: () => ({
+                    items: [rootZone, ...masterZones],
+                    total: 7,
+                }),
+            },
+            {
+                url: 'http://localhost/api/zones?start=:start&limit=:limit&zoneType=builtin&zoneType=consumer&zoneType=delegation-only&zoneType=forward&zoneType=hint&zoneType=mirror&zoneType=native&zoneType=primary&zoneType=producer&zoneType=redirect&zoneType=secondary&zoneType=static-stub&zoneType=stub',
+                method: 'GET',
+                status: 200,
+                response: () => ({
+                    items: [rootZone, ...builtinZones, ...masterZones],
+                    total: 10,
+                }),
+            },
+            {
+                url: 'http://localhost/api/zones?start=:start&limit=:limit&zoneType=consumer&zoneType=delegation-only&zoneType=forward&zoneType=hint&zoneType=mirror&zoneType=native&zoneType=primary&zoneType=producer&zoneType=redirect&zoneType=secondary&zoneType=static-stub&zoneType=stub&appId=:appId',
+                method: 'GET',
+                status: 200,
+                response: (req) => {
+                    const filteredZones = [rootZone, ...masterZones].filter((z) => {
+                        return z.localZones.some((lz) => lz.appId == req?.searchParams?.appId ?? 0)
+                    })
+                    return { items: filteredZones, total: filteredZones.length }
+                },
+            },
+            {
+                url: 'http://localhost/api/zones?start=:start&limit=:limit&zoneType=builtin&zoneType=consumer&zoneType=delegation-only&zoneType=forward&zoneType=hint&zoneType=mirror&zoneType=native&zoneType=primary&zoneType=producer&zoneType=redirect&zoneType=secondary&zoneType=static-stub&zoneType=stub&appId=:appId',
+                method: 'GET',
+                status: 200,
+                response: (req) => {
+                    const filteredZones = [rootZone, ...builtinZones, ...masterZones].filter((z) => {
+                        return z.localZones.some((lz) => lz.appId == req?.searchParams?.appId ?? 0)
+                    })
+                    return { items: filteredZones, total: filteredZones.length }
+                },
+            },
+        ],
+    },
+}
