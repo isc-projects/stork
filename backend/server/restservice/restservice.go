7d9983613e80b29ca60f01f20a92e45944e48f8b
Slawek Figiel <slawek@isc.org>
2026-01-30 17:53:43 +0100

    [#2250] Add TLS1.3 ciphers

--- backend/server/restservice/restservice.go
+++ backend/server/restservice/restservice.go
@@ -257,6 +257,9 @@ func prepareTLS(httpServer *http.Server, s *RestAPISettings) error {
 		httpServer.TLSConfig.MinVersion = tls.VersionTLS12
 		// These cipher suites support Forward Secrecy: https://en.wikipedia.org/wiki/Forward_secrecy
 		httpServer.TLSConfig.CipherSuites = []uint16{
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
