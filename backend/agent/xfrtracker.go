e79f8752904422373300b04c1172123cb6641be5
Marcin Siodelski <marcin@isc.org>
2026-04-15 12:35:00 +0200

    [#2393] Zone transfer logs parsing

--- backend/agent/xfrtracker.go
+++ backend/agent/xfrtracker.go
@@ -1,15 +1,42 @@
 package agent
 
 import (
+	"container/list"
 	"context"
+	"iter"
+	"slices"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
 // The default number of days to look into the past for the XFR tracking
 // when the tracker starts analyzing the systemd logs.
-const defaultXfrTrackingSinceDaysAgo = 1
+const (
+	defaultXfrTrackingSinceDaysAgo = 1
+	defaultXfrTrackingMaxStates    = 1000
+)
+
+type xfrStatus int
+
+const (
+	xfrStatusUnknown xfrStatus = iota
+	xfrStatusStarted
+	xfrStatusCompleted
+	xfrStatusMessage
+)
+
+type xfrTimeFormat int
+
+const (
+	xfrTimeFormatRFC3339 xfrTimeFormat = iota
+	xfrTimeFormatBind9
+	xfrTimeFormatUnknown
+)
 
 // Zone transfer tracker uses the underlying log tracker to subscribe to the
 // logs containing messages marking the beginning and end of the zone transfers
@@ -38,7 +65,34 @@ type xfrTracker struct {
 	cancelFn context.CancelFunc
 	// The channel used to wait for the cancellation of the goroutine that consumes the
 	// log lines.
-	cancelCh chan struct{}
+	cancelCh      chan struct{}
+	startedList   list.List
+	startedMap    map[xfrStateKey]*list.Element
+	completedList list.List
+	maxStates     int
+	mutex         sync.RWMutex
+}
+
+type xfrState struct {
+	viewName       string
+	zoneName       string
+	serial         int64
+	client         string
+	server         string
+	messagesCount  int64
+	recordsCount   int64
+	bytesCount     int64
+	duration       time.Duration
+	status         xfrStatus
+	startTime      time.Time
+	completionTime time.Time
+	timeFormat     xfrTimeFormat
+	message        string
+}
+type xfrStateKey struct {
+	viewName string
+	zoneName string
+	client   string
 }
 
 // Instantiates a new XFR tracker instance. It is associated with the log
@@ -47,6 +101,8 @@ type xfrTracker struct {
 func newXfrTracker(logTracker *logTracker) *xfrTracker {
 	return &xfrTracker{
 		logTracker: logTracker,
+		startedMap: make(map[xfrStateKey]*list.Element),
+		maxStates:  defaultXfrTrackingMaxStates,
 	}
 }
 
@@ -77,16 +133,16 @@ func (t *xfrTracker) track() {
 			select {
 			case <-ctx.Done():
 				return
-			case _, ok := <-chan0:
+			case line, ok := <-chan0:
 				if !ok {
 					return
 				}
-				// TODO: Process the XFR request from this channel.
-			case _, ok := <-chan1:
+				t.feed(line.text)
+			case line, ok := <-chan1:
 				if !ok {
 					return
 				}
-				// TODO: Process the XFR request from this channel.
+				t.feed(line.text)
 			}
 		}
 	}()
@@ -160,4 +216,345 @@ func (t *xfrTracker) stop() {
 		}
 	}
 	t.subscribers = nil
+}
+
+func parseTime(tokens []string) (time.Time, xfrTimeFormat) {
+	if len(tokens) == 0 {
+		return time.Time{}, xfrTimeFormatUnknown
+	}
+	if len(tokens) > 1 {
+		localTime := strings.Join(tokens[:2], " ")
+		parsedTime, err := time.ParseInLocation("02-Jan-2006 15:04:05.000", localTime, time.Local)
+		if err == nil {
+			return parsedTime, xfrTimeFormatBind9
+		}
+	}
+	parsedTime, err := time.Parse(time.RFC3339, tokens[0])
+	if err == nil {
+		return parsedTime, xfrTimeFormatRFC3339
+	}
+	return time.Time{}, xfrTimeFormatUnknown
+}
+
+func pull(tokens []string) (func(n int) (string, bool, int), func()) {
+	seq := slices.Values(tokens)
+	next, stop := iter.Pull(seq)
+	index := 0
+	return func(n int) (string, bool, int) {
+		var (
+			token string
+			ok    bool
+		)
+		for i := 0; i < n; i++ {
+			token, ok = next()
+			if !ok {
+				return "", false, index
+			}
+			index++
+		}
+		token = strings.TrimFunc(token, func(r rune) bool {
+			return strings.ContainsRune("():',", r)
+		})
+		return token, ok, index
+	}, stop
+}
+
+func (t *xfrTracker) parse(logLine string) *xfrState {
+	// Limit the length of the log line to 1024 characters to avoid
+	// parsing excessively long log lines.
+	runes := []rune(logLine)
+	if len(runes) > 1024 {
+		runes = runes[:1024]
+		logLine = string(runes)
+	}
+
+	// Remove any trailing dot from the log line. Some BIND9 logs contain
+	// trailing dots, other don't.
+	logLine = strings.TrimRight(logLine, ".")
+
+	// Tokenize the log line and ensure it has reasonable length.
+	tokens := strings.Fields(logLine)
+	if len(tokens) > 50 {
+		tokens = tokens[:50]
+	}
+
+	// We're only interested in the log lines containing the "transfer" keyword.
+	// All other log lines are ignored. It is possible that we get some unrelated
+	// ones but they will be filtered out once we parse them.
+	if !slices.ContainsFunc(tokens, func(token string) bool {
+		return strings.ToLower(token) == "transfer"
+	}) {
+		return nil
+	}
+
+	var (
+		parsedTime time.Time
+		s          xfrState
+	)
+
+	// The log line typically begins with a timestamp. Parse it and determine
+	// the time format used by BIND 9.
+	parsedTime, s.timeFormat = parseTime(tokens)
+
+	// Go over the tokens.
+	next, stop := pull(tokens)
+	defer stop()
+
+	var (
+		prevToken string
+		msgIndex  int
+	)
+	for {
+		// Get the next token.
+		token, ok, _ := next(1)
+		if !ok {
+			// No more tokens to process.
+			break
+		}
+		// Process the token.
+		switch token {
+		case "zone":
+			parseZone(next, &s)
+		case "view":
+			parseView(next, &s)
+		case "transfer":
+			if index := parseTransfer(next, &s); index >= 0 {
+				msgIndex = index
+				s.status = xfrStatusMessage
+			}
+		case "serial":
+			if err := parseSerial(next, &s); err != nil {
+				log.WithError(err).Debugf("Failed to parse serial %s", token)
+			}
+		case "from":
+			parseFrom(next, &s)
+		case "client":
+			parseClient(next, &s)
+		case "started":
+			s.status = xfrStatusStarted
+			s.startTime = parsedTime
+		case "completed", "ended":
+			s.status = xfrStatusCompleted
+			s.completionTime = parsedTime
+		case "messages", "records", "bytes":
+			if err := parseMessagesRecordsBytes(prevToken, parsedTime, token, &s); err != nil {
+				log.WithError(err).Debugf("Failed to parse the number of %s", token)
+			}
+		case "secs":
+			if err := parseSecs(prevToken, &s); err != nil {
+				log.WithError(err).Debugf("Failed to parse the duration %s", prevToken)
+			}
+		}
+		prevToken = token
+	}
+	if s.zoneName == "" {
+		log.Debug("zone name not found in the log message")
+		return nil
+	}
+	if msgIndex > 0 {
+		s.message = strings.Join(tokens[msgIndex:], " ")
+	}
+	return &s
+}
+
+func parseTransfer(next func(n int) (string, bool, int), s *xfrState) int {
+	var (
+		token string
+		ok    bool
+		index int
+	)
+	if token, ok, index = next(1); !ok || token != "of" {
+		return -1
+	}
+	index = parseZone(next, s)
+	return index
+}
+
+func parseZone(next func(n int) (string, bool, int), s *xfrState) (index int) {
+	if s.zoneName != "" {
+		return
+	}
+	var (
+		token string
+		ok    bool
+	)
+	if token, ok, index = next(1); ok {
+		split := strings.Split(token, "/")
+		if len(split) > 1 {
+			s.zoneName = split[0]
+		}
+		if len(split) > 2 {
+			s.viewName = split[2]
+		}
+	}
+	return
+}
+
+func parseView(next func(n int) (string, bool, int), s *xfrState) {
+	if s.viewName != "" {
+		return
+	}
+	if token, ok, _ := next(1); ok {
+		s.viewName = token
+	}
+}
+
+func parseFrom(next func(n int) (string, bool, int), s *xfrState) {
+	if token, ok, _ := next(1); ok {
+		split := strings.Split(token, "#")
+		if len(split) > 0 {
+			s.server = split[0]
+		}
+	}
+}
+
+func parseClient(next func(n int) (string, bool, int), s *xfrState) {
+	if token, ok, _ := next(2); ok {
+		split := strings.Split(token, "#")
+		if len(split) > 0 {
+			s.client = split[0]
+		}
+	}
+}
+
+func parseSerial(next func(n int) (string, bool, int), s *xfrState) (err error) {
+	if s.serial != 0 {
+		return
+	}
+	if token, ok, _ := next(1); ok {
+		if s.serial, err = strconv.ParseInt(token, 10, 64); err != nil {
+			err = errors.WithStack(err)
+			return
+		}
+	}
+	return err
+}
+
+func parseMessagesRecordsBytes(prevToken string, parsedTime time.Time, token string, s *xfrState) (err error) {
+	if prevToken == "" {
+		return
+	}
+	s.status = xfrStatusCompleted
+	s.completionTime = parsedTime
+	v, err := strconv.ParseInt(prevToken, 10, 64)
+	if err != nil {
+		err = errors.WithStack(err)
+		return
+	}
+	switch token {
+	case "messages":
+		s.messagesCount = v
+	case "records":
+		s.recordsCount = v
+	case "bytes":
+		s.bytesCount = v
+	}
+	return
+}
+
+func parseSecs(prevToken string, s *xfrState) (err error) {
+	if prevToken == "" {
+		return
+	}
+	duration, err := strconv.ParseFloat(prevToken, 64)
+	if err != nil {
+		err = errors.WithStack(err)
+		return
+	}
+	s.duration = time.Duration(duration * float64(time.Second))
+	return
+}
+
+func (t *xfrTracker) putState(key *xfrStateKey, s *xfrState) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	element, ok := t.startedMap[*key]
+	if ok {
+		element.Value = s
+		t.startedList.MoveToBack(element)
+	} else {
+		element := t.startedList.PushBack(s)
+		t.startedMap[*key] = element
+	}
+	if t.startedList.Len() > t.maxStates {
+		element := t.startedList.Remove(t.startedList.Front())
+		state := element.(*xfrState)
+		key := xfrStateKey{
+			viewName: state.viewName,
+			zoneName: state.zoneName,
+			client:   state.client,
+		}
+		delete(t.startedMap, key)
+	}
+}
+
+func (t *xfrTracker) updateState(key *xfrStateKey, s *xfrState) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	element, ok := t.startedMap[*key]
+	if !ok {
+		return
+	}
+	state := element.Value.(*xfrState)
+	if state.startTime.IsZero() {
+		return
+	}
+	s.startTime = state.startTime
+	t.startedList.MoveToBack(element)
+	element.Value = s
+}
+
+func (t *xfrTracker) moveStateToCompleted(key *xfrStateKey, s *xfrState) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	element, ok := t.startedMap[*key]
+	if ok {
+		t.startedList.Remove(element)
+		delete(t.startedMap, *key)
+	}
+	t.completedList.PushBack(s)
+	if t.completedList.Len() > t.maxStates {
+		t.completedList.Remove(t.completedList.Front())
+	}
+}
+
+func (t *xfrTracker) feed(logLine string) {
+	s := t.parse(logLine)
+	if s == nil {
+		return
+	}
+	key := xfrStateKey{
+		viewName: s.viewName,
+		zoneName: s.zoneName,
+		client:   s.client,
+	}
+	switch s.status {
+	case xfrStatusStarted:
+		t.putState(&key, s)
+	case xfrStatusMessage:
+		t.updateState(&key, s)
+	case xfrStatusCompleted:
+		t.moveStateToCompleted(&key, s)
+	default:
+	}
+}
+
+func (t *xfrTracker) getNotCompleted() []*xfrState {
+	t.mutex.RLock()
+	defer t.mutex.RUnlock()
+	states := make([]*xfrState, 0, t.startedList.Len())
+	for element := t.startedList.Front(); element != nil; element = element.Next() {
+		states = append(states, element.Value.(*xfrState))
+	}
+	return states
+}
+
+func (t *xfrTracker) getCompleted() []*xfrState {
+	t.mutex.RLock()
+	defer t.mutex.RUnlock()
+	states := make([]*xfrState, 0, t.completedList.Len())
+	for element := t.completedList.Front(); element != nil; element = element.Next() {
+		states = append(states, element.Value.(*xfrState))
+	}
+	return states
 }
--- backend/agent/xfrtracker_test.go
+++ backend/agent/xfrtracker_test.go
@@ -2,16 +2,22 @@ package agent
 
 import (
 	"bufio"
+	_ "embed"
+	"fmt"
 	"path/filepath"
 	"strings"
 	"testing"
 	"testing/synctest"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	gomock "go.uber.org/mock/gomock"
 	"isc.org/stork/testutil"
 	storkutil "isc.org/stork/util"
 )
+
+//go:embed testdata/xfr-mixed-logs.txt
+var xfrMixedLogs string
 
 // Test instantiating the XFR tracker.
 func TestNewXfrTracker(t *testing.T) {
@@ -256,4 +262,293 @@ func TestXfrTrackerTrackSystemdUnit(t *testing.T) {
 		// Verify that the two subscribers were different.
 		require.NotEqual(t, firstSubscriber0, secondSubscriber0)
 	})
+}
+
+func TestXfrTrackerParseIncomingTransferStarted(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("23-Feb-2026 10:41:27.071 zone bind9.example.org/IN: Transfer started.")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusStarted, xfrState.status)
+	require.Empty(t, xfrState.viewName)
+	require.Equal(t, "bind9.example.org", xfrState.zoneName)
+	require.Zero(t, xfrState.serial)
+	require.Empty(t, xfrState.client)
+	require.Empty(t, xfrState.server)
+	require.Zero(t, xfrState.serial)
+	require.Zero(t, xfrState.messagesCount)
+	require.Zero(t, xfrState.recordsCount)
+	require.Zero(t, xfrState.bytesCount)
+	require.Zero(t, xfrState.duration)
+	require.Equal(t, xfrStatusStarted, xfrState.status)
+	require.NotZero(t, xfrState.startTime)
+	require.Zero(t, xfrState.completionTime)
+	require.Equal(t, xfrTimeFormatBind9, xfrState.timeFormat)
+	require.Equal(t, "Transfer started", xfrState.message)
+}
+
+func TestXfrTrackerParseIncomingTransferConnected(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("23-Feb-2026 10:41:27.115 0x7ffff943c000: transfer of 'bind9.example.org/IN' from 192.5.5.241#53: connected using 192.5.5.241#53")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusMessage, xfrState.status)
+	require.Empty(t, xfrState.viewName)
+	require.Equal(t, "bind9.example.org", xfrState.zoneName)
+	require.Equal(t, "192.5.5.241", xfrState.server)
+	require.Empty(t, xfrState.client)
+	require.Zero(t, xfrState.serial)
+	require.Zero(t, xfrState.messagesCount)
+	require.Equal(t, "Transfer connected", xfrState.message)
+}
+
+func TestXfrTrackerParseIncomingTransferStatusSuccess(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("23-Feb-2026 10:41:27.147 0x7ffffb63b000: transfer of 'drop.rpz.example.com/IN' from 172.24.0.53#53: Transfer status: success")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusMessage, xfrState.status)
+	require.Empty(t, xfrState.viewName)
+	require.Equal(t, "drop.rpz.example.com", xfrState.zoneName)
+	require.Equal(t, "172.24.0.53", xfrState.server)
+	require.Empty(t, xfrState.client)
+	require.Zero(t, xfrState.serial)
+	require.Zero(t, xfrState.messagesCount)
+	require.Zero(t, xfrState.recordsCount)
+	require.Zero(t, xfrState.bytesCount)
+}
+
+func TestXfrTrackerParseIncomingTransferCompleted(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("23-Feb-2026 10:41:27.147 0x7ffffb63b000: transfer of 'bind9.example.org/IN' from 172.24.0.53#53: Transfer completed: 1 messages, 5 records, 294 bytes, 0.014 secs (21000 bytes/sec) (serial 201702121)")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusCompleted, xfrState.status)
+	require.Empty(t, xfrState.viewName)
+	require.Equal(t, "bind9.example.org", xfrState.zoneName)
+	require.Equal(t, "172.24.0.53", xfrState.server)
+	require.Empty(t, xfrState.client)
+	require.EqualValues(t, 201702121, xfrState.serial)
+	require.EqualValues(t, 1, xfrState.messagesCount)
+	require.EqualValues(t, 5, xfrState.recordsCount)
+	require.EqualValues(t, 294, xfrState.bytesCount)
+	require.Equal(t, xfrState.duration, 14*time.Millisecond)
+	require.NotZero(t, xfrState.completionTime)
+	require.Zero(t, xfrState.startTime)
+}
+
+func TestXfrTrackerParseIncomingTransferStartedView(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("zone drop.rpz.example.com/IN/trusted: Transfer started.")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusStarted, xfrState.status)
+	require.Equal(t, "trusted", xfrState.viewName)
+	require.Equal(t, "drop.rpz.example.com", xfrState.zoneName)
+	require.Empty(t, xfrState.server)
+	require.Empty(t, xfrState.client)
+	require.Zero(t, xfrState.serial)
+	require.Zero(t, xfrState.messagesCount)
+}
+
+func TestXfrTrackerParseIncomingTransferConnectedView(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("0x7ffff7c3b000: transfer of 'drop.rpz.example.com/IN/trusted' from 172.24.0.53#53: connected using 172.24.0.53#53 TSIG trusted-key")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusMessage, xfrState.status)
+	require.Equal(t, "trusted", xfrState.viewName)
+	require.Equal(t, "drop.rpz.example.com", xfrState.zoneName)
+	require.Equal(t, "172.24.0.53", xfrState.server)
+	require.Empty(t, xfrState.client)
+	require.Zero(t, xfrState.serial)
+	require.Zero(t, xfrState.messagesCount)
+}
+
+func TestXfrTrackerParseOutgoingTransferStarted(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("23-Feb-2026 10:41:27.138 client @0x7ffffaa28c00 172.24.0.54#34961/key trusted-key (drop.rpz.example.com): view trusted: transfer of 'drop.rpz.example.com/IN': AXFR started: TSIG trusted-key (serial 201702121)")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusStarted, xfrState.status)
+	require.Equal(t, "trusted", xfrState.viewName)
+	require.Equal(t, "drop.rpz.example.com", xfrState.zoneName)
+	require.Equal(t, "172.24.0.54", xfrState.client)
+	require.Empty(t, xfrState.server)
+	require.EqualValues(t, 201702121, xfrState.serial)
+	require.Zero(t, xfrState.messagesCount)
+	require.Zero(t, xfrState.recordsCount)
+	require.Zero(t, xfrState.bytesCount)
+}
+
+func TestXfrTrackerParseOutgoingTransferCompleted(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("23-Feb-2026 10:41:27.141 client @0x7ffffaa28c00 172.24.0.54#34961/key trusted-key (drop.rpz.example.com): view trusted: transfer of 'drop.rpz.example.com/IN': AXFR ended: 1 messages, 5 records, 294 bytes, 0.004 secs (73500 bytes/sec) (serial 201702121)")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusCompleted, xfrState.status)
+	require.Equal(t, "trusted", xfrState.viewName)
+	require.Equal(t, "drop.rpz.example.com", xfrState.zoneName)
+	require.Equal(t, "172.24.0.54", xfrState.client)
+	require.Empty(t, xfrState.server)
+	require.EqualValues(t, 201702121, xfrState.serial)
+	require.EqualValues(t, 1, xfrState.messagesCount)
+	require.EqualValues(t, 5, xfrState.recordsCount)
+	require.EqualValues(t, 294, xfrState.bytesCount)
+	require.Equal(t, xfrState.duration, 4*time.Millisecond)
+	require.NotZero(t, xfrState.completionTime)
+	require.Zero(t, xfrState.startTime)
+}
+
+func TestXfrTrackerParseSettingUpZoneTransferFailed(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("16-Apr-2026 12:09:11.650 client @0x7ffffa436000 127.0.0.1#55256 (bind.example.org): transfer of 'bind.example.org/IN': setting up zone transfer: failed")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusMessage, xfrState.status)
+	require.Equal(t, "bind.example.org", xfrState.zoneName)
+	require.Equal(t, "127.0.0.1", xfrState.client)
+	require.Empty(t, xfrState.server)
+	require.Zero(t, xfrState.serial)
+	require.Zero(t, xfrState.messagesCount)
+	require.Zero(t, xfrState.recordsCount)
+	require.Zero(t, xfrState.bytesCount)
+	require.Zero(t, xfrState.duration)
+	require.Zero(t, xfrState.completionTime)
+	require.Zero(t, xfrState.startTime)
+}
+
+func TestXfrTrackerParseSettingUpZoneTransferAborted(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("16-Apr-2026 12:09:11.650 client @0x7ffffa436000 127.0.0.1#55256 (bind.example.org): transfer of 'bind.example.org/IN': aborted")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusMessage, xfrState.status)
+	require.Equal(t, "bind.example.org", xfrState.zoneName)
+	require.Equal(t, "127.0.0.1", xfrState.client)
+	require.Empty(t, xfrState.server)
+	require.Zero(t, xfrState.serial)
+	require.Zero(t, xfrState.messagesCount)
+	require.Zero(t, xfrState.recordsCount)
+	require.Zero(t, xfrState.bytesCount)
+	require.Zero(t, xfrState.duration)
+	require.Zero(t, xfrState.completionTime)
+	require.Zero(t, xfrState.startTime)
+}
+
+func TestXfrTrackerParseSystemdLogs(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrState := xfrTracker.parse("2026-04-21T20:38:52+02:00 lightning named[1411]: client @0x7ffffa436000 127.0.0.1#55256 (bind.example.org): transfer of 'bind.example.org/IN': started")
+	require.NotNil(t, xfrState)
+	require.Equal(t, xfrStatusStarted, xfrState.status)
+	require.Equal(t, "bind.example.org", xfrState.zoneName)
+	require.Equal(t, "127.0.0.1", xfrState.client)
+	require.Empty(t, xfrState.server)
+	require.Zero(t, xfrState.serial)
+	require.Zero(t, xfrState.messagesCount)
+	require.Zero(t, xfrState.recordsCount)
+	require.Zero(t, xfrState.bytesCount)
+	require.Zero(t, xfrState.duration)
+	require.Zero(t, xfrState.completionTime)
+	require.Zero(t, xfrState.startTime)
+}
+
+func TestXfrTrackerFeed(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrMixedLogs := strings.Split(xfrMixedLogs, "\n")
+	for _, logLine := range xfrMixedLogs {
+		xfrTracker.feed(logLine)
+	}
+
+	notCompleted := xfrTracker.getNotCompleted()
+	require.Len(t, notCompleted, 1)
+	completed := xfrTracker.getCompleted()
+	require.Len(t, completed, 4)
+}
+
+func TestXfrTrackerLimits(t *testing.T) {
+	xfrTracker := newXfrTracker(nil)
+	xfrTracker.maxStates = 3
+	startedTemplate := "23-Feb-2026 10:41:27.141 @0x7ffffaa28c00 172.24.0.54#34961: transfer of '%d.example.com/IN': AXFR started"
+	for i := 0; i < xfrTracker.maxStates; i++ {
+		xfrTracker.feed(fmt.Sprintf(startedTemplate, i))
+		notCompleted := xfrTracker.getNotCompleted()
+		require.Len(t, notCompleted, i+1)
+		completed := xfrTracker.getCompleted()
+		require.Empty(t, completed)
+	}
+
+	xfrTracker.feed(fmt.Sprintf(startedTemplate, 3))
+	notCompleted := xfrTracker.getNotCompleted()
+	require.Len(t, notCompleted, 3)
+	require.Equal(t, notCompleted[0].zoneName, "1.example.com")
+	require.Equal(t, notCompleted[1].zoneName, "2.example.com")
+	require.Equal(t, notCompleted[2].zoneName, "3.example.com")
+	require.Empty(t, xfrTracker.getCompleted())
+
+	xfrTracker.feed(fmt.Sprintf(startedTemplate, 4))
+	notCompleted = xfrTracker.getNotCompleted()
+	require.Len(t, notCompleted, 3)
+	require.Equal(t, notCompleted[0].zoneName, "2.example.com")
+	require.Equal(t, notCompleted[1].zoneName, "3.example.com")
+	require.Equal(t, notCompleted[2].zoneName, "4.example.com")
+	require.Empty(t, xfrTracker.getCompleted())
+
+	endedTemplate := "23-Feb-2026 10:41:27.141 @0x7ffffaa28c00 172.24.0.54#34961: transfer of '%d.example.com/IN': AXFR completed"
+	xfrTracker.feed(fmt.Sprintf(endedTemplate, 3))
+	completed := xfrTracker.getCompleted()
+	require.Len(t, completed, 1)
+	require.Equal(t, completed[0].zoneName, "3.example.com")
+	notCompleted = xfrTracker.getNotCompleted()
+	require.Len(t, notCompleted, 2)
+	require.Equal(t, notCompleted[0].zoneName, "2.example.com")
+	require.Equal(t, notCompleted[1].zoneName, "4.example.com")
+
+	xfrTracker.feed(fmt.Sprintf(endedTemplate, 2))
+	completed = xfrTracker.getCompleted()
+	require.Len(t, completed, 2)
+	require.Equal(t, completed[0].zoneName, "3.example.com")
+	require.Equal(t, completed[1].zoneName, "2.example.com")
+	notCompleted = xfrTracker.getNotCompleted()
+	require.Len(t, notCompleted, 1)
+	require.Equal(t, notCompleted[0].zoneName, "4.example.com")
+
+	xfrTracker.feed(fmt.Sprintf(startedTemplate, 5))
+	notCompleted = xfrTracker.getNotCompleted()
+	require.Len(t, notCompleted, 2)
+	require.Equal(t, notCompleted[0].zoneName, "4.example.com")
+	require.Equal(t, notCompleted[1].zoneName, "5.example.com")
+	completed = xfrTracker.getCompleted()
+	require.Len(t, completed, 2)
+
+	xfrTracker.feed(fmt.Sprintf(endedTemplate, 5))
+	completed = xfrTracker.getCompleted()
+	require.Len(t, completed, 3)
+	require.Equal(t, completed[0].zoneName, "3.example.com")
+	require.Equal(t, completed[1].zoneName, "2.example.com")
+	require.Equal(t, completed[2].zoneName, "5.example.com")
+	notCompleted = xfrTracker.getNotCompleted()
+	require.Len(t, notCompleted, 1)
+	require.Equal(t, notCompleted[0].zoneName, "4.example.com")
+
+	xfrTracker.feed(fmt.Sprintf(endedTemplate, 4))
+	completed = xfrTracker.getCompleted()
+	require.Len(t, completed, 3)
+	require.Equal(t, completed[0].zoneName, "2.example.com")
+	require.Equal(t, completed[1].zoneName, "5.example.com")
+	require.Equal(t, completed[2].zoneName, "4.example.com")
+	notCompleted = xfrTracker.getNotCompleted()
+	require.Empty(t, notCompleted)
+}
+
+// Fuzz the XFR tracker parse function to ensure that it does not panic
+// for invalid log lines.
+func FuzzXfrTrackerParse(f *testing.F) {
+	logs := strings.Split(xfrMixedLogs, "\n")
+	for _, tc := range logs {
+		f.Add(tc)
+	}
+	f.Fuzz(func(t *testing.T, logLine string) {
+		xfrTracker := newXfrTracker(nil)
+		require.NotPanics(t, func() { xfrTracker.parse(logLine) })
+	})
+}
+
+// Benchmark the XFR tracker parse function.
+func BenchmarkXfrTrackerParse(b *testing.B) {
+	xfrTracker := newXfrTracker(nil)
+	for i := 0; i < b.N; i++ {
+		xfrState := xfrTracker.parse("23-Feb-2026 10:41:27.141 client @0x7ffffaa28c00 172.24.0.54#34961/key trusted-key (drop.rpz.example.com): view trusted: transfer of 'drop.rpz.example.com/IN': AXFR ended: 1 messages, 5 records, 294 bytes, 0.004 secs (73500 bytes/sec) (serial 201702121)")
+		require.NotNil(b, xfrState)
+	}
 }
