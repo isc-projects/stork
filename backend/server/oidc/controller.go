bffc38278cd9d7fca64dfa04321dea085b7b69f8
Piotrek Zadroga <piotrek@isc.org>
2026-05-13 10:07:47 +0200

    [#2455] add OIDC cotroller

---
+++ backend/server/oidc/controller.go
@@ -0,0 +1,57 @@
+package oidc
+
+import (
+	"net/http"
+	"net/url"
+
+	dbops "isc.org/stork/server/database"
+	dbsession "isc.org/stork/server/database/session"
+)
+
+// High-level component controlling OpenID Connect authentication.
+// It provides a middleware which handles HTTP requests related
+// to OIDC authentication flow.
+type Controller struct {
+	settings         Settings
+	configured       bool
+	db               *dbops.PgDB
+	dbSessionManager *dbsession.SessionMgr
+}
+
+// Constructs OIDC controller instance. It requires the settings
+// and a pointer to Stork server database, which is used to insert
+// and update authenticated users.
+func NewController(settings Settings, db *dbops.PgDB) *Controller {
+	if settings.IssuerURL == "" {
+		return &Controller{
+			configured: false,
+		}
+	}
+	return &Controller{
+		settings: settings,
+		db:       db,
+	}
+}
+
+// Configures the controller. It should be called only once at server startup.
+// It requires server URL used to construct OIDC redirection URI and the session
+// manager to create sessions for authenticated users.
+func (ctl *Controller) Configure(serverURL url.URL, dbSessionManager *dbsession.SessionMgr) {
+	if ctl.settings.IssuerURL == "" {
+		ctl.configured = false
+		return
+	}
+	ctl.dbSessionManager = dbSessionManager
+	ctl.configured = true
+}
+
+// Provides middleware handling all OIDC-related HTTP requests.
+// It should be chained with other server's middlewares.
+// If OIDC is not configured by end user, it is transparent.
+func (ctl *Controller) Middleware(next http.Handler) http.Handler {
+	if !ctl.configured {
+		// In case OIDC was not configured, make the middleware transparent.
+		return next
+	}
+	return ctl.dbSessionManager.SessionMiddleware(next)
+}
--- backend/server/oidc/settings.go
+++ backend/server/oidc/settings.go
@@ -1,5 +1,6 @@
 package oidc
 
+// The main OIDC settings structure.
 type Settings struct {
 	IssuerURL string `long:"oidc-issuer-url" description:"The OID Provider Issuer URL used for OIDC discovery process." env:"STORK_OIDC_ISSUER_URL" default:""`
 }
