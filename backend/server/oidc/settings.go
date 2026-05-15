efe73152daad185d9d1e341103a913f21e0b13a8
Piotrek Zadroga <piotrek@isc.org>
2026-05-15 15:59:56 +0200

    [#2457] Apply 2 suggestions

--- backend/server/oidc/settings.go
+++ backend/server/oidc/settings.go
@@ -9,9 +9,9 @@ type CommaSeparatedStrings []string
 
 // Specifies mapping between OIDC groups returned from token endpoint and Stork groups.
 type GroupMapping struct {
-	Admin      CommaSeparatedStrings `long:"oidc-group-admin" description:"The group returned from OIDC token endpoint that can be mapped to Stork 'admin' group; also accepts a comma-separated list of group names" env:"STORK_OIDC_GROUP_ADMIN" default:"stork-admin"`
-	SuperAdmin CommaSeparatedStrings `long:"oidc-group-super-admin" description:"The group returned from OIDC token endpoint that can be mapped to Stork 'super-admin' group; also accepts a comma-separated list of group names" env:"STORK_OIDC_GROUP_SUPER_ADMIN" default:"stork-super-admin"`
-	ReadOnly   CommaSeparatedStrings `long:"oidc-group-read-only" description:"The group returned from OIDC token endpoint that can be mapped to Stork 'read-only' group; also accepts a comma-separated list of group names" env:"STORK_OIDC_GROUP_READ_ONLY" default:"stork-read-only"`
+	Admin      CommaSeparatedStrings `long:"oidc-group-admin" description:"The claim value returned from OIDC token endpoint that can be mapped to Stork 'admin' group; also accepts a comma-separated list of group names" env:"STORK_OIDC_GROUP_ADMIN" default:"stork-admin"`
+	SuperAdmin CommaSeparatedStrings `long:"oidc-group-super-admin" description:"The claim value returned from OIDC token endpoint that can be mapped to Stork 'super-admin' group; also accepts a comma-separated list of group names" env:"STORK_OIDC_GROUP_SUPER_ADMIN" default:"stork-super-admin"`
+	ReadOnly   CommaSeparatedStrings `long:"oidc-group-read-only" description:"The claim value returned from OIDC token endpoint that can be mapped to Stork 'read-only' group; also accepts a comma-separated list of group names" env:"STORK_OIDC_GROUP_READ_ONLY" default:"stork-read-only"`
 }
 
 // The main OIDC settings structure.
@@ -19,7 +19,7 @@ type Settings struct {
 	IssuerURL            string                `long:"oidc-issuer-url" description:"Mandatory. The OID Provider Issuer URL used for OIDC discovery process." env:"STORK_OIDC_ISSUER_URL" default:""`
 	ClientID             string                `long:"oidc-client-id" description:"Mandatory. Client ID registered at the OID Provider." env:"STORK_OIDC_CLIENT_ID" default:""`
 	ClientSecret         string                `long:"oidc-client-secret" description:"Optional. Client secret provided by the OID Provider. Optional, because only some Providers require this in the OIDC process." env:"STORK_OIDC_CLIENT_SECRET" default:""`
-	IdentityProviderName string                `long:"oidc-provider-name" description:"Optional. The OID Provider name that will be displayed on a Login page. Leave blank to display generic OAuth/OIDC name." env:"STORK_OIDC_PROVIDER_NAME" default:""`
+	IdentityProviderName string                `long:"oidc-provider-name" description:"Optional. The OID Provider name that will be displayed on a Login page. Leave blank to display generic OAuth/OIDC name." env:"STORK_OIDC_PROVIDER_NAME" default:"OpenID Connect"`
 	MandatoryAllowGroup  string                `long:"oidc-group-allow" description:"The mandatory group that user must belong to, to access Stork, empty for allow all users" env:"STORK_OIDC_GROUP_ALLOW" default:""`
 	EnableGroupMapping   bool                  `long:"oidc-map-groups" description:"Enable mapping OIDC groups returned from token endpoint to Stork groups" env:"STORK_OIDC_MAP_GROUPS"`
 	GroupMapping         GroupMapping          `group:"OIDC to Stork group mapping"`
