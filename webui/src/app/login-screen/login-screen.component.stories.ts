b01db7ad297eb58f7853658aed746cfc816eb617
Piotrek Zadroga <piotrek@isc.org>
2026-05-25 16:45:52 +0200

    [#2480] add oidc interaction tests

--- webui/src/app/login-screen/login-screen.component.stories.ts
+++ webui/src/app/login-screen/login-screen.component.stories.ts
@@ -6,6 +6,7 @@ import { MessageService } from 'primeng/api'
 import { toastDecorator } from '../utils-stories'
 import { action } from 'storybook/actions'
 import { provideRouter, withHashLocation } from '@angular/router'
+import { userEvent, within, expect, waitFor } from 'storybook/test'
 
 export default {
     title: 'App/LoginScreen',
@@ -35,7 +36,7 @@ export default {
                 status: 200,
                 delay: 2000,
                 response: {
-                    total: 4,
+                    total: 5,
                     items: [
                         {
                             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
@@ -152,5 +153,149 @@ export const FailedFetch: Story = {
                 },
             },
         ],
+    },
+}
+
+export const TestAuthMethodIsStoredInLocalStorage: Story = {
+    parameters: {
+        mockData: [
+            {
+                url: 'http://localhost/api/version',
+                method: 'GET',
+                status: 200,
+                delay: 100,
+                response: { date: '2023-03-03', version: '1.4.2' } as Version,
+            },
+            {
+                url: 'http://localhost/api/authentication-methods',
+                method: 'GET',
+                status: 200,
+                delay: 100,
+                response: {
+                    total: 3,
+                    items: [
+                        {
+                            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                            formLabelIdentifier: 'Login/Email',
+                            formLabelSecret: 'Password',
+                            id: 'internal',
+                            name: 'Internal',
+                        },
+                        {
+                            description: 'Fusce faucibus mauris purus, in mattis tellus pellentesque sed.',
+                            formLabelIdentifier: 'Login',
+                            formLabelSecret: 'Password',
+                            id: 'ldap',
+                            name: 'LDAP',
+                        },
+                        {
+                            description:
+                                'OAuth2/OIDC authentication. You will get redirected to OpenID Provider to authenticate and authorize your access to Stork.',
+                            id: 'oidc',
+                            name: 'Log in with OpenID Connect',
+                        },
+                    ],
+                } as AuthenticationMethods,
+            },
+        ],
+    },
+    play: async ({ canvasElement }) => {
+        // Arrange
+        const canvas = within(canvasElement)
+        const combobox = await canvas.findByRole('combobox')
+        // Configure delay between consecutive user events to be more human-like and to give more time for PrimeNG animations when automatically testing.
+        const user = userEvent.setup({ delay: 50 })
+
+        // Act
+        let options
+        await user.click(combobox)
+        options = await canvas.findAllByRole('option')
+        await expect(options.length).toBeGreaterThanOrEqual(3)
+
+        // Reset storage to have selected the first option.
+        await user.click(options[0])
+        await expect(combobox).toHaveTextContent('Internal')
+
+        await user.click(combobox)
+        options = await canvas.findAllByRole('option')
+        await expect(options.length).toBeGreaterThanOrEqual(3)
+        await user.click(options[2])
+
+        // Assert
+        const stored = localStorage.getItem('selected-auth-method')
+        await expect(stored).not.toBeFalsy()
+        await expect(stored).toEqual('oidc')
+
+        await expect(combobox).toHaveTextContent('Log in with OpenID Connect')
+        await expect(canvas.queryByLabelText('Login')).toBeNull()
+        await expect(canvas.queryByLabelText('Login/Email')).toBeNull()
+        await expect(canvas.queryByLabelText('Password')).toBeNull()
+        const btn = await canvas.findByRole('link', { name: 'Log in with OpenID Connect' })
+        await expect(btn).not.toBeFalsy()
+    },
+}
+
+export const TestAuthMethodIsReadFromLocalStorage: Story = {
+    parameters: {
+        mockData: [
+            {
+                url: 'http://localhost/api/version',
+                method: 'GET',
+                status: 200,
+                delay: 100,
+                response: { date: '2023-03-03', version: '1.4.2' } as Version,
+            },
+            {
+                url: 'http://localhost/api/authentication-methods',
+                method: 'GET',
+                status: 200,
+                delay: 200,
+                response: {
+                    total: 3,
+                    items: [
+                        {
+                            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                            formLabelIdentifier: 'Login/Email',
+                            formLabelSecret: 'Password',
+                            id: 'internal',
+                            name: 'Internal',
+                        },
+                        {
+                            description: 'Fusce faucibus mauris purus, in mattis tellus pellentesque sed.',
+                            formLabelIdentifier: 'Login',
+                            formLabelSecret: 'Password',
+                            id: 'ldap',
+                            name: 'LDAP',
+                        },
+                        {
+                            description:
+                                'OAuth2/OIDC authentication. You will get redirected to OpenID Provider to authenticate and authorize your access to Stork.',
+                            id: 'oidc',
+                            name: 'Log in with OpenID Connect',
+                        },
+                    ],
+                } as AuthenticationMethods,
+            },
+        ],
+    },
+    play: async ({ canvasElement }) => {
+        // Arrange + Act + Assert
+        localStorage.setItem('selected-auth-method', 'ldap')
+        const canvas = within(canvasElement)
+        await waitFor(() => canvas.findByRole('combobox'))
+        const combobox = await canvas.findByRole('combobox')
+        await expect(combobox).toHaveTextContent('LDAP')
+    },
+}
+
+export const TestDisplayFirstAuthMethodIfLocalStorageEntryNotFound: Story = {
+    parameters: TestAuthMethodIsReadFromLocalStorage.parameters,
+    play: async ({ canvasElement }) => {
+        // Arrange + Act + Assert
+        localStorage.setItem('selected-auth-method', 'does-not-exist-any-more')
+        const canvas = within(canvasElement)
+        await waitFor(() => canvas.findByRole('combobox'))
+        const combobox = await canvas.findByRole('combobox')
+        await expect(combobox).toHaveTextContent('Internal')
     },
 }
