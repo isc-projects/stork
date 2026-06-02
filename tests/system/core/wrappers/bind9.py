b0f94c9d08ca87eb0534a0ec2b03436507916872
Tomek Sawka <tsawka@isc.org>
2026-06-02 20:54:38 +0200

    [#1910] remove service wrappers

--- tests/system/core/wrappers/bind9.py
+++
@@ -1,9 +0,0 @@
-from core.wrappers.agent import Agent
-
-
-class Bind9(Agent):
-    """
-    A wrapper for the docker-compose service containing Bind9 and Stork Agent.
-    """
-
-    prometheus_exporter_port = 9119
