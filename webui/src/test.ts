4e4d969e0d83440d6cab842d2ecb1174f3a6db65
Tomek Mrugalski <tomek@isc.org>
2026-06-10 18:54:38 +0200

    [#2488] Migrated to angular 21.2.16

--- webui/src/test.ts
+++ webui/src/test.ts
@@ -1,7 +1,8 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing'
-import { getTestBed } from '@angular/core/testing'
+import { provideZoneChangeDetection } from '@angular/core'
+import { getTestBed, TestBed, TestModuleMetadata } from '@angular/core/testing'
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing'
 
 // First, initialize the Angular testing environment.
@@ -9,3 +10,11 @@ getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDyn
     errorOnUnknownElements: true,
     errorOnUnknownProperties: true,
 })
+
+// Angular 21 defaults tests to zoneless change detection. Zone.js-based apps must opt in explicitly.
+const configureTestingModule = TestBed.configureTestingModule.bind(TestBed)
+TestBed.configureTestingModule = (moduleDef: TestModuleMetadata) =>
+    configureTestingModule({
+        ...moduleDef,
+        providers: [provideZoneChangeDetection({ eventCoalescing: true }), ...(moduleDef.providers ?? [])],
+    })
