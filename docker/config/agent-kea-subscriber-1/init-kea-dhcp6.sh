e60e3b0ea59f70f524cf82044f2852d881abf204
Slawek Figiel <slawek@isc.org>
2026-08-24 20:00:53 +0200

    [#2597] Add DHCPv6 daemons

---
+++ docker/config/agent-kea-subscriber-1/init-kea-dhcp6.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -eu
+
+kea-shell --service dhcp6 remote-server6-set << 'EOF'
+"servers": [
+    {
+        "server-tag": "agent-kea-subscriber-1",
+        "description": "A DHCP server for testing purposes."
+    }
+]
+EOF
+
+kea-shell --service dhcp6 remote-network6-set << 'EOF'
+"shared-networks": [
+    {
+        "name": "palma"
+    }
+],
+"server-tags": [ "all" ]
+EOF
+
+kea-shell --service dhcp6 remote-subnet6-set << 'EOF'
+"subnets": [
+    {
+        "id": 5,
+        "subnet": "2000:db8:19::/64",
+        "shared-network-name": "palma",
+        "pools": [ { "pool": "2000:db8:19::100-2000:db8:19::200" } ]
+    }
+],
+"server-tags": [ "all" ]
+EOF
+
+kea-shell --service dhcp6 remote-subnet6-set << 'EOF'
+"subnets": [
+    {
+        "id": 6,
+        "subnet": "2000:db8:20::/64",
+        "shared-network-name": null,
+        "pools": [ { "pool": "2000:db8:20::100-2000:db8:20::200" } ]
+    }
+],
+"server-tags": [ "agent-kea-subscriber-1" ]
+EOF
---
+++ docker/config/agent-kea-subscriber-2/init-kea-dhcp6.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+kea-shell --service dhcp6 remote-server6-set << 'EOF'
+"servers": [
+    {
+        "server-tag": "agent-kea-subscriber-2",
+        "description": "A DHCP server for testing purposes."
+    }
+]
+EOF
+
+kea-shell --service dhcp6 remote-subnet6-set << 'EOF'
+"subnets": [
+    {
+        "id": 7,
+        "subnet": "2000:db8:21::/64",
+        "shared-network-name": null,
+        "pools": [ { "pool": "2000:db8:21::100-2000:db8:21::200" } ]
+    }
+],
+"server-tags": [ "agent-kea-subscriber-2" ]
+EOF
