22a68d784859875b7999ad7ce1840941e37f37d6
Slawek Figiel <slawek@isc.org>
2026-08-31 15:50:57 +0200

    [#2627] Fix routing issue

--- webui/src/app/app-routing.module.ts
+++ webui/src/app/app-routing.module.ts
@@ -152,6 +152,11 @@ const routes: Routes = [
     },
     {
         path: 'dns/zone-transfers',
+        pathMatch: 'full',
+        redirectTo: 'dns/zone-transfers/all',
+    },
+    {
+        path: 'dns/zone-transfers/:id',
         component: ZoneTransfersPageComponent,
         canActivate: [AuthGuard],
         data: { key: 'zone-transfers' },
